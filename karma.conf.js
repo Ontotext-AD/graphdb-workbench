5e2b60132804bafca4ac726cd7313130585969b2
Mihail Radkov <mihail.radkov@ontotext.com>
2019-07-19 13:38:15 +0300

    Configured Travis CI build cycle
    The cycle includes triggering Jasmine tests under PhantomJS (headless)
    and coverage publishing to Coveralls.

--- karma.conf.js
+++ karma.conf.js
@@ -33,14 +33,23 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/js/angular/**/*.js': ['coverage'],
+      'src/app.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type: 'lcov',
+      dir : 'coverage/',
+      subdir: '.'
+    },
 
     // web server port
     port: 9876,
@@ -72,4 +81,4 @@ module.exports = function(config) {
     // how many browser should be started simultaneous
     concurrency: Infinity
   })
-}
+};
