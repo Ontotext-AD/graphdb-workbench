847638820d62bb86bc139aa9cd03f1b2069ac750
Svilen Velikov <svilen.velikov@sirma.bg>
2019-07-19 10:48:44 +0300

    Configure test framework and make the tests pass
    Add test runner script in package.json
    
    Add test runner script in package.json.
    Moved angular-mocks library in test package.
    Deleted unused code.
    
    Add section for the tests in the README
    
    Deleted logging

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ The web application for GraphDB APIs
 Running `npm run start:dev` will start webpack development server which servers the files from src/ and proxy requests 
 to localhost:7200 (default)
 
+Unit tests can be run by executing `npm test`. It's important to be noted that the angular components in the application 
+and the tests are build as AMD modules using Requirejs as module loader. The test framework is Jasmine and Karma as test 
+launcher. Karma is configured to watch source and tests for changes and continuously re-executing the tests.
+
 
 The repo includes sample Dockerfile which can be build and started using `docker-compose up --build`. 
 The compose requires to create `.env` file in the root directory of the project where the `HOST_IP` environment variable 
---
+++ karma.conf.js
@@ -0,0 +1,75 @@
+// Karma configuration
+// Generated on Thu Jul 18 2019 18:49:29 GMT+0300 (EEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'requirejs'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/test-main.js',
+      'src/js/lib/jquery/2.1.4/jquery.min.js',
+      'src/js/lib/angularjs/1.3.8/angular.js',
+      'test/lib/angularjs/1.3.8/angular-mocks.js',
+      { pattern: 'src/**/*.js', included: false },
+      { pattern: 'test/**/*.spec.js', included: false }
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+        'src/app.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
