c317c1b23727836b3b20907d1e78a22b9dc2ac59
Mihail Radkov <mihail.radkov@ontotext.com>
2019-08-27 17:17:22 +0300

    Added Cypress tests for the Swagger documentation page
    Tested the presence of Swagger's iframe, GraphDB and RDF4J sections and
    the operations for each controller.
    
    Added custom command for encapsulating iframes content due to Cypress
    inability to work with them out of the box. The command will yield the
    frame's body and allow further chaining of commands and assertions.

--- test-cypress/support/commands.js
+++ test-cypress/support/commands.js
@@ -27,6 +27,32 @@
 import './repository-commands';
 import './import-commands';
 
+/**
+ * Cypress cannot directly work with iframes due to https://github.com/cypress-io/cypress/issues/136
+ *
+ * This command acts as a workaround that encapsulates the frame's content and makes it queryable and assertable.
+ */
+Cypress.Commands.add('iframe', {prevSubject: 'element'}, ($iframe) => {
+    Cypress.log({
+        name: 'iframe',
+        consoleProps() {
+            return {
+                iframe: $iframe,
+            };
+        },
+    });
+    return new Cypress.Promise(resolve => {
+        // Directly resolve the body if it is loaded, otherwise wait
+        if ($iframe.contents().find('body').children().length > 0) {
+            resolve($iframe.contents().find('body'));
+        } else {
+            $iframe.on('load', () => {
+                resolve($iframe.contents().find('body'));
+            });
+        }
+    });
+});
+
 // ================================================================================
 // ================================================================================
 // ========================= FOR REVIEW / REFACTOR/ REMOVAL =======================
