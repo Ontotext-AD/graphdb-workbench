c8e32a9a480ffbd6ae82a501f7494534cb4b937e
Svilen Velikov <svilen.velikov@sirma.bg>
2019-09-24 19:15:25 +0300

    Remove angular-route and replace with concrete version 1.3.8

--- src/js/angular/core/services.js
+++ src/js/angular/core/services.js
@@ -1,6 +1,6 @@
 import 'angular-animate/angular-animate.min';
 import 'angular-cookies/angular-cookies.min';
-import 'lib/angularjs/1.3.8/angular-route';
+import 'angular-route/angular-route.min';
 import 'lib/angular-local-storage/0.1.5/angular-local-storage.min';
 import 'lib/angular-sanitize/1.3.11/angular-sanitize.min';
 import toastr from 'angular-toastr';
