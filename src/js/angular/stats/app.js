b8a8ba3faa7324cb4174d6099c8d0b8ef2a70d6b
Svilen Velikov <svilen.velikov@sirma.bg>
2019-10-01 14:04:43 +0300

    Fix issues in stats module

--- src/js/angular/stats/app.js
+++ src/js/angular/stats/app.js
@@ -17,8 +17,8 @@ adminInfoApp.config(['$routeProvider', '$locationProvider', '$menuItemsProvider'
             + 'provides an interactive interface to execute the requests.'
         });
 
-        var getDocLink = function () {
-            var pi = $menuItemsProvider.getProductInfo();
+        const getDocLink = function () {
+            const pi = $menuItemsProvider.getProductInfo();
             if (pi.productType) {
                 return 'http://graphdb.ontotext.com/documentation/' + pi.productShortVersion + '/' + pi.productType + '/';
             } else {
@@ -26,8 +26,8 @@ adminInfoApp.config(['$routeProvider', '$locationProvider', '$menuItemsProvider'
             }
         };
 
-        var getDevHubLink = function () {
-            var pi = $menuItemsProvider.getProductInfo();
+        const getDevHubLink = function () {
+            const pi = $menuItemsProvider.getProductInfo();
             if (pi.productType) {
                 return 'http://graphdb.ontotext.com/documentation/' + pi.productShortVersion + '/' + pi.productType + '/devhub/';
             } else {
@@ -35,8 +35,8 @@ adminInfoApp.config(['$routeProvider', '$locationProvider', '$menuItemsProvider'
             }
         };
 
-        var getSupportLink = function () {
-            var pi = $menuItemsProvider.getProductInfo();
+        const getSupportLink = function () {
+            const pi = $menuItemsProvider.getProductInfo();
             if (pi.productType) {
                 return 'http://graphdb.ontotext.com/documentation/' + pi.productShortVersion + '/' + pi.productType + '/support.html';
             } else {
@@ -44,43 +44,43 @@ adminInfoApp.config(['$routeProvider', '$locationProvider', '$menuItemsProvider'
             }
         };
 
-        $menuItemsProvider.addItem({label: 'Setup', href: '#', order: 7, role: 'IS_AUTHENTICATED_FULLY', icon: "icon-settings"});
-        $menuItemsProvider.addItem({label: 'Help', href: '#', order: 8, icon: "icon-help"});
+        $menuItemsProvider.addItem({label: 'Setup', href: '#', order: 7, role: 'IS_AUTHENTICATED_FULLY', icon: 'icon-settings'});
+        $menuItemsProvider.addItem({label: 'Help', href: '#', order: 8, icon: 'icon-help'});
         $menuItemsProvider.addItem({label: 'System information', href: 'sysinfo', order: 50, parent: 'Help', role: 'ROLE_ADMIN'});
         $menuItemsProvider.addItem({label: 'REST API', href: 'webapi', order: 1, parent: 'Help'});
-        $menuItemsProvider.addItem({label: 'Documentation', hrefFun: getDocLink, order: 2, parent: 'Help', icon: "icon-external"});
-        $menuItemsProvider.addItem({label: 'Developer Hub', hrefFun: getDevHubLink, order: 3, parent: 'Help', icon: "icon-external"});
-        $menuItemsProvider.addItem({label: 'Support', hrefFun: getSupportLink, order: 4, parent: 'Help', icon: "icon-external"});
+        $menuItemsProvider.addItem({label: 'Documentation', hrefFun: getDocLink, order: 2, parent: 'Help', icon: 'icon-external'});
+        $menuItemsProvider.addItem({label: 'Developer Hub', hrefFun: getDevHubLink, order: 3, parent: 'Help', icon: 'icon-external'});
+        $menuItemsProvider.addItem({label: 'Support', hrefFun: getSupportLink, order: 4, parent: 'Help', icon: 'icon-external'});
     }]);
 
-adminInfoApp.controller('AdminInfoCtrl', ['$scope', '$http', 'toastr', '$timeout', '$jwtAuth', 'productInfo',
-    function ($scope, $http, toastr, $timeout, $jwtAuth, productInfo) {
+adminInfoApp.controller('AdminInfoCtrl', ['$scope', '$http', 'toastr', '$timeout', '$jwtAuth',
+    function ($scope, $http, toastr, $timeout, $jwtAuth) {
 
-        $http.get("rest/info/data")
-            .success(function (data, status, headers, config) {
+        $http.get('rest/info/data')
+            .success(function (data) {
                 $scope.info = data;
             })
-            .error(function (data, status, headers, config) {
-                let msg = getError(data);
+            .error(function (data) {
+                const msg = getError(data);
                 toastr.error(msg, 'Error');
             });
 
 
         //NONE, IN_PROGRESS, READY
-        $scope.getReportStatus = function (endTimerOnSuccess) {
-            $http.get("rest/report/status")
-                .success(function (data, status, headers, config) {
-                    var statusElements = data.split("|", 3);
+        $scope.getReportStatus = function () {
+            $http.get('rest/report/status')
+                .success(function (data) {
+                    const statusElements = data.split('|', 3);
                     $scope.status = statusElements[0];
                     $scope.timestamp = statusElements[1];
                     $scope.errorMessage = statusElements[2];
 
-                    if ($scope.status == 'IN_PROGRESS') {
+                    if ($scope.status === 'IN_PROGRESS') {
                         $timeout($scope.getReportStatus, 3000);
                     }
                 })
-                .error(function (data, status, headers, config) {
-                    let msg = getError(data);
+                .error(function (data) {
+                    const msg = getError(data);
                     toastr.error(msg, 'Error');
                 });
         };
@@ -88,24 +88,23 @@ adminInfoApp.controller('AdminInfoCtrl', ['$scope', '$http', 'toastr', '$timeout
         $scope.getReportStatus();
 
         $scope.getReport = function () {
-            var url = 'rest/report';
+            let url = 'rest/report';
             if ($jwtAuth.isAuthenticated()) {
-                url = url + "?authToken=" + encodeURIComponent($jwtAuth.getAuthToken());
+                url = url + '?authToken=' + encodeURIComponent($jwtAuth.getAuthToken());
             }
 
             window.open(url);
         };
 
         $scope.makeReport = function () {
-            $http.post("rest/report")
-                .success(function (data, status, headers, config) {
+            $http.post('rest/report')
+                .success(function () {
                     $scope.status = 'IN_PROGRESS';
                     $timeout($scope.getReportStatus, 2000);
                 })
-                .error(function (data, status, headers, config) {
-                    let msg = getError(data);
+                .error(function (data) {
+                    const msg = getError(data);
                     toastr.error(msg, 'Error');
                 });
-        }
-
+        };
     }]);
