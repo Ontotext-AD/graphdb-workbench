5a25d9b84339721f7f9f0ff6c6622b2010a5b723
Svilen Velikov <svilen.velikov@sirma.bg>
2019-10-22 18:45:40 +0300

    Refactored monitor queries view pages to work separately.

--- src/js/angular/queries/app.js
+++ src/js/angular/queries/app.js
@@ -1,13 +1,11 @@
 import 'angular/core/services';
 import 'angular/core/directives';
 import 'angular/queries/controllers';
-import 'angular/queries/directives';
 
 const modules = [
     'toastr',
     'ui.bootstrap',
     'graphdb.framework.jmx.queries.controllers',
-    'graphdb.framework.jmx.queries.directives',
     'graphdb.framework.core.directives'
 ];
 
