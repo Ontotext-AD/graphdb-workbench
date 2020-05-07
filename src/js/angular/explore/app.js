d63d6625f51212b8e96832936f8a404eec96bebc
teodossi.dossev@ontotext.com </d/dP.djd55>
2020-05-07 09:12:17 +0300

    GDB-4400 Provides font auto resize to match parent directive
     The directive manipulates the font size of DOM elements that they fill the width of a parent element. Desired max and min font size can be passed as argument, or get from source. Works with window resize, dynamic recalculates font size on parent changes. Line heights and display property are preserved.
    
     Related to GDB-4400 Resource view: Reduce the font of nested triples in the resource view

--- src/js/angular/explore/app.js
+++ src/js/angular/explore/app.js
@@ -5,7 +5,7 @@ import 'angular/explore/controllers';
 import 'angular/explore/directives';
 import 'angular-xeditable/dist/js/xeditable.min';
 import 'lib/angucomplete-alt/angucomplete-alt-onto.min';
-import 'angular/explore/fit-text.directive';
+import 'angular/core/directives/fit-text.directive';
 
 const modules = [
     'ngRoute',
@@ -15,7 +15,7 @@ const modules = [
     'graphdb.framework.explore.services',
     'graphdb.framework.explore.controllers',
     'graphdb.framework.explore.directives',
-    'graphdb.framework.explore.directives.fittext'
+    'graphdb.framework.core.directives.fittext'
 ];
 
 angular.module('graphdb.framework.explore', modules);
