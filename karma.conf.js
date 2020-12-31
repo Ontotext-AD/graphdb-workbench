ca403cb5806490b8230dc1edd7659b12e2703e95
Pavel Mihaylov <pavel@ontotext.com>
2020-12-31 14:43:28 +0200

    GDB-5344: Various OpenID fixes
    - Extra space in OpenID scope
    - Extra & in URL parameters
    - Sending some unencoded parameters
    - Success callback not called correctly on login with the implicit callback
    - Possible errors in corner cases
    - Replaced window with $window for better testability
    - Unit tests that should cover almost everything

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'jasmine-matchers'],
 
     client: {
         jasmine: {
