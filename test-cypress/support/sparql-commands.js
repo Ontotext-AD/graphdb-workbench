138b5d144ddc17a9298c1bf6a73677a7e8c0a2a2
sava <sava.savov@ontotext.com>
2021-04-09 08:20:23 +0300

    GDB-5576 Removed absolute parameter

--- test-cypress/support/sparql-commands.js
+++ test-cypress/support/sparql-commands.js
@@ -2,7 +2,7 @@ Cypress.Commands.add('pasteQuery', (query) => {
     clearQuery();
     // Using force because the textarea is not visible
     getQueryTextArea().invoke('val', query).trigger('change', {force: true});
-    waitUntilQueryIsVisible(query);
+    waitUntilQueryIsVisible();
 });
 
 Cypress.Commands.add('executeQuery', () => {
