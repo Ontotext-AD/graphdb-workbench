4c1a0a20129255a0179537f339f16623a4698993
sava <sava.savov@ontotext.com>
2021-06-01 14:14:25 +0300

    GDB-5762 Changed repositories.spec.js dropdown expectations

--- test-cypress/integration/repository/repositories.spec.js
+++ test-cypress/integration/repository/repositories.spec.js
@@ -129,7 +129,7 @@ describe('Repositories', () => {
         getRepositoriesDropdown()
             .click().within(() => {
             cy.get('.dropdown-menu .dropdown-item')
-                .should('not.exist');
+                .should('not.be.visible');
         })
     });
 
@@ -327,7 +327,7 @@ describe('Repositories', () => {
         // Check the repo has been deselected and is not present in the repo dropdown menu
         getRepositoriesDropdown().click().within(() => {
             cy.get('#btnReposGroup').should('not.contain', repositoryId);
-            cy.get('.dropdown-menu .dropdown-item').should('not.exist');
+            cy.get('.dropdown-menu .dropdown-item').should('not.be.visible');
         });
     });
 
