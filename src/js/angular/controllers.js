088c08f142c1b8c5d61262585fb920bfd7dc9a28
vasilnv <vasil_vasilev1999@abv.bg>
2021-07-27 11:28:34 +0300

    Revert "GDB-5962 Disable autocomplete check for FedX repos when getting active repository size"
    This reverts commit bae4b57b

--- src/js/angular/controllers.js
+++ src/js/angular/controllers.js
@@ -63,15 +63,10 @@ function homeCtrl($scope, $rootScope, $http, $repositories, $jwtAuth, Autocomple
     }
 
     function checkAutocompleteStatus() {
-        let activeRepo = $repositories.repositories.filter(el => el.id === $repositories.getActiveRepository())[0];
-        if (activeRepo.type === 'fedx') {
-            $scope.getActiveRepositorySize();
-        } else {
-            $scope.getAutocompletePromise = AutocompleteRestService.checkAutocompleteStatus()
-                .success(function () {
-                    $scope.getActiveRepositorySize();
-                });
-        }
+        $scope.getAutocompletePromise = AutocompleteRestService.checkAutocompleteStatus()
+            .success(function () {
+                $scope.getActiveRepositorySize();
+            });
     }
 
     $scope.$on('autocompleteStatus', function() {
