55a0883292482b97e512a9670bea9850c689c1b8
sukhumi <lubenkaraslavov@abv.bg>
2021-11-04 12:00:03 +0200

    changed openInNewWindowTab to keep the context path

--- src/js/angular/core/directives.js
+++ src/js/angular/core/directives.js
@@ -367,7 +367,7 @@ function searchResourceInput($location, toastr, ClassInstanceDetailsService, Aut
 
             const openInNewWindowTab = function (visual, params) {
                 const view = visual ? 'graphs-visualizations' : 'resource';
-                window.open(`/${view}?uri=${encodeURIComponent(params.uri)}`);
+                window.open(`${view}?uri=${encodeURIComponent(params.uri)}`);
             };
 
             if (angular.isUndefined(attrs.$attr.textCallback)) {
