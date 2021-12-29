6812b66490b22f7b1ce1fc7532f09a0630dfed0e
Diana Ivanova <diana.ivanova@ontotext.com>
2021-12-29 11:50:34 +0200

    GDB-6448 Warn users when they unset the password A modal window is shown when the user clicks Save at "My settings" if he had checked Unset password checkbox. The modal warns the user about the consequences of unsetting the password.

--- src/js/angular/security/controllers.js
+++ src/js/angular/security/controllers.js
@@ -661,8 +661,8 @@ securityCtrl.controller('RolesMappingController', ['$scope', 'toastr', 'Security
     });
 }]);
 
-securityCtrl.controller('ChangeUserPasswordSettingsCtrl', ['$scope', 'toastr', '$window', '$timeout', '$jwtAuth', '$rootScope', '$controller', 'SecurityRestService',
-    function ($scope, toastr, $window, $timeout, $jwtAuth, $rootScope, $controller, SecurityRestService) {
+securityCtrl.controller('ChangeUserPasswordSettingsCtrl', ['$scope', 'toastr', '$window', '$timeout', '$jwtAuth', '$rootScope', '$controller', 'SecurityRestService', 'ModalService',
+    function ($scope, toastr, $window, $timeout, $jwtAuth, $rootScope, $controller, SecurityRestService, ModalService) {
 
         angular.extend(this, $controller('CommonUserCtrl', {$scope: $scope}));
 
@@ -731,8 +731,19 @@ securityCtrl.controller('ChangeUserPasswordSettingsCtrl', ['$scope', 'toastr', '
         $scope.loader = false;
 
         $scope.submit = function () {
-            $scope.updateUser();
-        };
+            if ($scope.noPassword) {
+                ModalService.openSimpleModal({
+                    title: 'Save user settings',
+                    message: 'If you unset the password and then enable security, that user will not be ' +
+                        'able to log into GraphDB through the workbench.',
+                    warning: true
+                }).result.then(function () {
+                    $scope.updateUser();
+                });
+            } else {
+                $scope.updateUser();
+            }
+        }
 
         $scope.updateUserHttp = function () {
             $scope.loader = true;
