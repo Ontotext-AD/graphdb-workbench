f6a0df6b276561afaef812a024e3e75bd197566b
Diana Ivanova <diana.ivanova@ontotext.com>
2022-02-04 10:44:27 +0200

    GDB-6337-As-a-user-I-need-to-be-able-to-start/stop/list-plugins-from-the-Workbench Add plugin management from the workbench. All plugins can be enabled/disabled, except plugin control, because it cannot be disabled. Added cypress test.

---
+++ src/js/angular/plugins/controllers.js
@@ -0,0 +1,86 @@
+import 'angular/rest/plugins.rest.service';
+
+const modules = [
+    'graphdb.framework.rest.plugins.service'
+];
+
+angular
+    .module('graphdb.framework.plugins.controllers', modules)
+    .controller('PluginsCtrl', PluginsCtrl);
+
+PluginsCtrl.$inject = ['$scope', '$interval', '$repositories', '$licenseService', '$modal', '$timeout', 'PluginsRestService'];
+
+function PluginsCtrl($scope, $interval, $repositories, $licenseService, $modal, $timeout, PluginsRestService) {
+
+    $scope.setPluginIsActive = function (isPluginActive) {
+        $scope.pluginIsActive = isPluginActive;
+    };
+    const getPlugins = function () {
+        PluginsRestService.getPlugins($scope.getActiveRepository())
+            .success(function (data) {
+                $scope.plugins = $scope.buildPluginsArray(data.results.bindings);
+                $scope.setLoader(false);
+            }).error(function (data) {
+            toastr.error(getError(data));
+        });
+    };
+
+    const init = function () {
+        if (!$licenseService.isLicenseValid() ||
+            !$repositories.getActiveRepository() ||
+            $repositories.isActiveRepoOntopType() ||
+            $repositories.isActiveRepoFedXType()) {
+            return;
+        }
+        getPlugins();
+    };
+
+    $scope.buildPluginsArray = function (plugins) {
+        return plugins.map((plugin) => ({
+            name: plugin.s.value,
+            enabled: plugin.o.value === 'true'
+        })).filter(function (plugin) {
+            return plugin.name !== 'plugincontrol';
+        }).sort(function (a, b) {
+            const textA = a.name.toLowerCase();
+            const textB = b.name.toLowerCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        });
+    };
+
+    $scope.togglePlugin = function (pluginName, enabled) {
+        const repoId = $scope.getActiveRepository();
+        $scope.setLoader(true, enabled ? `Enabling ${pluginName}` : `Disabling ${pluginName}`);
+        PluginsRestService.togglePlugin(repoId, enabled, pluginName).success(function () {
+            getPlugins();
+        });
+    };
+    // this is used when repository is changed from the upper menu to refresh the plugins for it.
+    $scope.$on('repositoryIsSet', function () {
+        if (!$licenseService.isLicenseValid() ||
+            !$repositories.getActiveRepository() ||
+            $repositories.isActiveRepoOntopType() ||
+            $repositories.isActiveRepoFedXType()) {
+            return;
+        }
+        getPlugins();
+    });
+
+    $scope.getLoaderMessage = function () {
+        return $scope.loaderMessage || 'Loading...';
+    };
+
+    $scope.setLoader = function (loader, message) {
+        $timeout.cancel($scope.loaderTimeout);
+        if (loader) {
+            $scope.loaderTimeout = $timeout(function () {
+                $scope.loader = loader;
+                $scope.loaderMessage = message;
+            }, 300);
+        } else {
+            $scope.loader = false;
+        }
+    };
+
+    init();
+}
---
+++ test-cypress/integration/setup/plugins.spec.js
@@ -0,0 +1,67 @@
+describe('Plugins', () => {
+
+    let repositoryId;
+
+    function createRepository() {
+        repositoryId = 'plugin-' + Date.now();
+        cy.createRepository({id: repositoryId});
+        cy.presetRepository(repositoryId);
+        cy.initializeRepository(repositoryId);
+    }
+
+    function waitUntilPluginsPageIsLoaded() {
+        // Workbench loading screen should not be visible
+        cy.get('.ot-splash').should('not.be.visible');
+
+        // No active loader
+        cy.get('.ot-loader').should('not.exist');
+    }
+
+    beforeEach(() => {
+        createRepository();
+        cy.visit('/plugins');
+        cy.window();
+        waitUntilPluginsPageIsLoaded();
+    });
+
+    afterEach(() => {
+        cy.visit('/plugins');
+        cy.window();
+        // re-enable historyplugin
+        getPluginsHeader()
+            .should('be.visible').parent().within(() => {
+            cy.get('.switch').then((elem) => {
+                var value = elem.val();
+                if (!value.checked) {
+                    getPluginsSwitch().click();
+                }
+            });
+        });
+        cy.deleteRepository(repositoryId);
+    });
+
+    it('should allow to enable and disable the plugins', () => {
+        // Verify initial status is ON
+        getPluginsHeader()
+            .should('be.visible').parent().within(() => {
+            cy.get('.tag-primary')
+                .contains('ON');
+
+            cy.get('.switch').should('be.checked');
+            getPluginsSwitch().click();
+        });
+
+        getPluginsHeader()
+            .should('be.visible').parent().within(() => {
+            cy.get('.switch').should('not.be.checked');
+        });
+    });
+
+    function getPluginsHeader() {
+        return cy.get('.plugins-header').contains("history");
+    }
+
+    function getPluginsSwitch() {
+        return cy.get('.plugins-switch');
+    }
+});
