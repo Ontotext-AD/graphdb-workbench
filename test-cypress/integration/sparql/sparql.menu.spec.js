5ff080675861826087e371640d6dc6975b160260
Diana Ivanova <diana.ivanova@ontotext.com>
2022-02-10 14:41:57 +0200

    GDB-6337 As-a-user-I-need-to-be-able-to-start/stop/list-plugins-from-the-Workbench sparql main menu revert fix

--- test-cypress/integration/sparql/sparql.menu.spec.js
+++ test-cypress/integration/sparql/sparql.menu.spec.js
@@ -436,19 +436,11 @@ describe('SPARQL screen validation', () => {
 
             // Uncheck ‘Include inferred’
             cy.waitUntil(() =>
-                getInferenceButton()
-                    .within(infBtn =>
-                    {   cy.get('.icon-inferred-on');
-                        console.log(infBtn);
-                        infBtn && cy.wrap(infBtn).click();
-                    }
-                    ).then(() => {
-                getInferenceButton()
-                    .within(button => {
-                        cy.get('.icon-inferred-off')
-                        cy.wrap(button).should('be.visible')
-                    });
-            }))
+                getInferenceButton().find('.icon-inferred-on')
+                    .then(infBtn => infBtn && cy.wrap(infBtn).click()))
+                .then(() =>
+                    cy.get('.icon-inferred-off').should('be.visible'));
+
             // Confirm that only inferred statements (only 2) are available
             executeQuery();
             verifyResultsPageLength(2);
