89933d4b508531239b7d35376b05cf47db099545
Diana Ivanova <diana.ivanova@ontotext.com>
2022-02-11 11:23:03 +0200

    GDB-6337-As-a-user-I-need-to-be-able-to-start/stop/list-plugins-from-the-Workbench return changes made to main.menu.spec.js

--- test-cypress/integration/sparql/main.menu.spec.js
+++ test-cypress/integration/sparql/main.menu.spec.js
@@ -169,9 +169,7 @@ describe('Main menu tests', function () {
             let menuVisibilityCheck = menu.visible ? 'be.visible' : 'not.be.visible';
             // Verify that main menu items are present and their visibility
             cy.get('.main-menu .menu-element-root').eq(menuIndex + 1).as('menu')
-                .should(menuVisibilityCheck);
-                cy.get('@menu').within(() =>
-                cy.get('.menu-item').contains(menu.name));
+                .should(menuVisibilityCheck).and('contain', menu.name);
 
             // Verify submenu items and their visibility when the main menu is not opened
             (menu.submenu || []).forEach((submenu, submenuIndex) => {
