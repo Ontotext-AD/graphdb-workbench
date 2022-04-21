ddc574806f742400a3f05e27a762d92d59e1517e
Diana Ivanova <diana.ivanova@ontotext.com>
2022-04-21 14:37:49 +0300

    GDB-6750-Simultaneous-start/stop-of-plugins-from-2-users-causes-infinite-loading - remove compare objects because useless here

--- src/js/angular/plugins/controllers.js
+++ src/js/angular/plugins/controllers.js
@@ -23,11 +23,6 @@ function PluginsCtrl($scope, $interval, toastr, $repositories, $licenseService, 
     $scope.setPluginIsActive = function (isPluginActive) {
         $scope.pluginIsActive = isPluginActive;
     };
-    const objectsEqual = (o1, o2) =>
-        typeof o1 === 'object' && Object.keys(o1).length > 0
-            ? Object.keys(o1).length === Object.keys(o2).length
-            && Object.keys(o1).every(p => objectsEqual(o1[p], o2[p]))
-            : o1 === o2;
 
     const getPlugins = function () {
         initPlugins();
