a0b5135e19dac77d8321a8652975f9549d73d487
sava-savov-ontotext <sava.savov@ontotext.com>
2022-05-16 17:19:06 +0300

    GDB-6329 Update package-lock.json file and removed on load event from rdf-class-hierarchy.directive.js, because latter has concurrency issues with the

--- src/js/angular/graphexplore/directives/rdf-class-hierarchy.directive.js
+++ src/js/angular/graphexplore/directives/rdf-class-hierarchy.directive.js
@@ -34,37 +34,22 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
         link: linkFunc
     };
 
-    function linkFunc(scope, element, attrs) {
-        window.addEventListener('load', renderCirclePacking);
-
-        scope.$on('$destroy', function (event) {
-            window.removeEventListener('load', renderCirclePacking);
-        });
+    function linkFunc(scope) {
+        renderCirclePacking(scope);
     }
 
-    function renderCirclePacking(scope, element) {
+    function renderCirclePacking(scope) {
 
         var width = 800,
             height = 800,
             diameter = height,
-            margin = 20,
-            node,
-            root;
+            margin = 20;
 
         var pack = d3.layout.pack()
             .size([diameter - margin, diameter - margin])
             .value(function (d) {
                 return d.size;
             });
-
-        var colors = [
-            "hsl(33, 75%, 75%)",
-            "hsl(213, 75%, 75%)",
-            "hsl(333, 75%, 75%)",
-            "hsl(153, 75%, 75%)",
-            "hsl(273, 75%, 75%)",
-            "hsl(93, 75%, 75%)"
-        ];
 
         var color = d3.scale.linear()
             .domain([0, 4])
@@ -130,7 +115,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
             $rootScope.hierarchyError = false;
             const selGraphFromCache = LocalStorageAdapter.get(`classHierarchy-selectedGraph-${$repositories.getActiveRepository()}`);
             GraphDataRestService.getClassHierarchyData(selGraphFromCache !== null ? selGraphFromCache.contextID.uri : "")
-                .success(function (response, status, headers) {
+                .success(function (response, status) {
                     $rootScope.loader = false;
                     if (status === 207) {
                         toastr.warning($translate.instant('graphexplore.update.diagram'), $translate.instant('graphexplore.repository.data.changed'));
@@ -183,7 +168,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
             var tip = d3tip()
                 .attr('class', 'd3-tip')
                 //.offset([-10, 0])
-                .customPosition(function (d) {
+                .customPosition(function () {
                     return {
                         top: 'inherit',
                         bottom: ($window.innerHeight - d3.event.clientY) + 'px',
@@ -201,7 +186,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
                 .enter().append("g");
 
             if (config.doFade) {
-                circleGroup.each(function (d) {
+                circleGroup.each(function () {
                     D3.Transition.fadeIn(d3.select(this), 550);
                 });
             }
@@ -265,8 +250,6 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
             }
 
             var clickCancel = D3.Click.delayDblClick();
-            var parentCircles = d3.selectAll(".parent, .topLevelParent, .child, .root")
-                .call(clickCancel);
 
             doFocus = function (obj) {
                 zoom(obj);
@@ -279,7 +262,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
                     showClassInfoPanel();
 
                     // FIXME: is it ok to bind this event handler every time and not just once?
-                    scope.$on('sidePanelClosed', function (event) {
+                    scope.$on('sidePanelClosed', function () {
                         d3.selectAll(".selected").classed("selected", obj.selected = false);
                     });
                 }
@@ -443,7 +426,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
             });
 
             if (config.doFade && !config.keepPrevState) {
-                text.each(function (d) {
+                text.each(function () {
                     D3.Transition.fadeIn(d3.select(this), 550);
                 });
             }
@@ -452,7 +435,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
                 function focusOnCurrentClass(focusHistoryId) {
                     if (focusHistoryId) {
                         circle.each(function (d) {
-                            if (focusHistoryId == d.id) {
+                            if (focusHistoryId === d.id) {
                                 doFocus(d);
                             }
                         });
@@ -614,7 +597,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
                     // the fancy version for faster browsers
                     var transition = d3.transition()
                         .duration(ZOOM_DURATION)
-                        .tween("zoom", function (d) {
+                        .tween("zoom", function () {
                             var i = d3.interpolateZoom(view, [focus.x, focus.y, adjustRadius(focus.r)]);
                             return function (t) {
                                 zoomTo(i(t));
@@ -821,7 +804,7 @@ function classHierarchyDirective($rootScope, $location, GraphDataRestService, $w
                 // hit enter again on the already selected class in order again react and zoom to it again
                 // if we are not at the moment
                 var currentSearchedClass;
-                onEnterClassSearchBroadcast = scope.$on('onEnterKeypressSearchAction', function (event) {
+                onEnterClassSearchBroadcast = scope.$on('onEnterKeypressSearchAction', function () {
                     if (currentSearchedClass) {
                         doFocus(currentSearchedClass);
                     }
