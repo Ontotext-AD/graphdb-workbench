28b738237afcd1e2b8e9999ff0b560f5ba2ee42a
emiliyan <emiliyan.sinigerov@ontotext.com>
2022-06-02 12:56:51 +0300

    Increase seconds

--- src/js/angular/plugins/controllers.js
+++ src/js/angular/plugins/controllers.js
@@ -28,7 +28,7 @@ function PluginsCtrl($scope, $interval, toastr, $repositories, $licenseService, 
         initPlugins();
         timer = $interval(function () {
             initPlugins();
-        }, 10000);
+        }, 100000);
     };
     const initPlugins = function () {
             PluginsRestService.getPlugins($scope.getActiveRepository())
