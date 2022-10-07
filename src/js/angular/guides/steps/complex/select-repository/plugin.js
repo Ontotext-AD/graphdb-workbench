36e8dbd9a8c824b73e81c323a56f5ead0a340a8d
boyan.tonchev <boyan.tonchev@ontotext.com>
2022-10-07 09:24:55 +0300

    GDB-7450: User Guide crashes on repo select step
    Added logic to prevent closing select repository menu when click somewhere on the page when a guide is active.

--- src/js/angular/guides/steps/complex/select-repository/plugin.js
+++ src/js/angular/guides/steps/complex/select-repository/plugin.js
@@ -5,6 +5,7 @@ PluginRegistry.add('guide.step', [
             const GuideUtils = services.GuideUtils;
             options.mainAction = 'select-repository';
 
+            let repositoryButtonSelector = GuideUtils.getGuideElementSelector(`repository-${options.repositoryId}-button`);
             return [{
                 guideBlockName: 'clickable-element',
                 options: angular.extend({}, {
@@ -17,8 +18,17 @@ PluginRegistry.add('guide.step', [
                 guideBlockName: 'clickable-element',
                 options: angular.extend({}, {
                     content: 'guide.step_plugin.select-repository.content',
-                    elementSelector: GuideUtils.getGuideElementSelector(`repository-${options.repositoryId}-button`),
-                    onNextClick: (guide) => GuideUtils.clickOnGuideElement(`repository-${options.repositoryId}-button`)().then(() => guide.next())
+                    elementSelector: repositoryButtonSelector,
+                    beforeShowPromise: () => new Promise(function (resolve, reject) {
+                        services.GuideUtils.waitFor(repositoryButtonSelector, 1)
+                            .then(() => resolve())
+                            .catch((error) => {
+                                services.toastr.error(services.$translate.instant('guide.unexpected.error.message'));
+                                reject(error);
+                            });
+                    }),
+                    onNextClick: (guide) => GuideUtils.clickOnGuideElement(`repository-${options.repositoryId}-button`)().then(() => guide.next()),
+                    canBePaused: false
                 }, options)
             }
             ];
