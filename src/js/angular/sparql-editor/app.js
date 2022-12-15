cf98e548c0790e4362e8aa5860475813e339ffe5
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2022-12-15 14:09:15 +0200

    GDB-7680: Integrate the ontotext-yasgui component in the workbench (#823)
    * GDB-7680: Integrate the ontotext-yasgui component in the workbench
    
    ## What
     * The "ontotext-yasgui-web-component" is integrated in workbench.
    
    ## Why
     * As developers we need a page with integrated "ontotext-yasgui-web-component" to start development.
    
    ## How
     * Added "ontotext-yasgui-web-component" and "ng-custom-element" modules as project dependence.
     * A new page "sparql-editor" is created and added a menu to open it.
    
    * Removes hard coded repository name.
    
    * Added config initialization when instantiating the page controller.
    
    * Fixes sparql-editor test.

---
+++ src/js/angular/sparql-editor/app.js
@@ -0,0 +1,8 @@
+import 'angular/sparql-editor/controllers';
+
+const modules = [
+    'ui.bootstrap',
+    'graphdb.framework.sparql-editor.controllers'
+];
+
+angular.module('graphdb.framework.sparql-editor', modules);
