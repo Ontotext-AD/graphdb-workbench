60c1ae4c9aa902e658d98c4abc16a6ccb2f1b9da
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2022-12-15 14:09:15 +0200

    GDB-7680: Integrate the ontotext-yasgui component in the workbench (#823)
    * GDB-7680: Integrate the ontotext-yasgui component in the workbench
    
    ## What
     * The "ontotext-yasgui-web-component" is integrated in workbench.
    
    ## Why
     * As developers we need a page with integrated "ontotext-yasgui-web-component" to start development.
    
    ## How
     * Added "ontotext-yasgui-web-component" and "ng-custom-element" modules as project dependence.
     * A new page "sparql-editor" is created and added a menu to open it.
    
    * Removes hard coded repository name.
    
    * Added config initialization when instantiating the page controller.
    
    * Fixes sparql-editor test.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import 'angular/core/interceptors/unauthorized.interceptor';
 import 'angular/core/directives/rdfresourcesearch/rdf-resource-search.directive';
 import 'angular/core/directives/languageselector/language-selector.directive';
 import 'angular/core/directives/angulartooltips/angular-tooltips.js';
+import {defineCustomElementOntotextYasgui} from 'ontotext-yasgui-web-component/dist/components/index';
 
 // $translate.instant converts <b> from strings to &lt;b&gt
 // and $sce.trustAsHtml could not recognise that this is valid html
@@ -28,7 +29,8 @@ const modules = [
     'graphdb.framework.core.directives.rdfresourcesearch.rdfresourcesearch',
     'graphdb.framework.core.directives.languageselector.languageselector',
     'graphdb.framework.core.directives.angular-tooltips',
-    'graphdb.framework.guides.services'
+    'graphdb.framework.guides.services',
+    'ngCustomElement'
 ];
 
 const providers = [
@@ -44,6 +46,9 @@ const providers = [
 ];
 
 const moduleDefinition = function (productInfo) {
+
+    defineCustomElementOntotextYasgui();
+
     const workbench = angular.module('graphdb.workbench', modules);
 
     workbench.config([...providers,
