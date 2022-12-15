3076a17a67deb0ff89a50c0a473181a12c7e32d4
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2022-12-15 14:09:15 +0200

    GDB-7680: Integrate the ontotext-yasgui component in the workbench (#823)
    * GDB-7680: Integrate the ontotext-yasgui component in the workbench
    
    ## What
     * The "ontotext-yasgui-web-component" is integrated in workbench.
    
    ## Why
     * As developers we need a page with integrated "ontotext-yasgui-web-component" to start development.
    
    ## How
     * Added "ontotext-yasgui-web-component" and "ng-custom-element" modules as project dependence.
     * A new page "sparql-editor" is created and added a menu to open it.
    
    * Removes hard coded repository name.
    
    * Added config initialization when instantiating the page controller.
    
    * Fixes sparql-editor test.

--- test-cypress/integration/sparql-editor/sparql-editor.spec.js
+++ test-cypress/integration/sparql-editor/sparql-editor.spec.js
@@ -1,11 +1,10 @@
 import {SparqlEditorSteps} from "../../steps/sparql-editor-steps";
-import {YasguiSteps} from "../../steps/yasgui/yasgui-steps";
+import {YasguiSteps} from "../../steps/yasgui-steps";
 
 describe('Default view', () => {
 
     beforeEach(() => {
         const repositoryId = 'sparql-editor-' + Date.now();
-        cy.intercept('GET', '/rest/monitor/query/count', {body: 0});
         cy.createRepository({id: repositoryId});
         cy.presetRepository(repositoryId);
         cy.intercept('/repositories/test-repo', {fixture: '/graphql-editor/default-query-response.json'}).as('getGuides');
