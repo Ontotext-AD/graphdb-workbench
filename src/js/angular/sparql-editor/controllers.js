b769ce5007382b83ea9164f3ec7be676b2066c14
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2022-12-15 14:09:15 +0200

    GDB-7680: Integrate the ontotext-yasgui component in the workbench (#823)
    * GDB-7680: Integrate the ontotext-yasgui component in the workbench
    
    ## What
     * The "ontotext-yasgui-web-component" is integrated in workbench.
    
    ## Why
     * As developers we need a page with integrated "ontotext-yasgui-web-component" to start development.
    
    ## How
     * Added "ontotext-yasgui-web-component" and "ng-custom-element" modules as project dependence.
     * A new page "sparql-editor" is created and added a menu to open it.
    
    * Removes hard coded repository name.
    
    * Added config initialization when instantiating the page controller.
    
    * Fixes sparql-editor test.

--- src/js/angular/sparql-editor/controllers.js
+++ src/js/angular/sparql-editor/controllers.js
@@ -1,173 +1,37 @@
-import {merge} from "lodash";
-import {
-    savedQueriesResponseMapper,
-    queryPayloadFromEvent,
-    savedQueryResponseMapper, buildQueryModel
-} from "../rest/mappers/saved-query-mapper";
-import {RouteConstants} from "../utils/route-constants";
-
 angular
     .module('graphdb.framework.sparql-editor.controllers', ['ui.bootstrap'])
     .controller('SparqlEditorCtrl', SparqlEditorCtrl);
 
-SparqlEditorCtrl.$inject = ['$scope', '$location', '$repositories', 'toastr', '$translate', 'SparqlRestService', 'ShareQueryLinkService', '$languageService'];
+SparqlEditorCtrl.$inject = ['$scope', '$repositories'];
 
-function SparqlEditorCtrl($scope, $location, $repositories, toastr, $translate, SparqlRestService, ShareQueryLinkService, $languageService) {
-    const ontoElement = document.querySelector('ontotext-yasgui');
+function SparqlEditorCtrl($scope, $repositories) {
 
-    this.repository = '';
+    this.repository = "";
 
     $scope.config = undefined;
-    $scope.savedQueryConfig = undefined;
 
-    $scope.language = $languageService.getLanguage();
+    function init() {
+        $scope.configChanged();
+    }
 
-    $scope.configChanged = () => {
-        const activeRepository = $repositories.getActiveRepository();
-        if (activeRepository) {
-            $scope.config = {
-                endpoint: `/repositories/${activeRepository}`,
-                showToolbar: true,
-                componentId: 'graphdb-workbench-sparql-editor'
-            };
-        }
-    };
-
-    $scope.queryExecuted = (query) => {
+    $scope.queryExecuted = function (query) {
         console.log(query.detail);
     };
 
-    $scope.createSavedQuery = (event) => {
-        const payload = queryPayloadFromEvent(event);
-        SparqlRestService.addNewSavedQuery(payload).then(() => queryCreatedHandler(payload)).catch(querySaveErrorHandler);
-    };
-
-    $scope.updateSavedQuery = (event) => {
-        const payload = queryPayloadFromEvent(event);
-        SparqlRestService.editSavedQuery(payload).then(() => queryUpdatedHandler(payload)).catch(querySaveErrorHandler);
-    };
-
-    $scope.deleteSavedQuery = (event) => {
-        const payload = queryPayloadFromEvent(event);
-        SparqlRestService.deleteSavedQuery(payload.name).then(() => {
-            toastr.success($translate.instant('query.editor.delete.saved.query.success.msg', {savedQueryName: payload.name}));
-        }).catch((err) => {
-            const msg = getError(err);
-            toastr.error(msg, $translate.instant('query.editor.delete.saved.query.error'));
-        });
-    };
-
-    $scope.shareSavedQuery = (event) => {
-        const payload = queryPayloadFromEvent(event);
-        $scope.savedQueryConfig = {
-            shareQueryLink: ShareQueryLinkService.createShareSavedQueryLink(payload.name, payload.owner)
-        };
-    };
-
-    $scope.shareQuery = (event) => {
-        const payload = queryPayloadFromEvent(event);
-        $scope.savedQueryConfig = {
-            shareQueryLink: ShareQueryLinkService.createShareQueryLink(payload)
-        };
-    };
-
-    $scope.queryShareLinkCopied = () => {
-        toastr.success($translate.instant('modal.ctr.copy.url.success'));
-    };
-
-    $scope.loadSavedQueries = () => {
-        SparqlRestService.getSavedQueries().then((res) => {
-            const savedQueries = savedQueriesResponseMapper(res);
-            $scope.savedQueryConfig = {
-                savedQueries: savedQueries
+    $scope.configChanged = function () {
+        const activeRepository = $repositories.getActiveRepository();
+        if (activeRepository) {
+            $scope.config = {
+                yasguiConfig: {
+                    requestConfig: {
+                        endpoint: `/repositories/${activeRepository}`
+                    }
+                }
             };
-        }).catch((err) => {
-            const msg = getError(err);
-            toastr.error(msg, $translate.instant('query.editor.get.saved.queries.error'));
-        });
-    };
-
-    $scope.$on('repositoryIsSet', $scope.configChanged);
-    $scope.$on('language-changed', function (event, args) {
-        $scope.language = args.locale;
-    });
-
-    // private functions
-
-    const initTabFromSavedQuery = (queryParams) => {
-        const savedQueryName = queryParams[RouteConstants.savedQueryName];
-        const savedQueryOwner = queryParams[RouteConstants.savedQueryOwner];
-        SparqlRestService.getSavedQuery(savedQueryName, savedQueryOwner).then((res) => {
-            const savedQuery = savedQueryResponseMapper(res);
-            // * Check if there is an open tab with the same query already. If there is one, then open it.
-            // * Otherwise open a new tab and load the query in the editor.
-            // TODO: Before opening a new tab: check if there is a running query or update and prevent opening it.
-            // Same as the above should be checked on tab switching for existing tab too.
-            ontoElement.openTab(savedQuery);
-        }).catch((err) => {
-            toastr.error($translate.instant('query.editor.missing.saved.query.data.error', {
-                savedQueryName: savedQueryName,
-                error: getError(err)
-            }));
-        });
-    };
-
-    const initTabFromSharedQuery = (queryParams) => {
-        const queryName = queryParams[RouteConstants.name];
-        const query = queryParams[RouteConstants.query];
-        const queryOwner = queryParams[RouteConstants.owner];
-        const sharedQueryModel = buildQueryModel(query, queryName, queryOwner, true);
-        // * Check if there is an open tab with the same query already. If there is one, then open it.
-        // * Otherwise open a new tab and load the query in the editor.
-        // TODO: Before opening a new tab: check if there is a running query or update and prevent opening it.
-        // Same as the above should be checked on tab switching for existing tab too.
-        ontoElement.openTab(sharedQueryModel);
-    };
-
-    const initViewFromUrlParams = () => {
-        const queryParams = $location.search();
-        if (queryParams.hasOwnProperty(RouteConstants.savedQueryName)) {
-            // init new tab from shared saved query link
-            initTabFromSavedQuery(queryParams);
-        } else if (queryParams.hasOwnProperty(RouteConstants.query)) {
-            // init new tab from shared query link
-            initTabFromSharedQuery(queryParams);
         }
     };
 
-    const querySavedHandler = (successMessage) => {
-        toastr.success(successMessage);
-        $scope.savedQueryConfig = merge({}, $scope.savedQueryConfig, {
-            saveSuccess: true
-        });
-    };
-
-    const queryCreatedHandler = (payload) => {
-        return querySavedHandler($translate.instant('query.editor.save.saved.query.success.msg', {name: payload.name}));
-    };
-
-    const queryUpdatedHandler = (payload) => {
-        return querySavedHandler($translate.instant('query.editor.edit.saved.query.success.msg', {name: payload.name}));
-    };
-
-    const querySaveErrorHandler = (err) => {
-        const msg = getError(err);
-        const errorMessage = $translate.instant('query.editor.create.saved.query.error');
-        toastr.error(msg, errorMessage);
-        $scope.savedQueryConfig = merge({}, $scope.savedQueryConfig, {
-            saveSuccess: false,
-            errorMessage: [errorMessage]
-        });
-    };
-
-    // Initialization and bootstrap
-    function init() {
-        $scope.configChanged();
-        // check is there is a savedquery or query url parameter and init the editor
-        initViewFromUrlParams();
-        // on repo change do the same as above
-        // focus on the active editor on init
-    }
+    $scope.$on('repositoryIsSet', $scope.configChanged);
 
     init();
 }
