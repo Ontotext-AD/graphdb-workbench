c8f454dc70f8c3eb334114add544b5f6be233ac7
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-01-12 15:42:08 +0200

    GDB-7783 integrate the save query action (#833)
    ## What
    Integrate the save query action from the ontotext yasgui component.
    
    ## Why
    Save query action exists in current implementation in the GDB WB. It allows the user to complete the query name and the query itself, to select if the query will be private or public and save it.
    
    ## How
    * The ontotext yasgui component now supports the save query action. This changes in this MR are related with the integration of the save action. See also the related branch https://github.com/Ontotext-AD/ontotext-yasgui/pull/39
    * Implemented tests.
    
    Install latest ontotext yasgui component version and fix error message show after save query failing request
    
    Updated test
    Prevent test failing

--- test-cypress/steps/yasgui-steps.js
+++ test-cypress/steps/yasgui-steps.js
@@ -3,4 +3,68 @@ export class YasguiSteps {
     static getYasgui() {
         return cy.get('.yasgui');
     }
+
+    static getCreateSavedQueryButton() {
+        return cy.get('.yasqe_createSavedQueryButton');
+    }
+
+    static createSavedQuery() {
+        this.getCreateSavedQueryButton().click();
+    }
+
+    static getSaveQueryDialog() {
+        return cy.get('.dialog');
+    }
+
+    static closeSaveQueryDialog() {
+        this.getSaveQueryDialog().find('.close-button').click();
+    }
+
+    static cancelSaveQuery() {
+        this.getSaveQueryDialog().find('.cancel-button').click();
+    }
+
+    static getSaveQueryButton() {
+        return this.getSaveQueryDialog().find('.ok-button');
+    }
+
+    static saveQuery() {
+        this.getSaveQueryButton().click();
+    }
+
+    static getQueryField() {
+        return this.getSaveQueryDialog().find('#query');
+    }
+
+    static writeQuery(query) {
+        this.getQueryField().type(query, {parseSpecialCharSequences: false});
+    }
+
+    static clearQueryField() {
+        this.getQueryField().clear();
+    }
+
+    static getQueryNameField() {
+        return this.getSaveQueryDialog().find('#queryName');
+    }
+
+    static writeQueryName(queryName) {
+        this.getQueryNameField().type(queryName);
+    }
+
+    static clearQueryNameField() {
+        this.getQueryNameField().clear();
+    }
+
+    static toggleIsPublic() {
+        this.getSaveQueryDialog().find('#publicQuery').click();
+    }
+
+    static getErrorsPane() {
+        return this.getSaveQueryDialog().find('.alert-danger');
+    }
+
+    static getErrors() {
+        return this.getErrorsPane().find('.error-message');
+    }
 }
