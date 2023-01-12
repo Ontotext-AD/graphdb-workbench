9f78ec5a00508c5f1420c6ad96256429d70bf3a2
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-01-12 15:42:08 +0200

    GDB-7783 integrate the save query action (#833)
    ## What
    Integrate the save query action from the ontotext yasgui component.
    
    ## Why
    Save query action exists in current implementation in the GDB WB. It allows the user to complete the query name and the query itself, to select if the query will be private or public and save it.
    
    ## How
    * The ontotext yasgui component now supports the save query action. This changes in this MR are related with the integration of the save action. See also the related branch https://github.com/Ontotext-AD/ontotext-yasgui/pull/39
    * Implemented tests.
    
    Install latest ontotext yasgui component version and fix error message show after save query failing request
    
    Updated test
    Prevent test failing

--- src/js/angular/rest/sparql.rest.service.js
+++ src/js/angular/rest/sparql.rest.service.js
@@ -17,26 +17,10 @@ function SparqlRestService($http) {
         addNewSavedQuery
     };
 
-    /**
-     * Fetch a saved query by name and owner.
-     * @param {string} savedQueryName The query name.
-     * @param {string} owner The owner of the query.
-     * @return {Promise} A promise which resolves with the saved query data in format
-     * <pre>
-     * [
-     *   {
-     *       // the name of the query
-     *       name: string;
-     *       // the query itself
-     *       body: string;
-     *       // the query creator
-     *       owner: string;
-     *       // if the query is public or private
-     *       shared: boolean;
-     *   }
-     * ]
-     * </pre>
-     */
+    function getSavedQueries() {
+        return $http.get(SAVED_QUERIES_ENDPOINT);
+    }
+
     function getSavedQuery(savedQueryName, owner) {
         let ownerQuery = '';
         if (owner != null) {
@@ -45,73 +29,32 @@ function SparqlRestService($http) {
         return $http.get(`${SAVED_QUERIES_ENDPOINT}?name=${encodeURIComponent(savedQueryName)}${ownerQuery}`);
     }
 
-    /**
-     * Fetch saved queries.
-     * @return {Promise} a promise which resolves with the saved queries list result in format
-     * <pre>
-     * [
-     *   {
-     *       // the name of the query
-     *       name: string;
-     *       // the query itself
-     *       body: string;
-     *       // the query creator
-     *       owner: string;
-     *       // if the query is public or private
-     *       shared: boolean;
-     *   }
-     * ]
-     * </pre>
-     */
-    function getSavedQueries() {
-        return $http.get(SAVED_QUERIES_ENDPOINT);
+    function addKnownPrefixes(prefixes) {
+        return $http.post(`${SPARQL_ENDPOINT}/add-known-prefixes`, prefixes);
+    }
+
+    function editSavedQuery(query) {
+        return $http.put(SAVED_QUERIES_ENDPOINT, query);
+    }
+
+    function deleteSavedQuery(savedQueryName) {
+        return $http.delete(`${SAVED_QUERIES_ENDPOINT}?name=${encodeURIComponent(savedQueryName)}`);
     }
 
     /**
      * Creates a new saved query.
      *
      * @param {object} payload A payload object in format
-     * <pre>
+     * <code>
      *  {
      *      body: string,
      *      name: string,
      *      shared: boolean
      *  }
-     * </pre>
-     * @return {Promise} a promise which resolves with the result from the save query request or an error message.
+     * </code>
+     * @return {Promise} a promise which resolves with the result of the save query request or an error message.
      */
     function addNewSavedQuery(payload) {
         return $http.post(SAVED_QUERIES_ENDPOINT, payload);
-    }
-
-    /**
-     * Updates an existing saved query.
-     *
-     * @param {object} payload A payload object in format
-     * <pre>
-     *  {
-     *      body: string,
-     *      name: string,
-     *      shared: boolean
-     *  }
-     * </pre>
-     * @return {Promise} a promise which resolves with the result from the save query request or an error message.
-     */
-    function editSavedQuery(payload) {
-        return $http.put(SAVED_QUERIES_ENDPOINT, payload);
-    }
-
-    /**
-     * Deletes an existing saved query.
-     *
-     * @param {string} savedQueryName The name of the query which should be deleted.
-     * @return {Promise} a promise which resolves with the result from the delete query request or an error message.
-     */
-    function deleteSavedQuery(savedQueryName) {
-        return $http.delete(`${SAVED_QUERIES_ENDPOINT}?name=${encodeURIComponent(savedQueryName)}`);
-    }
-
-    function addKnownPrefixes(prefixes) {
-        return $http.post(`${SPARQL_ENDPOINT}/add-known-prefixes`, prefixes);
     }
 }
