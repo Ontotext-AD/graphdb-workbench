0849d2bcaac15c36923f18e89927723abb043b72
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-01-12 15:42:08 +0200

    GDB-7783 integrate the save query action (#833)
    ## What
    Integrate the save query action from the ontotext yasgui component.
    
    ## Why
    Save query action exists in current implementation in the GDB WB. It allows the user to complete the query name and the query itself, to select if the query will be private or public and save it.
    
    ## How
    * The ontotext yasgui component now supports the save query action. This changes in this MR are related with the integration of the save action. See also the related branch https://github.com/Ontotext-AD/ontotext-yasgui/pull/39
    * Implemented tests.
    
    Install latest ontotext yasgui component version and fix error message show after save query failing request
    
    Updated test
    Prevent test failing

--- src/js/angular/sparql-editor/controllers.js
+++ src/js/angular/sparql-editor/controllers.js
@@ -1,12 +1,14 @@
+import {merge} from "lodash";
+
 angular
     .module('graphdb.framework.sparql-editor.controllers', ['ui.bootstrap'])
     .controller('SparqlEditorCtrl', SparqlEditorCtrl);
 
-SparqlEditorCtrl.$inject = ['$scope', '$repositories'];
+SparqlEditorCtrl.$inject = ['$scope', '$repositories', 'toastr', '$translate', 'SparqlRestService'];
 
-function SparqlEditorCtrl($scope, $repositories) {
+function SparqlEditorCtrl($scope, $repositories, toastr, $translate, SparqlRestService) {
 
-    this.repository = "";
+    this.repository = '';
 
     $scope.config = undefined;
 
@@ -14,18 +16,44 @@ function SparqlEditorCtrl($scope, $repositories) {
         $scope.configChanged();
     }
 
-    $scope.queryExecuted = function (query) {
-        console.log(query.detail);
-    };
-
     $scope.configChanged = function () {
         const activeRepository = $repositories.getActiveRepository();
         if (activeRepository) {
             $scope.config = {
-                endpoint: "/repositories/test-repo",
+                endpoint: `/repositories/${activeRepository}`,
                 showToolbar: true
             };
         }
+    };
+
+    $scope.queryExecuted = function (query) {
+        console.log(query.detail);
+    };
+
+    $scope.createSavedQuery = function (event) {
+        const payload = {
+            name: event.detail.queryName,
+            body: event.detail.query,
+            shared: event.detail.isPublic
+        };
+        SparqlRestService.addNewSavedQuery(payload).then((res) => {
+            toastr.success($translate.instant('query.editor.save.saved.query.success.msg', {name: payload.name}));
+            $scope.config = merge({}, $scope.config, {
+                savedQuery: {
+                    saveSuccess: true
+                }
+            });
+        }).catch((err) => {
+            const msg = getError(err);
+            const errorMessage = $translate.instant('query.editor.create.saved.query.error');
+            toastr.error(msg, errorMessage);
+            $scope.config = merge({}, $scope.config, {
+                savedQuery: {
+                    saveSuccess: false,
+                    errorMessage: [errorMessage]
+                }
+            });
+        });
     };
 
     $scope.$on('repositoryIsSet', $scope.configChanged);
