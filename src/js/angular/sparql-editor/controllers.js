41a4eed9029cb1074682e3ffff01e3f7853ed64f
Svilen Velikov <svilen.velikov@ontotext.com>
2023-01-13 19:46:51 +0200

    commit forgotten controller

--- src/js/angular/sparql-editor/controllers.js
+++ src/js/angular/sparql-editor/controllers.js
@@ -1,4 +1,5 @@
 import {merge} from "lodash";
+import {savedQueriesResponseMapper} from "../rest/mappers/saved-query-response-mapper";
 
 angular
     .module('graphdb.framework.sparql-editor.controllers', ['ui.bootstrap'])
@@ -18,7 +19,7 @@ function SparqlEditorCtrl($scope, $repositories, toastr, $translate, SparqlRestS
         $scope.configChanged();
     }
 
-    $scope.configChanged = function () {
+    $scope.configChanged = () => {
         const activeRepository = $repositories.getActiveRepository();
         if (activeRepository) {
             $scope.config = {
@@ -27,6 +28,50 @@ function SparqlEditorCtrl($scope, $repositories, toastr, $translate, SparqlRestS
                 componentId: 'graphdb-workbench-sparql-editor'
             };
         }
+    };
+
+    $scope.queryExecuted = (query) => {
+        console.log(query.detail);
+    };
+
+    $scope.createSavedQuery = (event) => {
+        const payload = {
+            name: event.detail.queryName,
+            body: event.detail.query,
+            shared: event.detail.isPublic
+        };
+        SparqlRestService.addNewSavedQuery(payload).then((res) => {
+            toastr.success($translate.instant('query.editor.save.saved.query.success.msg', {name: payload.name}));
+            $scope.config = merge({}, $scope.config, {
+                savedQuery: {
+                    saveSuccess: true
+                }
+            });
+        }).catch((err) => {
+            const msg = getError(err);
+            const errorMessage = $translate.instant('query.editor.create.saved.query.error');
+            toastr.error(msg, errorMessage);
+            $scope.config = merge({}, $scope.config, {
+                savedQuery: {
+                    saveSuccess: false,
+                    errorMessage: [errorMessage]
+                }
+            });
+        });
+    };
+
+    $scope.loadSavedQueries = () => {
+        SparqlRestService.getSavedQueries().then((res) => {
+            const savedQueries = savedQueriesResponseMapper(res.data);
+            $scope.config = merge({}, $scope.config, {
+                savedQueries: {
+                    data: savedQueries
+                }
+            });
+        }).catch((err) => {
+            const msg = getError(err);
+            toastr.error(msg, $translate.instant('query.editor.get.saved.queries.error'));
+        });
     };
 
     $scope.$on('repositoryIsSet', $scope.configChanged);
