5b9a4b7658532dd43de16348bb7b0a8318b9b478
Svilen Velikov <svilen.velikov@ontotext.com>
2023-01-13 19:39:43 +0200

    GDB-7784 integrate show saved queries action
    ## What
    Introduce a show saved queries action.
    
    ## Why
    Save query action exists in current implementation in the GDB WB. It allows the user to load and see the available saved queries then for each query additional actions can be performed, e.g. select, edit, delete.
    
    ## How
    * Add custom button in the yasqe buttons extension point. The button rises an event for loading of the saved queries. Handling of this event is responsibility of the client who then must set the queries list in the config. Setting the queries in the config triggers the popup to appear.
    * Add a saved queries popup which implements the visualization of the saved queries list. Each saved query is represented as a link and can be selected via click which then opens the query in a new yasgui tab. The dialog can be closed via click outside.
    * Implemented tests.

--- src/js/angular/sparql-editor/controllers.js
+++ src/js/angular/sparql-editor/controllers.js
@@ -29,36 +29,6 @@ function SparqlEditorCtrl($scope, $repositories, toastr, $translate, SparqlRestS
         }
     };
 
-    $scope.queryExecuted = function (query) {
-        console.log(query.detail);
-    };
-
-    $scope.createSavedQuery = function (event) {
-        const payload = {
-            name: event.detail.queryName,
-            body: event.detail.query,
-            shared: event.detail.isPublic
-        };
-        SparqlRestService.addNewSavedQuery(payload).then((res) => {
-            toastr.success($translate.instant('query.editor.save.saved.query.success.msg', {name: payload.name}));
-            $scope.config = merge({}, $scope.config, {
-                savedQuery: {
-                    saveSuccess: true
-                }
-            });
-        }).catch((err) => {
-            const msg = getError(err);
-            const errorMessage = $translate.instant('query.editor.create.saved.query.error');
-            toastr.error(msg, errorMessage);
-            $scope.config = merge({}, $scope.config, {
-                savedQuery: {
-                    saveSuccess: false,
-                    errorMessage: [errorMessage]
-                }
-            });
-        });
-    };
-
     $scope.$on('repositoryIsSet', $scope.configChanged);
     $scope.$on('language-changed', function (event, args) {
         $scope.language = args.locale;
