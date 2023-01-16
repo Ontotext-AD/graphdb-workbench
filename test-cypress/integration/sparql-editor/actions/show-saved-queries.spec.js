9d65fdfe4ab58093e3e5d3f0fcca54e7fe30cedb
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-01-16 10:20:58 +0200

    GDB-7784 integrate show saved queries action (#836)
    * GDB-7784 integrate show saved queries action
    
    ## What
    Introduce a show saved queries action.
    
    ## Why
    Save query action exists in current implementation in the GDB WB. It allows the user to load and see the available saved queries then for each query additional actions can be performed, e.g. select, edit, delete.
    
    ## How
    * Add custom button in the yasqe buttons extension point. The button rises an event for loading of the saved queries. Handling of this event is responsibility of the client who then must set the queries list in the config. Setting the queries in the config triggers the popup to appear.
    * Add a saved queries popup which implements the visualization of the saved queries list. Each saved query is represented as a link and can be selected via click which then opens the query in a new yasgui tab. The dialog can be closed via click outside.
    * Implemented tests.
    
    * commit forgotten controller
    
    * Update ontotext-yasgui-web-component

---
+++ test-cypress/integration/sparql-editor/actions/show-saved-queries.spec.js
@@ -0,0 +1,53 @@
+import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
+import {YasguiSteps} from "../../../steps/yasgui-steps";
+
+describe('Show saved queries', () => {
+
+    let repositoryId;
+
+    beforeEach(() => {
+        repositoryId = 'sparql-editor-' + Date.now();
+        cy.createRepository({id: repositoryId});
+        cy.presetRepository(repositoryId);
+        cy.intercept(`/repositories/${repositoryId}`, {fixture: '/graphql-editor/default-query-response.json'}).as('getGuides');
+
+        SparqlEditorSteps.visitSparqlEditorPage();
+        YasguiSteps.getYasgui().should('be.visible');
+    });
+
+    afterEach(() => {
+        cy.deleteRepository(repositoryId);
+    });
+
+    it('Should open a popup with the saved queries list', () => {
+        // When I click on show saved queries button
+        YasguiSteps.showSavedQueries();
+        // Then I expect that a popup with a saved queries list to be opened
+        YasguiSteps.getSavedQueriesPopup().should('be.visible');
+        YasguiSteps.getSavedQueries().should('have.length.gt', 0);
+    });
+
+    it('Should be able to select a query from the list', () => {
+        // Given I have opened the saved queries popup
+        YasguiSteps.showSavedQueries();
+        YasguiSteps.getSavedQueriesPopup().should('be.visible');
+        // When I select a query from the list
+        YasguiSteps.selectSavedQuery(0);
+        // Then I expect that the popup should be closed
+        YasguiSteps.getSavedQueriesPopup().should('not.exist');
+        // And the query will be populated in a new tab in the yasgui
+        YasguiSteps.getTabs().should('have.length', 2);
+        YasguiSteps.getCurrentTab().should('contain', 'Add statements');
+        YasguiSteps.getTabQuery(1).should('equal', 'PREFIX dc: <http://purl.org/dc/elements/1.1/>\nINSERT DATA\n      {\n      GRAPH <http://example> {\n          <http://example/book1> dc:title "A new book" ;\n                                 dc:creator "A.N.Other" .\n          }\n      }');
+    });
+
+    it('Should be able to close the popup by clicking outside', () => {
+        // Given I have opened the saved queries popup
+        YasguiSteps.showSavedQueries();
+        YasguiSteps.getSavedQueriesPopup().should('be.visible');
+        // When I click outside of the popup
+        cy.get('body').click();
+        // Then the popup should be closed
+        YasguiSteps.getSavedQueriesPopup().should('not.exist');
+    });
+});
