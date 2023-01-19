c29f2b49f7d42cdde6ba7344f74732e14a49d575
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-01-19 09:25:39 +0200

    GDB-7784 integrate saved query edit action (#838)
    * GDB-7784 integrate saved query edit action
    
    ## What
    Integrate the saved queries edit action.
    
    ## Why
    Edit saved query action exists in current implementation in the GDB WB. It allows the user to edit an existing saved query and to save it again.
    
    ## How
    * Introduced additional event handler for the edit action as well as a new saved queries related configuration property which is required by the ontotext yasgui component.
    * Improved the controller and the sparql rest service.
    * Implemented tests.
    
    * Get latest ontotext yasgui component version and fixed review note
    
    Get latest ontotext yasgui component version and fixed review note
    
    * fix failing test

--- test-cypress/integration/sparql-editor/actions/show-saved-queries.spec.js
+++ test-cypress/integration/sparql-editor/actions/show-saved-queries.spec.js
@@ -1,5 +1,5 @@
 import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
-import {YasguiSteps} from "../../../steps/yasgui-steps";
+import {YasguiSteps} from "../../../steps/yasgui/yasgui-steps";
 
 describe('Show saved queries', () => {
 
@@ -32,7 +32,7 @@ describe('Show saved queries', () => {
         YasguiSteps.showSavedQueries();
         YasguiSteps.getSavedQueriesPopup().should('be.visible');
         // When I select a query from the list
-        YasguiSteps.selectSavedQuery(0);
+        YasguiSteps.selectSavedQueryByIndex(0);
         // Then I expect that the popup should be closed
         YasguiSteps.getSavedQueriesPopup().should('not.exist');
         // And the query will be populated in a new tab in the yasgui
