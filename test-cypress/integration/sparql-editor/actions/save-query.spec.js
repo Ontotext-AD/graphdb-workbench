6bc002badc0d157d6df7b45254c9d9b82bb6a052
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-01-24 15:32:23 +0200

    GDB-7785 integrate share saved query action (#842)
    * GDB-7785 integrate share saved query action
    
    ## What
    Integrate the share saved query action.
    
    ## Why
    Share saved query action exists in current implementation in the GDB WB. It allows the user to get a shareable link for an existing saved query.
    
    ## How
    * Introduced additional event handler for the share saved query action.
    * Implemented tests and reorganized the old tests a bit to be better structured and readable as well as to reduce some duplication.
    
    removed logging and fixed typo in the spec name
    
    * Update ontotext yasgui component version
    
    * Fix test checking wrong share link
    
    fixed test using non-existing step method
    
    * some flaky test

--- test-cypress/integration/sparql-editor/actions/save-query.spec.js
+++ test-cypress/integration/sparql-editor/actions/save-query.spec.js
@@ -2,6 +2,9 @@ import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
 import {YasguiSteps} from "../../../steps/yasgui/yasgui-steps";
 import {ApplicationSteps} from "../../../steps/application-steps";
 import {QueryStubs} from "../../../stubs/yasgui/query-stubs";
+import {SaveQueryDialog} from "../../../steps/yasgui/save-query-dialog";
+import {SavedQuery} from "../../../steps/yasgui/saved-query";
+import {SavedQueriesDialog} from "../../../steps/yasgui/saved-queries-dialog";
 
 describe('Save query', () => {
 
@@ -9,6 +12,7 @@ describe('Save query', () => {
 
     beforeEach(() => {
         repositoryId = 'sparql-editor-' + Date.now();
+        cy.intercept('GET', '/rest/monitor/query/count', {body: 0});
         cy.createRepository({id: repositoryId});
         cy.presetRepository(repositoryId);
         QueryStubs.stubDefaultQueryResponse(repositoryId);
@@ -23,26 +27,15 @@ describe('Save query', () => {
 
     it('Should be able to edit query and save it', () => {
         // Given I have opened the sparql editor page
-        YasguiSteps.getCreateSavedQueryButton().should('be.visible');
-        YasguiSteps.createSavedQuery();
-        // When I write a query name
-        YasguiSteps.clearQueryNameField();
-        const savedQueryName = generateQueryName();
-        YasguiSteps.writeQueryName(savedQueryName);
-        YasguiSteps.clearQueryField();
-        YasguiSteps.writeQuery('select *');
-        YasguiSteps.toggleIsPublic();
-        // And I click on save button
-        YasguiSteps.saveQuery();
-        // Then the query should be saved
-        YasguiSteps.getSaveQueryDialog().should('not.exist');
-        ApplicationSteps.getSuccessNotifications().should('be.visible');
+        // And I have created a query
+        const savedQueryName = SavedQuery.generateQueryName();
+        SavedQuery.create(savedQueryName);
         // When I open the saved queries popup
         YasguiSteps.showSavedQueries();
         // Then I expect to see the saved query in the list
-        YasguiSteps.getSavedQueries().should('contain', savedQueryName);
+        SavedQueriesDialog.getSavedQueries().should('contain', savedQueryName);
         // When I select the query
-        YasguiSteps.selectSavedQueryByName(savedQueryName);
+        SavedQueriesDialog.selectSavedQueryByName(savedQueryName);
         // Then I expect to see the query opened in a new editor tab
         YasguiSteps.getTabs().should('have.length', 2);
         YasguiSteps.getCurrentTab().should('contain', savedQueryName);
@@ -52,40 +45,26 @@ describe('Save query', () => {
     it('Should prevent saving query with duplicated name', () => {
         // Given I have opened the sparql editor page
         YasguiSteps.getCreateSavedQueryButton().should('be.visible');
-        YasguiSteps.createSavedQuery();
-        // When I write a query name
-        YasguiSteps.clearQueryNameField();
-        const savedQueryName = generateQueryName();
-        YasguiSteps.writeQueryName(savedQueryName);
-        YasguiSteps.clearQueryField();
-        YasguiSteps.writeQuery('select *');
-        YasguiSteps.toggleIsPublic();
-        // And I click on save button
-        YasguiSteps.saveQuery();
-        // Then the query should be saved
-        YasguiSteps.getSaveQueryDialog().should('not.exist');
-        ApplicationSteps.getSuccessNotifications().should('be.visible');
+        // And I have created a query
+        const savedQueryName = SavedQuery.generateQueryName();
+        SavedQuery.create(savedQueryName);
         // When I try to save the query with the same name
         YasguiSteps.createSavedQuery();
-        YasguiSteps.clearQueryNameField();
-        YasguiSteps.writeQueryName(savedQueryName);
-        YasguiSteps.saveQuery();
+        SaveQueryDialog.clearQueryNameField();
+        SaveQueryDialog.writeQueryName(savedQueryName);
+        SaveQueryDialog.saveQuery();
         // Then I expect that dialog will remain open and an error will be visible
         // TODO: find out why this check fails on Jenkins sometimes with
         // AssertionError: Timed out retrying after 30000ms: Expected to find element: `.toast-error`, but never found it. Queried from element: <div#toast-container.toast-bottom-right>
         // ApplicationSteps.getErrorNotifications().should('be.visible');
-        YasguiSteps.getSaveQueryDialog().should('be.visible');
-        YasguiSteps.getErrorsPane().should('contain', 'Error! Cannot create saved query');
+        SaveQueryDialog.getSaveQueryDialog().should('be.visible');
+        SaveQueryDialog.getErrorsPane().should('contain', 'Error! Cannot create saved query');
         // When I change the query name
-        YasguiSteps.clearQueryNameField();
-        YasguiSteps.writeQueryName(generateQueryName());
-        YasguiSteps.saveQuery();
+        SaveQueryDialog.clearQueryNameField();
+        SaveQueryDialog.writeQueryName(SavedQuery.generateQueryName());
+        SaveQueryDialog.saveQuery();
         // Then I should be able to save the query
-        YasguiSteps.getSaveQueryDialog().should('not.exist');
+        SaveQueryDialog.getSaveQueryDialog().should('not.exist');
         ApplicationSteps.getSuccessNotifications().should('be.visible');
     });
 });
-
-function generateQueryName() {
-    return 'Saved query - ' + Date.now();
-}
