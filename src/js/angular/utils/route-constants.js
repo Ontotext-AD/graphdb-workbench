a34047cd1eb55ac24c5c0060be25da4153c593af
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-01-24 15:32:23 +0200

    GDB-7785 integrate share saved query action (#842)
    * GDB-7785 integrate share saved query action
    
    ## What
    Integrate the share saved query action.
    
    ## Why
    Share saved query action exists in current implementation in the GDB WB. It allows the user to get a shareable link for an existing saved query.
    
    ## How
    * Introduced additional event handler for the share saved query action.
    * Implemented tests and reorganized the old tests a bit to be better structured and readable as well as to reduce some duplication.
    
    removed logging and fixed typo in the spec name
    
    * Update ontotext yasgui component version
    
    * Fix test checking wrong share link
    
    fixed test using non-existing step method
    
    * some flaky test

--- src/js/angular/utils/route-constants.js
+++ src/js/angular/utils/route-constants.js
@@ -1,9 +1,5 @@
 export const RouteConstants = {
     'savedQueryName': 'savedQueryName',
     'savedQueryOwner': 'owner',
-    'query': 'query',
-    // same as savedQueryName but for shared queries - this is how WB handles it
-    'name': 'name',
-    // same as savedQueryOwner but for shared queries - this is how WB handles it
-    'owner': 'owner'
+    'query': 'query'
 };
