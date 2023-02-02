1fb3085fa3b0bc6d1765f67af2dd17e603be9e7f
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-02-02 13:57:39 +0200

    GDB-7786 Introduce share query action (#853)
    ## What
    Allow the client to get a shareable link for the currently opened query and open it.
    
    ## Why
    The WB has this feature.
    
    ## How
    * Introduced the share query action in the editor and handling the events from it.
    * Implemented resolver for the shared query url parameters allowing to open and init a sparql editor tab based on these parameters.
    * Implemented tests.

--- src/js/angular/utils/route-constants.js
+++ src/js/angular/utils/route-constants.js
@@ -1,5 +1,9 @@
 export const RouteConstants = {
     'savedQueryName': 'savedQueryName',
     'savedQueryOwner': 'owner',
-    'query': 'query'
+    'query': 'query',
+    // same as savedQueryName but for shared queries - this is how WB handles it
+    'name': 'name',
+    // same as savedQueryOwner but for shared queries - this is how WB handles it
+    'owner': 'owner'
 };
