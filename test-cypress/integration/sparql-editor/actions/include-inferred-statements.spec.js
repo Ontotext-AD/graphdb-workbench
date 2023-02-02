2dd5040f7a2d26b6b561386f4a67c5b40eddd4de
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2023-02-02 11:39:03 +0200

    GDB-7786 Introduce actions to include inferred statements and expand results over owl:sameAs (#852)
    ## What
    Allow the client to include inferred statements and expand results over owl:sameAs.
    
    ## Why
    The WB has these features.
    
    ## How
    * Introduced include inferred statements action in the editor which controls the value of the `infer` request parameter.
    * Introduced expand results over owl:sameAs action in the editor which controls the value of the `sameAs` request parameter.
    * Implemented tests.

---
+++ test-cypress/integration/sparql-editor/actions/include-inferred-statements.spec.js
@@ -0,0 +1,37 @@
+import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
+import {YasguiSteps} from "../../../steps/yasgui/yasgui-steps";
+import {YasqeSteps} from "../../../steps/yasgui/yasqe-steps";
+
+describe('Include inferred statements', () => {
+
+    let repositoryId;
+
+    beforeEach(() => {
+        repositoryId = 'sparql-editor-' + Date.now();
+        cy.intercept('GET', '/rest/monitor/query/count', {body: 0});
+        cy.createRepository({id: repositoryId});
+        cy.presetRepository(repositoryId);
+        cy.intercept(`/repositories/${repositoryId}`, {fixture: '/graphql-editor/default-query-response.json'}).as('query');
+
+        SparqlEditorSteps.visitSparqlEditorPage();
+        YasguiSteps.getYasgui().should('be.visible');
+    });
+
+    afterEach(() => {
+        cy.deleteRepository(repositoryId);
+    });
+
+    it('Should be able to toggle include inferred statements', () => {
+        // When I open the editor
+        // Then I expect that include inferred statements should be enabled by default
+        YasqeSteps.getIncludeInferredStatementsButton().should('have.attr', 'title', 'Include inferred data in results: ON');
+        YasqeSteps.getIncludeInferredStatementsButton().should('have.class', 'icon-inferred-on');
+        YasqeSteps.executeQuery();
+        cy.wait('@query').its('request.body').should('contain', 'infer=true');
+        YasqeSteps.includeInferredStatements();
+        YasqeSteps.getIncludeInferredStatementsButton().should('have.attr', 'title', 'Include inferred data in results: OFF');
+        YasqeSteps.getIncludeInferredStatementsButton().should('have.class', 'icon-inferred-off');
+        YasqeSteps.executeQuery();
+        cy.wait('@query').its('request.body').should('contain', 'infer=false');
+    });
+});
