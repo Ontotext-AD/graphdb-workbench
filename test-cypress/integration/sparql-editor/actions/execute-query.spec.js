05df24b7c7c22bd9b84b1eda2108ec3d151c68ec
Svilen Velikov <svilen.velikov@ontotext.com>
2023-02-13 10:59:57 +0200

    GDB-7890 Handle queryExecuted and add proper http headers to the request.
    ## What
    Add http GDB specific http headers before query request.
    
    ## Why
    GDB expects certain http headers to be sent with each query request like: x-graphdb-track-alias, X-GraphDB-Repository-Location, X-Requested-With, etc.
    
    ## How
    * Handled executeQuery event from ontotext-yasgui and added needed headers.
    * Added some documentation.
    
    Add tests

---
+++ test-cypress/integration/sparql-editor/actions/execute-query.spec.js
@@ -0,0 +1,43 @@
+import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
+import {YasguiSteps} from "../../../steps/yasgui/yasgui-steps";
+import {QueryStubs} from "../../../stubs/yasgui/query-stubs";
+import {YasqeSteps} from "../../../steps/yasgui/yasqe-steps";
+
+describe('Execute query', () => {
+
+    let repositoryId;
+
+    beforeEach(() => {
+        repositoryId = 'sparql-editor-' + Date.now();
+        cy.intercept('GET', '/rest/monitor/query/count', {body: 0});
+        cy.createRepository({id: repositoryId});
+        cy.presetRepository(repositoryId);
+        QueryStubs.stubDefaultQueryResponse(repositoryId);
+
+        SparqlEditorSteps.visitSparqlEditorPage();
+        YasguiSteps.getYasgui().should('be.visible');
+    });
+
+    afterEach(() => {
+        cy.deleteRepository(repositoryId);
+    });
+
+    it('Should be able to execute query from each editor tab', () => {
+        cy.intercept('POST', `/repositories/${repositoryId}`).as('query');
+        YasqeSteps.executeQuery();
+        cy.wait('@query').then((interception) => {
+            const headers = interception.request.headers;
+            expect(headers).to.have.property('x-graphdb-local-consistency');
+            expect(headers).to.have.property('x-graphdb-repository-location');
+            expect(headers).to.have.property('x-graphdb-track-alias');
+        });
+        YasguiSteps.openANewTab();
+        YasqeSteps.executeQuery(1);
+        cy.wait('@query').then((interception) => {
+            const headers = interception.request.headers;
+            expect(headers).to.have.property('x-graphdb-local-consistency');
+            expect(headers).to.have.property('x-graphdb-repository-location');
+            expect(headers).to.have.property('x-graphdb-track-alias');
+        });
+    });
+});
