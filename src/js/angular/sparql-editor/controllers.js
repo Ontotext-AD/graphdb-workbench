20b98fde32f7b824eb8e7bc28725546ed321dc85
Svilen Velikov <svilen.velikov@ontotext.com>
2023-02-13 10:59:57 +0200

    GDB-7890 Handle queryExecuted and add proper http headers to the request.
    ## What
    Add http GDB specific http headers before query request.
    
    ## Why
    GDB expects certain http headers to be sent with each query request like: x-graphdb-track-alias, X-GraphDB-Repository-Location, X-Requested-With, etc.
    
    ## How
    * Handled executeQuery event from ontotext-yasgui and added needed headers.
    * Added some documentation.
    
    Add tests

--- src/js/angular/sparql-editor/controllers.js
+++ src/js/angular/sparql-editor/controllers.js
@@ -31,13 +31,9 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
     // Public functions
     // =========================
 
-    $scope.configChanged = () => {
+    $scope.updateConfig = () => {
         const activeRepository = $repositories.getActiveRepository();
         if (activeRepository) {
-            const authToken = $jwtAuth.getAuthToken();
-            if (authToken) {
-                headers['Authorization'] = authToken;
-            }
             $scope.config = {
                 endpoint: `/repositories/${activeRepository}`,
                 showToolbar: true,
@@ -50,9 +46,39 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
         }
     };
 
-    $scope.queryExecuted = (query) => {
-        // eslint-disable-next-line no-console
-        console.log(query.detail);
+    // =========================
+    // Event handlers
+    // =========================
+
+    /**
+     * When the repository gets changed through the UI, we need to update the yasgui configuration so that new queries
+     * to be issued against the new repository.
+     */
+    $scope.$on('repositoryIsSet', $scope.updateConfig);
+    $scope.$on('repositoryIsSet', init);
+
+    $scope.$on('language-changed', function (event, args) {
+        $scope.language = args.locale;
+    });
+
+    /**
+     * Handles the queryExecuted event emitted by the ontotext-yasgui. The event is fired immediately before sending the
+     * request and the request object can be altered here and it will be sent with these changes.
+     * @param {object} event The event payload containing the query and the request object.
+     */
+    $scope.queryExecuted = (event) => {
+        updateRequestHeaders(event.detail.request);
+    };
+
+    /**
+     * Handles the createSavedQuery event emitted by the ontotext-yasgui. The event is fired when a saved query should
+     * be created.
+     * @param {object} event The event payload containing the query data from which a saved query object should be
+     * created.
+     */
+    $scope.createSavedQuery = (event) => {
+        const payload = queryPayloadFromEvent(event);
+        SparqlRestService.addNewSavedQuery(payload).then(() => queryCreatedHandler(payload)).catch(querySaveErrorHandler);
     };
 
     $scope.notify = (notification) => {
@@ -65,16 +91,21 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
         }
     };
 
-    $scope.createSavedQuery = (event) => {
-        const payload = queryPayloadFromEvent(event);
-        SparqlRestService.addNewSavedQuery(payload).then(() => queryCreatedHandler(payload)).catch(querySaveErrorHandler);
-    };
-
+    /**
+     * Handles the updateSavedQuery event emitted by the ontotext-yasgui. The event is fired when a saved query should
+     * be updated.
+     * @param {object} event The event payload containing the saved query data which should be updated.
+     */
     $scope.updateSavedQuery = (event) => {
         const payload = queryPayloadFromEvent(event);
         SparqlRestService.editSavedQuery(payload).then(() => queryUpdatedHandler(payload)).catch(querySaveErrorHandler);
     };
 
+    /**
+     * Handles the deleteSavedQuery event emitted by the ontotext-yasgui. The event is fired when a saved query should
+     * be deleted.
+     * @param {object} event The event payload containing the saved query data which should be deleted.
+     */
     $scope.deleteSavedQuery = (event) => {
         const payload = queryPayloadFromEvent(event);
         SparqlRestService.deleteSavedQuery(payload.name).then(() => {
@@ -85,6 +116,11 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
         });
     };
 
+    /**
+     * Handles the shareSavedQuery event emitted by the ontotext-yasgui. The event is fired when a saved query should
+     * be shared and is expected the share link to be created.
+     * @param {object} event The event payload containing the saved query data from which the share link to be created.
+     */
     $scope.shareSavedQuery = (event) => {
         const payload = queryPayloadFromEvent(event);
         $scope.savedQueryConfig = {
@@ -92,6 +128,11 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
         };
     };
 
+    /**
+     * Handles the shareQuery event emitted by the ontotext-yasgui. The event is fired when a query should be shared and
+     * is expected the share link to be created.
+     * @param {object} event The event payload containing the query data from which the share link to be created.
+     */
     $scope.shareQuery = (event) => {
         const payload = queryPayloadFromEvent(event);
         $scope.savedQueryConfig = {
@@ -99,10 +140,17 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
         };
     };
 
+    /**
+     * Handles the queryShareLinkCopied event emitted by the ontotext-yasgui. The event is fired when a query share link
+     * is copied by the user.
+     */
     $scope.queryShareLinkCopied = () => {
         toastr.success($translate.instant('modal.ctr.copy.url.success'));
     };
 
+    /**
+     * The event is fired when saved queries should be loaded in order to be displayed to the user.
+     */
     $scope.loadSavedQueries = () => {
         SparqlRestService.getSavedQueries().then((res) => {
             const savedQueries = savedQueriesResponseMapper(res);
@@ -116,18 +164,22 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
     };
 
     // =========================
-    // Event handlers
-    // =========================
-
-    $scope.$on('repositoryIsSet', init);
-
-    $scope.$on('language-changed', function (event, args) {
-        $scope.language = args.locale;
-    });
-
-    // =========================
     // Private function
     // =========================
+
+    const updateRequestHeaders = (req) => {
+        const authToken = $jwtAuth.getAuthToken();
+        if (authToken) {
+            req.header['Authorization'] = authToken;
+        }
+        // TODO: implement
+        // headers['X-GraphDB-Catch'] = scope.currentTabConfig.pageSize + '; throw';
+        // Generates a new tracking alias for queries based on time
+        $scope.currentTrackAlias = `query-editor-${performance.now()}-${Date.now()}`;
+        req.header['X-GraphDB-Track-Alias'] = $scope.currentTrackAlias;
+        req.header['X-GraphDB-Repository-Location'] = $repositories.getActiveRepositoryObject().location;
+        req.header['X-Requested-With'] = 'XMLHttpRequest';
+    };
 
     const initTabFromSavedQuery = (queryParams) => {
         const savedQueryName = queryParams[RouteConstants.savedQueryName];
@@ -210,7 +262,7 @@ function SparqlEditorCtrl($scope, $location, $jwtAuth, $repositories, toastr, $t
                 setPrefixes(namespacesResponse);
             })
             .finally(() => {
-                $scope.configChanged();
+                $scope.updateConfig();
                 // check is there is a savedquery or query url parameter and init the editor
                 initViewFromUrlParams();
                 // on repo change do the same as above
