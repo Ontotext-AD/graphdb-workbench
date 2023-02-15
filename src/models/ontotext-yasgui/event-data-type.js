9ecb967d26cfda0ba912dd65a9099545b6c7fb46
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-02-15 19:19:06 +0200

    Refactors copy resource link success message.
    ## What
    Changes implementation of notification when an url of result link is copied into clipboard.
    
    ## Why
    There is a new implementation of event fired by "ontotext-yasgui" when an url link is copied.
    ## How
    Updates the version of "ontotext-yasgui". Register a notification event handler in "ontotext-yasgui" output event emitter.
    Additional work:
    Added models for:
    - EventData - event data model for all events fired by "ontotext-yasgui";
    - EventDataType - describes all possible events which can be fired by "ontotext-yasgui";
    - NotificationMessageType - describes all possible notification message types: "success", "warning", "error";
    - YasrPluginName - describes the names of yasr plugins.

---
+++ src/models/ontotext-yasgui/event-data-type.js
@@ -0,0 +1,12 @@
+/**
+ * Holds all possible values of {@see EventData} types.
+ */
+export class EventDataType {
+    static get DOWNLOAD_AS() {
+        return 'downloadAs';
+    }
+
+    static get NOTIFICATION_MESSAGE() {
+        return 'notificationMessage';
+    }
+}
