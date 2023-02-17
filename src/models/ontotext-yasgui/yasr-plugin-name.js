dea1a4e18e834e000404b31c5462523e730fa7c3
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-02-17 13:23:00 +0200

    Convert event type classed to just simple objects.

--- src/models/ontotext-yasgui/yasr-plugin-name.js
+++ src/models/ontotext-yasgui/yasr-plugin-name.js
@@ -1,16 +1,8 @@
 /**
  * Holds all yasr plugin names.
  */
-export class YasrPluginName {
-    static get EXTENDED_TABLE() {
-        return 'extended_table';
-    }
-
-    static get RAW_RESPONSE() {
-        return 'response';
-    }
-
-    static get TABLE() {
-        return 'table';
-    }
-}
+export const YasrPluginName = {
+    'EXTENDED_TABLE': 'extended_table',
+    'RAW_RESPONSE': 'response',
+    'TABLE': 'table'
+};
