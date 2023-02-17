f7f66df32367e43f501c3d57624e0b5c6ce504df
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-02-17 13:23:00 +0200

    Convert event type classed to just simple objects.

--- src/models/ontotext-yasgui/event-data-type.js
+++ src/models/ontotext-yasgui/event-data-type.js
@@ -1,12 +1,7 @@
 /**
  * Holds all possible values of {@see EventData} types.
  */
-export class EventDataType {
-    static get DOWNLOAD_AS() {
-        return 'downloadAs';
-    }
-
-    static get NOTIFICATION_MESSAGE() {
-        return 'notificationMessage';
-    }
-}
+export const EventDataType = {
+    'DOWNLOAD_AS': 'downloadAs',
+    'NOTIFICATION_MESSAGE': 'notificationMessage'
+};
