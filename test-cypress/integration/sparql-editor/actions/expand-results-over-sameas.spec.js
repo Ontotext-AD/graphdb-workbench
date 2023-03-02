37c5b2391f3d4a997d77e9fd21680e6978faeb10
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-03-03 00:11:46 +0200

    GDB-7954: Add pagination control to all yasr plugins
    ## What
    Added pagination to yasr component.
    
    ## Why
    Some queries returns a lot of results. To show all of it we need a lot of resource and visualization will be slow.
    
    ## How
     - Updates "ontotext-yasgui-web-component".
     - Refactoring query event. It is registered in output handlers.

--- test-cypress/integration/sparql-editor/actions/expand-results-over-sameas.spec.js
+++ test-cypress/integration/sparql-editor/actions/expand-results-over-sameas.spec.js
@@ -1,6 +1,7 @@
 import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
 import {YasguiSteps} from "../../../steps/yasgui/yasgui-steps";
 import {YasqeSteps} from "../../../steps/yasgui/yasqe-steps";
+import {QueryStubDescription, QueryStubs} from "../../../stubs/yasgui/query-stubs";
 
 describe('Expand results over owl:sameAs', () => {
 
@@ -22,12 +23,16 @@ describe('Expand results over owl:sameAs', () => {
     });
 
     it('Should be able to toggle expand results parameter', () => {
+        const queryDescription = new QueryStubDescription()
+            .setRepositoryId(repositoryId)
+            .setTotalElements(1);
+        QueryStubs.stubQueryResults(queryDescription);
         // When I open the editor
         // Then I expect that expand results should be enabled by default
         YasqeSteps.getExpandResultsOverSameAsButtonTooltip().should('have.attr', 'data-tooltip', 'Expand results over owl:sameAs: ON');
         YasqeSteps.getExpandResultsOverSameAsButton().should('have.class', 'icon-same-as-on');
         YasqeSteps.executeQuery();
-        cy.wait('@query').its('request.body').should('contain', 'sameAs=true');
+        cy.wait('@query-1_0_1001_1').its('request.body').should('contain', 'sameAs=true');
         // When I disable the expand results action
         YasqeSteps.expandResultsOverSameAs();
         // Then I expect that the button state should be changed
@@ -35,7 +40,7 @@ describe('Expand results over owl:sameAs', () => {
         YasqeSteps.getExpandResultsOverSameAsButton().should('have.class', 'icon-same-as-off');
         // And sameAs=false parameter should be sent with the request
         YasqeSteps.executeQuery();
-        cy.wait('@query').its('request.body').should('contain', 'sameAs=false');
+        cy.wait('@query-1_0_1001_1').its('request.body').should('contain', 'sameAs=false');
         YasqeSteps.expandResultsOverSameAs();
         // When I disable the include inferred action
         YasqeSteps.includeInferredStatements();
@@ -43,6 +48,6 @@ describe('Expand results over owl:sameAs', () => {
         YasqeSteps.getExpandResultsOverSameAsButtonTooltip().should('have.attr', 'data-tooltip', 'Requires \'Include Inferred\'!');
         YasqeSteps.getExpandResultsOverSameAsButton().should('have.class', 'icon-same-as-off');
         YasqeSteps.executeQuery();
-        cy.wait('@query').its('request.body').should('contain', 'infer=false&sameAs=false');
+        cy.wait('@query-1_0_1001_1').its('request.body').should('contain', 'infer=false&sameAs=false');
     });
 });
