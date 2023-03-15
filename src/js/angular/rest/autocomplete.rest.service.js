1ca22f9431e40365748368b53f82e5c18b300e88
Svilen Velikov <svilen.velikov@ontotext.com>
2023-03-15 17:06:53 +0200

    GDB-7760 introduce local names autocomplete
    ## What
    Implemented a local names autocompleter and registered in yasqe.
    
    ## Why
    The autocompleter allows suggesting namespaces from the GDB instance to which the WB is connected.
    
    ## How
    Implemented the autocompleter and provided a new configuration allowing custom autocomplete functions to be provided by the component client.

--- src/js/angular/rest/autocomplete.rest.service.js
+++ src/js/angular/rest/autocomplete.rest.service.js
@@ -2,10 +2,10 @@ angular
     .module('graphdb.framework.rest.autocomplete.service', [])
     .factory('AutocompleteRestService', AutocompleteRestService);
 
-const AUTOCOMPLETE_ENDPOINT = 'rest/autocomplete/';
-const AUTOCOMPLETE_LABELS_ENDPOINT = `${AUTOCOMPLETE_ENDPOINT}labels`;
-const AUTOCOMPLETE_ENABLED_ENDPOINT = `${AUTOCOMPLETE_ENDPOINT}enabled`;
-const AUTOCOMPLETE_IRIS_ENDPOINT = `${AUTOCOMPLETE_ENDPOINT}iris`;
+const AUTOCOMPLETE_ENDPOINT = 'rest/autocomplete';
+const AUTOCOMPLETE_LABELS_ENDPOINT = `${AUTOCOMPLETE_ENDPOINT}/labels`;
+const AUTOCOMPLETE_ENABLED_ENDPOINT = `${AUTOCOMPLETE_ENDPOINT}/enabled`;
+const AUTOCOMPLETE_IRIS_ENDPOINT = `${AUTOCOMPLETE_ENDPOINT}/iris`;
 
 AutocompleteRestService.$inject = ['$http'];
 
@@ -42,23 +42,23 @@ function AutocompleteRestService($http) {
     }
 
     function buildIndex() {
-        return $http.post(`${AUTOCOMPLETE_ENDPOINT}reindex`);
+        return $http.post(`${AUTOCOMPLETE_ENDPOINT}/reindex`);
     }
 
     function interruptIndexing() {
-        return $http.post(`${AUTOCOMPLETE_ENDPOINT}interrupt`);
+        return $http.post(`${AUTOCOMPLETE_ENDPOINT}/interrupt`);
     }
 
     function refreshIndexStatus() {
-        return $http.get(`${AUTOCOMPLETE_ENDPOINT}status`);
+        return $http.get(`${AUTOCOMPLETE_ENDPOINT}/status`);
     }
 
     function checkForPlugin() {
-        return $http.get(`${AUTOCOMPLETE_ENDPOINT}plugin-found`);
+        return $http.get(`${AUTOCOMPLETE_ENDPOINT}/plugin-found`);
     }
 
     function getAutocompleteSuggestions(str, cancelerPromise) {
-        return $http.get(`${AUTOCOMPLETE_ENDPOINT}query`, {
+        return $http.get(`${AUTOCOMPLETE_ENDPOINT}/query`, {
             params: {
                 q: str
             },
