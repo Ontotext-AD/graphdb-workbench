b6e59a04ea9e9fb6972eb1631f6e02a4b5ec2e06
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-03-23 15:44:35 +0200

    GDB-8085: implement update query flow
    ## What
    There is difference between execution flow of "update" and "select" queries.
    Implements base functionality of update queries.
    
    ## Why
    The update queries has not results and YASR have to be not visible. Result info message have to describe how many statements have been affected.
    
    ## How
    - Updates version of "ontotext-yasgui-web-component".
    - Implements callback function "getRepositoryStatementsCount", that returns repository statements count.

--- test-cypress/steps/yasgui/yasqe-steps.js
+++ test-cypress/steps/yasgui/yasqe-steps.js
@@ -11,12 +11,29 @@ export class YasqeSteps {
         return cy.get(".yasqe:visible");
     }
 
+    static getCodeMirror() {
+        return this.getEditor().find('.CodeMirror').then(($el) => {
+            // @ts-ignore
+            return $el[0].CodeMirror;
+        });
+    }
+
     static getExecuteQueryButton() {
         return cy.get('.yasqe_queryButton');
     }
 
     static executeQuery(tabIndex = 0) {
         this.getExecuteQueryButton(tabIndex).eq(tabIndex).click();
+    }
+
+    static clearEditor() {
+        this.getCodeMirror().then((cm) => {
+            cm.getDoc().setValue('');
+        });
+    }
+
+    static writeInEditor(text, parseSpecialCharSequences = false) {
+        this.getEditor().find('textarea').type(text, {force: true, parseSpecialCharSequences});
     }
 
     static getControlBar() {
