d8d47b06b7976917ab306119c325b33344145223
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-04-06 13:33:46 +0300

    GDB-8132: Add appropriate headers depending on query mode and type
    ## What
    Updates query headers with correct headers and values.
    
    ## Why
    The different queries have a different Accept header.
    
    ## How
    Setting an appropriate Accept header value depends on the query mode and type.

---
+++ src/models/ontotext-yasgui/query-type.js
@@ -0,0 +1,19 @@
+/**
+ * Holds all possible values of the SPARQL queries.
+ * @type {{ADD: string, CONSTRUCT: string, CREATE: string, LOAD: string, COPY: string, SELECT: string, DROP: string, DELETE: string, MOVE: string, ASK: string, INSERT: string, DESCRIBE: string, CLEAR: string}}
+ */
+export const QueryType = {
+    "SELECT": "SELECT",
+    "CONSTRUCT": "CONSTRUCT",
+    "ASK": "ASK",
+    "DESCRIBE": "DESCRIBE",
+    "INSERT": "INSERT",
+    "DELETE": "DELETE",
+    "LOAD": "LOAD",
+    "CLEAR": "CLEAR",
+    "CREATE": "CREATE",
+    "DROP": "DROP",
+    "COPY": "COPY",
+    "MOVE": "MOVE",
+    "ADD": "ADD"
+};
