98e7ea4ac4fdb4cccc357053dc7f1680a1e1cb63
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-05-26 15:02:29 +0300

    GDB-8006: Integration in create sparql template view
    ## What
    Refactoring "Sparql template" view.
    
    ## Why
    We want to integrate "ontotext-yasgui-web-component" to WB.
    
    #How
    Replaces the old implementation with new one, because it is not applicable with "ontoext-yasgui-web-component".
    Implements test to cover the create/update sparql template functionality.

---
+++ test-cypress/steps/setup/sparql-create-update-steps.js
@@ -0,0 +1,55 @@
+export class SparqlCreateUpdateSteps {
+    static visit(templateId) {
+        cy.visit(`/sparql-template/create${templateId ? '?templateID=' + templateId : ''}`);
+        cy.get('.ontotext-yasgui').should('be.visible');
+    }
+
+    static verifyUrl() {
+        cy.url().should('include', '/sparql-template/create');
+    }
+
+    static getSaveButton() {
+        return cy.get('.save-query-btn');
+    }
+
+    static clickOnSaveButton() {
+        SparqlCreateUpdateSteps.getSaveButton().click();
+    }
+
+    static getCancelButton() {
+        return cy.get('.cancel-query-btn');
+    }
+
+    static clickOnCancelButton() {
+        SparqlCreateUpdateSteps.getCancelButton().click();
+    }
+
+    static typeTemplateId(templateId) {
+        SparqlCreateUpdateSteps.getTemplateIdField()
+            .should('exist')
+            .click()
+            // force is needed because ontotext-yasgui is created with focus on editor
+            // and if it appears in the middle of templateId typing the last part of template is written in ontotext-yasgui instead input element.
+            .type(templateId, {force: true});
+    }
+
+    static getTemplateIdField() {
+        return cy.get('.sparql-template-id');
+    }
+
+    static getRequiredErrorElement() {
+        return cy.get('.template-id-required');
+    }
+
+    static getInvalidErrorElement() {
+        return cy.get('.template-id-invalid');
+    }
+
+    static getInvalidQueryModeElement() {
+        return cy.get('.invalid-query-mode');
+    }
+
+    static getInvalidQueryElement() {
+        return cy.get('.invalid-query');
+    }
+}
