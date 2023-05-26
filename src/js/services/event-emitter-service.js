04a6762c6ee42bf41a704ebfbadd6837b49dba57
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-05-26 14:50:11 +0300

    GDB-8006: Integration in create sparql template view
    ## What
    Added functionality to prevent changing of active repository.
    
    ## Why
    There is scenarios, when user have stop active repository changing. Such scenario is when user opens a sparql template for edit and change the query without save it. If user try to change the active repository a confirm dialog will appear. When user click on cancel button, he will stay on same page, but the repository is already changed and page is in incorrect state, the repository is new one, but loaded template is from other repository.
    
    #How
    Implements event service that process synchronous communication between independent components through events. Through the service the components can subscribe for some event. All subscribers will be called when some component emit the event. If the event emitted with event data, the data will be passed as argument to the subscribers.
    Added a new event before the repository is changed. It will be emitted before the repository changes. The event data contains references to the new and the old repository and flag named cancel with default value false. If any of the subscribers changes the flag to true, then repository will not change.

---
+++ src/js/services/event-emitter-service.js
@@ -0,0 +1,58 @@
+const modules = [];
+
+angular
+    .module('graphdb.framework.utils.event-emitter-service', modules)
+    .factory('EventEmitterService', EventEmitterService);
+
+/**
+ * A service responsible for emitting events between components. It takes care to registering subscribers and calling them when an event for which they are registered occurs.
+ */
+function EventEmitterService() {
+    const subscribers = {};
+
+    /**
+     * Registers the <code>subscriber</code> for event with name <code>eventName</code>. When event occurred the subscribers will be called.
+     * @param eventName - the name of the event that will trigger a subscriber call.
+     * @param {Promise<any>} subscriber - promise that will call when event with <code>eventName</code> occurred.
+     * @return {(function(): void)|*} - unsubscribe function.
+     */
+    const subscribe = (eventName, subscriber) => {
+        if (!subscribers[eventName]) {
+            subscribers[eventName] = [];
+        }
+        subscribers[eventName].push(subscriber);
+
+        // Return an unsubscribe function
+        return function () {
+            const index = subscribers[eventName].indexOf(subscriber);
+            if (index !== -1) {
+                subscribers[eventName].splice(index, 1);
+            }
+        };
+    };
+
+    /**
+     * Call all subscribers that are registered for event with <code>eventName</code>.
+     * @param eventName - name of event.
+     * @param eventData - event data that passed between subscribers.
+     * @param callback - function that will be called (if exist), after all subscribers are called. The modified event data form subscribers
+     * will be pass as argument of callback functions.
+     */
+    const emit = (eventName, eventData, callback) => {
+        const eventSubscribers = subscribers[eventName] || [];
+        const eventSubscribersChain = eventSubscribers.reduce((prev, next) => {
+            return prev.then((value) => next(value))
+        }, Promise.resolve(eventData));
+
+        eventSubscribersChain.then(() => {
+            if (angular.isFunction(callback)) {
+                callback(eventData);
+            }
+        });
+    };
+
+    return {
+        subscribe,
+        emit
+    };
+}
