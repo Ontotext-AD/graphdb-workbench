f43b2bfb5ceec364729783cc494be8e536d10d6e
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-05-30 09:12:34 +0300

    - Refactoring onottext-yasgui-web-component's readonly configuration to use constants "WRITE", "READ" or "PROTECTED"; - Refactoring error processing of some functions to return Promise.reject instead throwing of  exception; - Rename the private function "confirm" to be "openConfirmDialog" to not be confused with native "confirm" function; - Removes unused label.

---
+++ src/models/ontotext-yasgui/yasqe-mode.js
@@ -0,0 +1,16 @@
+export const YasqeMode = {
+    /**
+     * The query can be edited.
+     */
+    WRITE: 'WRITE',
+
+    /**
+     * The editor is read-only, but the query can be copied.
+     */
+    READ: 'READ',
+
+    /**
+     * The editor is read-only and the query can't be copied.
+     */
+    PROTECTED: 'PROTECTED'
+}
