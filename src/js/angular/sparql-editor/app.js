cf008c31ee706147081e2a36813ade1fb7ffb1e5
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-03 13:26:15 +0300

    Refactoring sparql template
    ## What
    - Change sparql template view to use yasgui directive instead component directly.
    
    ## Why
    The Yasgui directive was introduced that wrap most of needed configurations of ontotext-yasgui-we-component
    
    ## How
    - Change the templates and the controllers to use the directive.
    
    Additional work:
     - added flag "failOnStatusCode" to delete repository command. It prevents the tests to failed if something is wrong if repository can't delete. This method is used after each test, it is not part of the test, it just should clean backend server after the test.

--- src/js/angular/sparql-editor/app.js
+++ src/js/angular/sparql-editor/app.js
@@ -1,10 +1,8 @@
 import 'angular/sparql-editor/controllers';
-import 'angular/sparql-editor/share-query-link.service';
 import 'angular/core/directives/yasgui-component/yasgui-component.directive';
 
 const modules = [
     'ui.bootstrap',
-    'graphdb.framework.sparql-editor.share-query.service',
     'graphdb.framework.sparql-editor.controllers',
     'graphdb.framework.core.directives.yasgui-component'
 ];
