d8ce5610087c7daa49b724f48b6080296dc1f63d
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-05 16:07:54 +0300

    Refactoring of similarity indexes view
    ## What
     - Defining the similarity search type.
    
    ## Why
    - Type Safety. Improved Code Quality. Performance Optimization.
    
    ## How
    - Extracts all possible values of similarity search type in a constant 'SimilaritySearchType'.

---
+++ src/js/angular/models/similarity/similarity-search-type.js
@@ -0,0 +1,7 @@
+export const SimilaritySearchType = {
+    'SEARCH_TERM': 'searchTerm',
+    'SEARCH_ANALOGICAL': 'searchAnalogical',
+    'SEARCH_ENTITY': 'searchEntity',
+    'SEARCH_ENTITY_PREDICATE': 'searchEntityPredicate',
+    'SEARCH_DOCUMENT': 'searchDocumentID'
+};
