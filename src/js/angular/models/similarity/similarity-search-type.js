0d45df2f0d1c71cc57ac2fefc6249e2d4eb4942a
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-06 15:38:12 +0300

    Refactored the checks for similarity search type
    ## What
    - Refactored the checks for similarity search type.
    
    ## Why
    - Makes code easier to read.
    
    ## How
    - Expose functions that checks the similarity search type.

--- src/js/angular/models/similarity/similarity-search-type.js
+++ src/js/angular/models/similarity/similarity-search-type.js
@@ -3,5 +3,25 @@ export const SimilaritySearchType = {
     'SEARCH_ANALOGICAL': 'searchAnalogical',
     'SEARCH_ENTITY': 'searchEntity',
     'SEARCH_ENTITY_PREDICATE': 'searchEntityPredicate',
-    'SEARCH_DOCUMENT': 'searchDocumentID'
+    'SEARCH_DOCUMENT': 'searchDocumentID',
+
+    'isSearchTermType': (type) => {
+        return SimilaritySearchType.SEARCH_TERM === type;
+    },
+
+    'isSearchAnalogicalType': (type) => {
+        return SimilaritySearchType.SEARCH_ANALOGICAL === type;
+    },
+
+    'isSearchEntityType': (type) => {
+        return SimilaritySearchType.SEARCH_ENTITY === type;
+    },
+
+    'isSearchEntityPredicateType': (type) => {
+        return SimilaritySearchType.SEARCH_ENTITY_PREDICATE === type;
+    },
+
+    'isSearchDocumentType': (type) => {
+        return SimilaritySearchType.SEARCH_DOCUMENT === type;
+    }
 };
