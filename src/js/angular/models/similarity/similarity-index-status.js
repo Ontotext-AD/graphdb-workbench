7bcb4ba688cd252460742168d3dc03bde4ff9c0e
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-06 15:42:54 +0300

    Refactored the checks for similarity search status
    ## What
    - Refactored the checks for similarity search status.
    
    ## Why
    - Makes code easier to read.
    
    ## How
    - Expose functions that checks the similarity search status.

--- src/js/angular/models/similarity/similarity-index-status.js
+++ src/js/angular/models/similarity/similarity-index-status.js
@@ -6,5 +6,37 @@ export const SimilarityIndexStatus = {
     'OUTDATED': 'OUTDATED',
     'INTERRUPTED': 'INTERRUPTED',
     'OBSOLETE': 'OBSOLETE',
-    'FAILED': 'FAILED'
+    'FAILED': 'FAILED',
+
+    'isCreatingStatus': (status) => {
+        return SimilarityIndexStatus.CREATING === status;
+    },
+
+    'isBuildingStatus': (status) => {
+        return SimilarityIndexStatus.BUILDING === status;
+    },
+
+    'isBuiltStatus': (status) => {
+        return SimilarityIndexStatus.BUILT === status;
+    },
+
+    'isRebuildingStatus': (status) => {
+        return SimilarityIndexStatus.REBUILDING === status;
+    },
+
+    'isOutdatedStatus': (status) => {
+        return SimilarityIndexStatus.OUTDATED === status;
+    },
+
+    'isInterruptedStatus': (status) => {
+        return SimilarityIndexStatus.INTERRUPTED === status;
+    },
+
+    'isObsoleteStatus': (status) => {
+        return SimilarityIndexStatus.OBSOLETE === status;
+    },
+
+    'isFailedStatus': (status) => {
+        return SimilarityIndexStatus.FAILED === status;
+    }
 };
