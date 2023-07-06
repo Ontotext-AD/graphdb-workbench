4a5645d9bb3331ba90b9e5d0f81c5ca4500f3e99
Svilen Velikov <svilen.velikov@ontotext.com>
2023-07-06 19:39:41 +0300

    GDB-7731 initial controller refactoring
    ## What
    Moving things around, fixing lint issues, formatting, changing the scope of functions and fields.
    
    ## Why
    The controller needs to be tidied up in order to make it more maintainable and easier to integrate the yasgui component at the end.

---
+++ src/js/angular/utils/string-utils.js
@@ -0,0 +1,16 @@
+/**
+ *  Removes brackets and serial commas from IRIs
+ *  when used to create ids for links and markers,
+ *  because elements with ids that contain such
+ *  are not valid XHTML and cause a host problems
+ *  when trying to style individual elements
+ * @param {string} string The string to be processed.
+ * @return {string} A string where the special characters are removed.
+ */
+const removeSpecialChars = (string) => {
+    return string.replace(/[()']/g, "");
+};
+
+export {
+    removeSpecialChars
+};
