80435c022eb01236c5a1fcf933c80c876a3f611b
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-06 15:29:06 +0300

    Refactored the checks for similarity index type
    ## What
    - Refactored the checks for similarity index type.
    
    ## Why
    - Makes code easier to read.
    
    ## How
    - Expose functions that checks the similarity index type.

--- src/js/angular/models/similarity/similarity-index-type.js
+++ src/js/angular/models/similarity/similarity-index-type.js
@@ -1,5 +1,17 @@
 export const SimilarityIndexType = {
     'TEXT': 'text',
     'TEXT_LITERAL': 'textLiteral',
-    'PREDICATION': 'predication'
+    'PREDICATION': 'predication',
+
+    'isTextType': (type) => {
+        return SimilarityIndexType.TEXT === type;
+    },
+
+    'isTextLiteralType': (type) => {
+        return SimilarityIndexType.TEXT_LITERAL === type;
+    },
+
+    'isPredicationType': (type) => {
+        return SimilarityIndexType.PREDICATION === type;
+    }
 };
