2ad16b317883c060995bc88be9da24269f076694
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-21 15:02:06 +0300

    GDB-7732: Added change repository listener and dirty check functionality
    What
    Added functionality to prevent changing the repository or location without user confirmation.
    
    Why
    - Similarity indexes are specific to each repository, so when a similarity index is open for editing, the repository should not be changed. If this occurs, the user should be notified.
    - If a similarity index is open for editing, or if the create similarity index form is open and the user makes changes in the form but tries to change the location without saving the changes, they should be notified about the unsaved changes.
    
    How
    Added a listener for changes in the repository. Added a listener for changes in the location. Implemented confirmation prompts if any of these events occur.

---
+++ test-cypress/steps/explore/similarity-index-create-steps.js
@@ -0,0 +1,90 @@
+export class SimilarityIndexCreateSteps {
+
+    static visit() {
+        cy.visit('/similarity/index/create');
+    }
+
+    static verifyUrl() {
+        cy.url().should('include', '/similarity/index/create');
+    }
+
+    static getCancelButton() {
+        return cy.get('.cancel-similarity-index-btn');
+    }
+
+    static cancel() {
+        SimilarityIndexCreateSteps.getCancelButton().click();
+    }
+
+    static getCreateButton() {
+        return cy.get('.create-similarity-index-btn');
+    }
+
+    static create() {
+        SimilarityIndexCreateSteps.getCreateButton().click();
+    }
+
+    static getQueryButton() {
+        return cy.get('.save-query-btn');
+    }
+
+    static save() {
+        SimilarityIndexCreateSteps.getQueryButton().click();
+    }
+
+    static getSimilarityIndexNameInput() {
+        return cy.get('.similarity-index-name');
+    }
+
+    static getSemanticVectorsInput() {
+        return cy.get('#indexParameters');
+    }
+
+    static getStopWordsInput() {
+        return cy.get('.stop-words');
+    }
+
+    static getAnalyzerClassInput() {
+        return cy.get('.analyzer-class');
+    }
+
+    static getLiteralIndexCheckbox() {
+        return cy.get('.literal-index');
+    }
+
+    static checkLiteralIndex() {
+        SimilarityIndexCreateSteps.getLiteralIndexCheckbox().check();
+    }
+
+    static getMoreOptions() {
+        return cy.get('.more-options-btn');
+    }
+
+    static showMoreOptions() {
+        SimilarityIndexCreateSteps.getMoreOptions().click();
+    }
+
+    static getSearchQueryTab() {
+        return cy.get('.search-query-tab');
+    }
+
+    static switchToSearchQueryTab() {
+        SimilarityIndexCreateSteps.getSearchQueryTab().click();
+    }
+
+    static getAnalogicalQueryTab() {
+        return cy.get('.analogical-query-tab');
+    }
+
+    static switchToAnalogicalQueryTab() {
+        SimilarityIndexCreateSteps.getAnalogicalQueryTab().click();
+    }
+
+    static getCreatePredictionIndexTab() {
+        return cy.get('#create-predication-index');
+    }
+
+    static switchToCreatePredictionIndexTab() {
+        SimilarityIndexCreateSteps.getCreatePredictionIndexTab().click();
+    }
+}
--- test-cypress/steps/modal-dialog-steps.js
+++ test-cypress/steps/modal-dialog-steps.js
@@ -1,6 +1,8 @@
 import {MainMenuSteps} from "./main-menu-steps";
 import {JdbcCreateSteps} from "./setup/jdbc-create-steps";
 import ImportSteps from "./import-steps";
+import {SimilarityIndexCreateSteps} from "./explore/similarity-index-create-steps";
+import {SimilarityIndexesSteps} from "./explore/similarity-indexes-steps";
 
 export class ModalDialogSteps {
     static getDialog() {
@@ -47,41 +49,71 @@ export class ModalDialogSteps {
         ModalDialogSteps.getCancelButton().click();
     }
 
-    static verifyUrlChangedConfirmation(confirmMessage) {
+    static verifyUrlChangedConfirmation(verifyConfirmationDialogOptions = new VerifyConfirmationDialogOptions()) {
         // and try to change the page
-        MainMenuSteps.clickOnMenuImport();
+        verifyConfirmationDialogOptions.changePageFunction();
 
         // Then I expect to be asked to confirm the leaving tha page.
-        ModalDialogSteps.verifyDialogBody(confirmMessage);
+        ModalDialogSteps.verifyDialogBody(verifyConfirmationDialogOptions.confirmationMessage);
 
         // When I click on close dialog button.
         ModalDialogSteps.clickOnCloseButton();
 
         // Then I expect to stay on same page.
-        JdbcCreateSteps.verifyUrl();
+        verifyConfirmationDialogOptions.verifyCurrentUrl();
+
 
         // When I try to change the page again.
-        MainMenuSteps.clickOnMenuImport();
+        verifyConfirmationDialogOptions.changePageFunction();
 
         // Then I expect to be asked to confirm the leaving tha page.
-        ModalDialogSteps.verifyDialogBody(confirmMessage);
+        ModalDialogSteps.verifyDialogBody(verifyConfirmationDialogOptions.confirmationMessage);
 
         // When I click on cancel dialog button.
         ModalDialogSteps.clickOnCancelButton();
 
         // Then I expect to stay on same page.
-        JdbcCreateSteps.verifyUrl();
+        verifyConfirmationDialogOptions.verifyCurrentUrl();
 
         // When I try to change the page again.
-        MainMenuSteps.clickOnMenuImport();
+        verifyConfirmationDialogOptions.changePageFunction();
 
         // Then I expect to be asked to confirm the leaving tha page.
-        ModalDialogSteps.verifyDialogBody(confirmMessage);
+        ModalDialogSteps.verifyDialogBody(verifyConfirmationDialogOptions.confirmationMessage);
 
         // When I click on confirm dialog button.
         ModalDialogSteps.clickOnConfirmButton();
 
-        // Then I expect to stay on same page.
-        ImportSteps.verifyUserImportUrl();
+        // Then I expect to be redirected.
+        verifyConfirmationDialogOptions.verifyRedirectedUrl();
+    }
+}
+
+export class VerifyConfirmationDialogOptions {
+    constructor() {
+        this.changePageFunction = () => {};
+        this.confirmationMessage = "";
+        this.verifyCurrentUrl = () => {};
+        this.verifyRedirectedUrl = () => {};
+    }
+
+    setChangePageFunction(changePageFunction) {
+        this.changePageFunction = changePageFunction;
+        return this;
+    }
+
+    setConfirmationMessage(confirmationMessage) {
+        this.confirmationMessage = confirmationMessage;
+        return this;
+    }
+
+    setVerifyCurrentUrl(verifyCurrentUrl) {
+        this.verifyCurrentUrl = verifyCurrentUrl;
+        return this;
+    }
+
+    setVerifyRedirectedUrl(verifyRedirectedUrl) {
+        this.verifyRedirectedUrl = verifyRedirectedUrl;
+        return this;
     }
 }
