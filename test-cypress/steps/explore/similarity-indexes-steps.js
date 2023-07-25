2c8990861677ca701af19c78838913249b7482c9
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-25 08:47:12 +0300

    Reorganize similarity tests.

--- test-cypress/steps/explore/similarity-indexes-steps.js
+++ test-cypress/steps/explore/similarity-indexes-steps.js
@@ -7,4 +7,12 @@ export class SimilarityIndexesSteps {
     static verifyUrl() {
         cy.url().should('eq', `${Cypress.config('baseUrl')}/similarity`);
     }
+
+    static getSimilarityIndexRow(similarityIndexName) {
+        return cy.get('.index-row').contains(similarityIndexName).parent().parent();
+    }
+
+    static getEditButton(similarityIndexName) {
+        return SimilarityIndexesSteps.getSimilarityIndexRow(similarityIndexName).find('.edit-query-btn');
+    }
 }
