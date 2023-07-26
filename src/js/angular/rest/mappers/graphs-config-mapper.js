8271822e607889b365699e038197d43b876ab065
Svilen Velikov <svilen.velikov@ontotext.com>
2023-07-26 18:56:13 +0300

    GDB-7731 integrate yasgui in graphs config view
    ## What
    
    ## Why
    
    reorganize graph config controller

---
+++ src/js/angular/rest/mappers/graphs-config-mapper.js
@@ -0,0 +1,22 @@
+import {GraphsConfig} from "../../models/graphs/graphs-config";
+
+export const mapGraphsConfigResponseToModel = (responseData) => {
+    return new GraphsConfig(
+        responseData.id,
+        responseData.name,
+        responseData.startMode,
+        responseData.owner,
+        responseData.startQueryIncludeInferred,
+        responseData.startQuerySameAs,
+        responseData.startGraphQuery,
+        responseData.startIRI,
+        responseData.startIRILabel,
+        responseData.expandQuery,
+        responseData.resourceQuery,
+        responseData.predicateLabelQuery,
+        responseData.resourcePropertiesQuery,
+        responseData.shared,
+        responseData.description,
+        responseData.hint
+    );
+};
--- test-cypress/support/visual-graph-commands.js
+++ test-cypress/support/visual-graph-commands.js
@@ -1,3 +1,6 @@
+export const EXPLORE_GRAPH_URL = '/rest/explore-graph';
+export const GRAPH_CONFIG_URL = `${EXPLORE_GRAPH_URL}/config`;
+
 Cypress.Commands.add('searchEasyVisualGraph', (searchGraph) => {
     cy.get('.search-rdf-resources > #search-resource-box > .form-control')
         .invoke('val', searchGraph)
@@ -14,4 +17,26 @@ Cypress.Commands.add('searchEasyVisualGraph', (searchGraph) => {
                         });
                 });
         });
+});
+
+Cypress.Commands.add('deleteGraphConfig', (name) => {
+    cy.request({
+        method: 'GET',
+        url: GRAPH_CONFIG_URL
+    }).then((response) => {
+        const config = response.body.find((config) => config.name === name);
+        if (!config) {
+            return false;
+        }
+        return cy.request({
+            method: 'DELETE',
+            url: `${GRAPH_CONFIG_URL}/${config.id}`,
+            // Prevent Cypress from failing the test on non-2xx status codes
+            failOnStatusCode: false
+        });
+    }).then((response) => {
+        if (response) {
+            cy.waitUntil(() => response && response.status === 200);
+        }
+    });
 });
