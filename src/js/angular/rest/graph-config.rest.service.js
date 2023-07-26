4612412edea459d6a0869875f3d469468b85a0b6
Svilen Velikov <svilen.velikov@ontotext.com>
2023-07-26 18:56:13 +0300

    GDB-7731 integrate yasgui in graphs config view
    ## What
    
    ## Why
    
    reorganize graph config controller

--- src/js/angular/rest/graph-config.rest.service.js
+++ src/js/angular/rest/graph-config.rest.service.js
@@ -1,9 +1,11 @@
+import {mapGraphsConfigResponseToModel} from "./mappers/graphs-config-mapper";
+
 angular
     .module('graphdb.framework.rest.graphconfig.service', [])
     .factory('GraphConfigRestService', GraphConfigRestService);
 
-const EXPLORE_GRAPH_ENDPOINT = 'rest/explore-graph/';
-const EXPLORE_GRAPH_CONFIG_ENDPOINT = `${EXPLORE_GRAPH_ENDPOINT}config`;
+const EXPLORE_GRAPH_ENDPOINT = 'rest/explore-graph';
+const EXPLORE_GRAPH_CONFIG_ENDPOINT = `${EXPLORE_GRAPH_ENDPOINT}/config`;
 
 GraphConfigRestService.$inject = ['$http'];
 
@@ -24,7 +26,7 @@ function GraphConfigRestService($http) {
     }
 
     function getGraphConfigSamples() {
-        return $http.get(`${EXPLORE_GRAPH_ENDPOINT}samples`);
+        return $http.get(`${EXPLORE_GRAPH_ENDPOINT}/samples`);
     }
 
     function loadGraphForConfig(config, includeInferred, linksLimit, startQuerySameAs) {
@@ -32,14 +34,26 @@ function GraphConfigRestService($http) {
     }
 
     function getConfig(id) {
-        return $http.get(`${EXPLORE_GRAPH_CONFIG_ENDPOINT}/${encodeURIComponent(id)}`);
+        return $http.get(`${EXPLORE_GRAPH_CONFIG_ENDPOINT}/${encodeURIComponent(id)}`)
+            .then((response) => mapGraphsConfigResponseToModel(response.data));
     }
 
+    /**
+     * Creates a new graph config.
+     * @param {GraphsConfig} config
+     * @return {Promise} A Promise which resolves with the result of the saved graph config.
+     */
     function createGraphConfig(config) {
         return $http.post(EXPLORE_GRAPH_CONFIG_ENDPOINT, config);
     }
 
+    /**
+     * Updates an existing graph config.
+     * @param {GraphsConfig} config
+     * @return {Promise} A Promise which resolves with the result of the updated graph config.
+     */
     function updateGraphConfig(config) {
+        console.log('rest update', config);
         return $http.put(`${EXPLORE_GRAPH_CONFIG_ENDPOINT}/${encodeURIComponent(config.id)}`, config);
     }
 
@@ -50,7 +64,7 @@ function GraphConfigRestService($http) {
     function validateQuery(query, queryType, params, all, oneOf) {
         return $http({
             method: 'POST',
-            url: `${EXPLORE_GRAPH_ENDPOINT}validate`,
+            url: `${EXPLORE_GRAPH_ENDPOINT}/validate`,
             data: {
                 query,
                 queryType,
