d6f808b8e29f566bb9a85693317f265de2b213c3
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-27 10:39:39 +0300

    Implements test for the resource view.
    What
    Tests for the resource view have been implemented.
    
    Why
    We want to replace the old implementation of YASGUI with the new one, but there were no tests for this view. Implementing tests will make it easier to verify that the changes do not break the implementation.
    
    How
    Implemented tests.
    
    (cherry picked from commit a87cb7de53706bbc8aa618aac65cde4af908b4fc)

--- test-cypress/steps/main-menu-steps.js
+++ test-cypress/steps/main-menu-steps.js
@@ -9,10 +9,31 @@ export class MainMenuSteps {
     }
 
     static clickOnMenu(menuName) {
-        MainMenuSteps.getMenuButton(menuName).click();
+        // Forced it because some element as "Explore" for example has CSS pointer-events: none
+        MainMenuSteps.getMenuButton(menuName).click({force: true});
     }
 
     static clickOnMenuImport() {
         MainMenuSteps.clickOnMenu('Import');
+    }
+
+    static clickOnExplore() {
+        MainMenuSteps.clickOnMenu('Explore');
+    }
+
+    static getSubMenus() {
+        return cy.get('.sub-menu-item');
+    }
+
+    static getSubMenuButton(submenuName) {
+        return MainMenuSteps.getSubMenus().contains(submenuName);
+    }
+
+    static clickOnSubMenu(submenuName) {
+        MainMenuSteps.getMenuButton(submenuName).click();
+    }
+
+    static clickOnGraphsOverview() {
+        MainMenuSteps.clickOnSubMenu('Graphs overview');
     }
 }
