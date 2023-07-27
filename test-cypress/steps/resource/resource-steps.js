c330b221eaf0cbc2e257014b1b118822b68db472
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-27 10:39:39 +0300

    Implements test for the resource view.
    What
    Tests for the resource view have been implemented.
    
    Why
    We want to replace the old implementation of YASGUI with the new one, but there were no tests for this view. Implementing tests will make it easier to verify that the changes do not break the implementation.
    
    How
    Implemented tests.
    
    (cherry picked from commit a87cb7de53706bbc8aa618aac65cde4af908b4fc)

---
+++ test-cypress/steps/resource/resource-steps.js
@@ -0,0 +1,133 @@
+const VIEW_URL = '/resource';
+
+export class ResourceSteps {
+
+    static visit(parameters) {
+        cy.visit(`${VIEW_URL}${parameters ? ('?' + parameters) : ''}`);
+        // Wait for yasr to be initialized to ensure that the page is loaded.
+        cy.get('.yasr');
+    }
+
+    static verifyUrl() {
+        cy.url().should('include+', `${Cypress.config('baseUrl')}${VIEW_URL}`);
+    }
+
+    static getInferenceSelectElement() {
+        return cy.get('#inference-select');
+    }
+
+    static selectInference(inference = 'Explicit only') {
+        ResourceSteps.getInferenceSelectElement().select(inference);
+    }
+
+    static selectExplicitOnlyInference() {
+        ResourceSteps.selectInference();
+    }
+
+    static selectImplicitOnlyInference() {
+        ResourceSteps.selectInference('Implicit only');
+    }
+
+    static selectExplicitAndImplicitInference() {
+        ResourceSteps.selectInference('Explicit and Implicit');
+    }
+
+    static getSameAsButton() {
+        return cy.get('.same-as-btn');
+    }
+
+    static verifySameAsEnable() {
+        ResourceSteps.getSameAsButton().find('.icon-sameas-on').should('have.length', 1);
+    }
+
+    static verifySameAsDisable() {
+        ResourceSteps.getSameAsButton().find('.icon-sameas-off').should('have.length', 1);
+    }
+
+    static getShowBlankNodesButton() {
+        return cy.get('.show-blank-nodes-btn');
+    }
+
+    static clickOnShowBlankNodesButton() {
+        ResourceSteps.getShowBlankNodesButton().click();
+    }
+
+    static getDownloadAsDropdown() {
+        return cy.get('.dropdown-toggle');
+    }
+
+    static getVisualGraphButton() {
+        return cy.get('.visual-graph-btn');
+    }
+
+    static clickOnVisualGraphButton() {
+        ResourceSteps.getVisualGraphButton().click();
+    }
+
+    static getEditResourceLink() {
+        return cy.get('.edit-resource-link');
+    }
+
+    static clickOnEditResourceLink() {
+        ResourceSteps.getEditResourceLink().click();
+    }
+
+    static getSourceLink() {
+        return cy.get('.source-link');
+    }
+
+    static clickOnSourceLink() {
+        ResourceSteps.getSourceLink().click();
+    }
+
+    static getContextLink() {
+        return cy.get('.context-link');
+    }
+
+    static clickOnContextLink() {
+        ResourceSteps.getContextLink().click();
+    }
+
+    static getRoleSelectionElement() {
+        return cy.get('#selection');
+    }
+
+    static getRoleTab(role = 'subject') {
+        return ResourceSteps.getRoleSelectionElement().find('.nav-link').contains(role);
+    }
+
+    static selectRole(role) {
+        ResourceSteps.getRoleTab(role).click();
+    }
+
+    static selectSubjectRole() {
+        ResourceSteps.selectRole('subject');
+    }
+
+    static selectPredicateRole() {
+        ResourceSteps.selectRole('predicate');
+    }
+
+    static selectObjectRole() {
+        ResourceSteps.selectRole('object');
+    }
+
+    static selectContextRole() {
+        ResourceSteps.selectRole('context');
+    }
+
+    static selectAlRole() {
+        ResourceSteps.selectRole('all');
+    }
+
+    static verifyActiveRoleTab(activeRole = 'subject') {
+        ResourceSteps.getAllRoles().forEach((role) => {
+            const chainer = activeRole === role ? 'have.class' : 'not.have.class';
+            ResourceSteps.getRoleTab(role).should(chainer, 'active');
+        });
+    }
+
+    static getAllRoles() {
+        return ['subject', 'predicate', 'object', 'context', 'all'];
+    }
+}
