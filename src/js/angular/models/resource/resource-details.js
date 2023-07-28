2a8c12c100e47620cd00af7cf1efd6923c50d233
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-07-28 15:24:12 +0300

    Extract resource info model. Extract resource detail model.

---
+++ src/js/angular/models/resource/resource-details.js
@@ -0,0 +1,10 @@
+export class ResourceDetails {
+    constructor() {
+        this.image = undefined;
+        this.uri = undefined;
+        this.label = undefined;
+        this.encodeURI = undefined;
+        this.comment = undefined;
+        this.context = undefined;
+    }
+}
