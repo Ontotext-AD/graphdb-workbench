65c6f78381596e634fb9dc84eeef664dd336c75e
Svilen Velikov <svilen.velikov@ontotext.com>
2023-08-01 15:32:13 +0300

    Cleanup: refactor graph config samples loading and map them to GraphConfig, also initial refactoring of markDirty function

--- src/js/angular/rest/mappers/graphs-config-mapper.js
+++ src/js/angular/rest/mappers/graphs-config-mapper.js
@@ -20,3 +20,33 @@ export const mapGraphsConfigResponseToModel = (responseData) => {
         responseData.hint
     );
 };
+
+export const mapGraphConfigSampleToGraphConfig = (responseData) => {
+    return new GraphsConfig(
+        responseData.id,
+        responseData.name,
+        responseData.startMode,
+        responseData.owner,
+        responseData.startQueryIncludeInferred,
+        responseData.startQuerySameAs,
+        responseData.startGraphQuery,
+        responseData.startIRI,
+        responseData.startIRILabel,
+        responseData.expandQuery,
+        responseData.resourceQuery,
+        responseData.predicateLabelQuery,
+        responseData.resourcePropertiesQuery,
+        responseData.shared,
+        responseData.description,
+        responseData.hint,
+        responseData.startGraphQueryDescription,
+        responseData.expandQueryDescription,
+        responseData.resourceQueryDescription,
+        responseData.predicateLabelQueryDescription,
+        responseData.resourcePropertiesQueryDescription
+    );
+};
+
+export const mapGraphConfigSamplesToGraphConfigs = (responseData) => {
+    return responseData.map((sampleResponse) => mapGraphConfigSampleToGraphConfig(sampleResponse));
+};
