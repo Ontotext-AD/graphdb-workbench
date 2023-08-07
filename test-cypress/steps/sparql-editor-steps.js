b4cc11115e7a1dd5c3f2e1fccb5c5f0500bf518f
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-08-07 20:36:49 +0300

    - Changed imported tests data to include triple resources. - Fixed tests according to new imported data. - Added test for triple resource.

--- test-cypress/steps/sparql-editor-steps.js
+++ test-cypress/steps/sparql-editor-steps.js
@@ -1,8 +1,10 @@
 import {YasqeSteps} from "./yasgui/yasqe-steps";
 
+const VIEW_URL = '/sparql';
+
 export class SparqlEditorSteps {
     static visitSparqlEditorPage() {
-        cy.visit('/sparql');
+        cy.visit(VIEW_URL);
         // Because we use angularjs less than 1.7.3 we use additional library ng-custom-element.umd.js to solve problem with property bindings
         // This library make additional call to ontotext-yasgui-web-component and the component that's way the component renders twice when page loaded.
         // This is bad because in tests cypress can find an element when it is rendered from first call and try to click it for example.
@@ -10,5 +12,9 @@ export class SparqlEditorSteps {
         // We will wait a little , to give a chance page is loaded correctly before test start.
         cy.wait(1000);
         YasqeSteps.waitUntilQueryIsVisible();
+    }
+
+    static verifyUrl() {
+        cy.url().should('include', `${Cypress.config('baseUrl')}${VIEW_URL}`);
     }
 }
