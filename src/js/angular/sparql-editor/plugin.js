b3c92348c56ae1f50ea757cfe2c6b60d758fd53d
Svilen Velikov <svilen.velikov@ontotext.com>
2023-08-08 14:15:14 +0300

    Removed obsolete sparql view heading label and use the original one.

--- src/js/angular/sparql-editor/plugin.js
+++ src/js/angular/sparql-editor/plugin.js
@@ -5,8 +5,8 @@ PluginRegistry.add('route', {
     'chunk': 'sparql',
     'controller': 'SparqlEditorCtrl',
     'templateUrl': 'pages/sparql-editor.html',
-    'title': 'view.sparql-editor.title',
-    'helpInfo': 'view.sparql-editor.helpInfo',
+    'title': 'view.sparql.title',
+    'helpInfo': 'view.sparql.helpInfo',
     'reloadOnSearch': false
 });
 
