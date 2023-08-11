76c1c7bd704af7e760380af7e13cac663e2349d1
Svilen Velikov <svilen.velikov@ontotext.com>
2023-08-11 19:40:14 +0300

    GDB-8597 introduce new workbench themes
    ## What
    * Introduces a new darker and more contrast default theme.
    * Introduces extension point for theme plugins.
    * Implemented two theme plugin definitions: default-theme and onto-original-theme (the old one)
    
    ## Why
    * The goal is to improve the accessibility of the workbench by complying with the WCAG AA standard for color contrast.
    * Ability to easily plug in new themes or to change the existing once.
    
    ## How
    * Implemented extension point and theme plugin definitions.
    * Integrated the themes selector in the settings page.
    * Selected theme is applied on application bootstrap.
    * Implemented a more contrast and WCAG AA standard compliant theme.

---
+++ src/themes/onto-original/plugin.js
@@ -0,0 +1,20 @@
+PluginRegistry.add('themes', {
+    'name': 'onto-original-theme',
+    'label': 'security.workbench.settings.theme.onto-original-theme',
+    'primary-color-hue': '17',
+    'primary-color-saturation': '87.9%',
+    'primary-color-lightness': '49%',
+    'secondary-color-hue': '207.3',
+    'secondary-color-saturation': '100%',
+    'secondary-color-lightness': '19.4%',
+    'tertiary-color-hue': '174.6',
+    'tertiary-color-saturation': '97.7%',
+    'tertiary-color-lightness': '33.5%',
+    'icon-on-primary-color': 'rgba(255, 255, 255, 0.8)',
+    'grey-rgb': '151, 153, 156',
+    'grey-color': '#97999C',
+    'color-danger-dark': '#bd362f',
+    'color-success-dark': '#51a351',
+    'color-warning-dark': '#f89406',
+    'color-info-dark': '#2f96b4'
+});
