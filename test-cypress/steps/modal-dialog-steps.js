f0216906caff4b3e0284cb1a5e739394b6c37ee3
Svilen Velikov <svilen.velikov@ontotext.com>
2023-09-13 10:54:39 +0300

    remove duplicated save button from graph config view and restored the modal-dialog-steps.js file needed by e2e tests

---
+++ test-cypress/steps/modal-dialog-steps.js
@@ -0,0 +1,113 @@
+export class ModalDialogSteps {
+    static getDialog() {
+        return cy.get('.modal-dialog');
+    }
+
+    static getDialogHeader() {
+        return ModalDialogSteps.getDialog().find('.modal-header');
+    }
+
+    static getCloseButton() {
+        return ModalDialogSteps.getDialogHeader().find('.close');
+    }
+
+    static clickOnCloseButton() {
+        ModalDialogSteps.getCloseButton().click();
+    }
+
+    static getDialogBody() {
+        return ModalDialogSteps.getDialog().find('.modal-body');
+    }
+
+    static verifyDialogBody(message) {
+        ModalDialogSteps.getDialogBody().contains(message);
+    }
+
+    static getDialogFooter() {
+        return ModalDialogSteps.getDialog().find('.modal-footer');
+    }
+
+    static getConfirmButton() {
+        return ModalDialogSteps.getDialogFooter().find('.confirm-btn');
+    }
+
+    static clickOnConfirmButton() {
+        ModalDialogSteps.getConfirmButton().click();
+    }
+
+    static getCancelButton() {
+        return ModalDialogSteps.getDialogFooter().find('.cancel-btn');
+    }
+
+    static clickOnCancelButton() {
+        ModalDialogSteps.getCancelButton().click();
+    }
+
+    static verifyUrlChangedConfirmation(verifyConfirmationDialogOptions = new VerifyConfirmationDialogOptions()) {
+        // and try to change the page
+        verifyConfirmationDialogOptions.changePageFunction();
+
+        // Then I expect to be asked to confirm the leaving tha page.
+        ModalDialogSteps.verifyDialogBody(verifyConfirmationDialogOptions.confirmationMessage);
+
+        // When I click on close dialog button.
+        ModalDialogSteps.clickOnCloseButton();
+
+        // Then I expect to stay on same page.
+        verifyConfirmationDialogOptions.verifyCurrentUrl();
+
+
+        // When I try to change the page again.
+        verifyConfirmationDialogOptions.changePageFunction();
+
+        // Then I expect to be asked to confirm the leaving tha page.
+        ModalDialogSteps.verifyDialogBody(verifyConfirmationDialogOptions.confirmationMessage);
+
+        // When I click on cancel dialog button.
+        ModalDialogSteps.clickOnCancelButton();
+
+        // Then I expect to stay on same page.
+        verifyConfirmationDialogOptions.verifyCurrentUrl();
+
+        // When I try to change the page again.
+        verifyConfirmationDialogOptions.changePageFunction();
+
+        // Then I expect to be asked to confirm the leaving tha page.
+        ModalDialogSteps.verifyDialogBody(verifyConfirmationDialogOptions.confirmationMessage);
+
+        // When I click on confirm dialog button.
+        ModalDialogSteps.clickOnConfirmButton();
+
+        // Then I expect to be redirected.
+        verifyConfirmationDialogOptions.verifyRedirectedUrl();
+    }
+}
+
+export class VerifyConfirmationDialogOptions {
+    constructor() {
+        this.changePageFunction = () => {};
+        this.confirmationMessage = "";
+        this.verifyCurrentUrl = () => {};
+        this.verifyRedirectedUrl = () => {};
+    }
+
+    setChangePageFunction(changePageFunction) {
+        this.changePageFunction = changePageFunction;
+        return this;
+    }
+
+    setConfirmationMessage(confirmationMessage) {
+        this.confirmationMessage = confirmationMessage;
+        return this;
+    }
+
+    setVerifyCurrentUrl(verifyCurrentUrl) {
+        this.verifyCurrentUrl = verifyCurrentUrl;
+        return this;
+    }
+
+    setVerifyRedirectedUrl(verifyRedirectedUrl) {
+        this.verifyRedirectedUrl = verifyRedirectedUrl;
+        return this;
+    }
+}
