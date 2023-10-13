a614f7d6b2712de7136918d9668da7827b33b848
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-10-13 10:12:00 +0300

    Updates version of ontotext-yasgui-web-component
    ## What
    - GDB-8438: "YASR: Update error plugin". When exception is thrown after executing of sparql query, the workbench shows the error. But if the error is too big the error area is too long. Most of the users do not need to see the entire message;
    - GDB-8188: "Buttons labels of dialogs are not translated". Some labels are not translated in French;
    - GDB-8185: "Тhe button for "Copy URL to clipboard" is not visible on Browser Firefox". When hovering over a URI in Yasr, the "Copy URL to clipboard" button is not visible in the Firefox browser.
    
    ## Why
    - WB displays all error messages no matter of its length;
    - Missed label translation;
    - The CSS selector that displays the URI includes a ":has" selector, which is not supported by the Firefox browser.
    
    ## How
    Updates version of ontotext-yasgui-web-component.

--- test-cypress/steps/yasgui/plugin/error-plugin-steps.js
+++ test-cypress/steps/yasgui/plugin/error-plugin-steps.js
@@ -4,7 +4,27 @@ export class ErrorPluginSteps {
         return cy.get('.error-response-plugin');
     }
 
+    static getErrorPluginHeader() {
+        return ErrorPluginSteps.getErrorPlugin().find('.error-response-plugin-header');
+    }
+
+    static getErrorPluginErrorStatus() {
+        return ErrorPluginSteps.getErrorPluginHeader().find('.error-response-plugin-error-status');
+    }
+
+    static getErrorPluginErrorTimeMessage() {
+        return ErrorPluginSteps.getErrorPluginHeader().find('.error-response-plugin-error-time-message');
+    }
+
     static getErrorPluginBody() {
         return ErrorPluginSteps.getErrorPlugin().find('.error-response-plugin-body');
+    }
+
+    static getShowFullErrorMessage() {
+        return cy.get('.show-full-message-link');
+    }
+
+    static getShowLessErrorMessage() {
+        return cy.get('.show-less-message-link');
     }
 }
