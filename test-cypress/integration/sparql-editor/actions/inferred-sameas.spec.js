8413ec8cdad2a7f700a33c83ae3911cab368a625
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-10-17 16:19:13 +0300

    GDB-8267: Disable toggling of infer and sameAs buttons on Virtual repository
    ## What
    When repository is virtual. the inferred and sameAs buttons have to be active and should not be toggleable.
    
    ## Why
    Missed requirement during development.
    
    ## How
    The version of "ontotext-yasgui-web-component" has been updated.
    Added styling when infer and sameAs are disabled.

---
+++ test-cypress/integration/sparql-editor/actions/inferred-sameas.spec.js
@@ -0,0 +1,47 @@
+import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
+import {YasqeSteps} from "../../../steps/yasgui/yasqe-steps";
+import {RepositoriesStub} from "../../../stubs/repositories-stub";
+
+describe('Expand results over owl:sameAs', () => {
+
+    let repositoryId;
+
+    beforeEach(() => {
+        repositoryId = 'sparql-editor-' + Date.now();
+        cy.setLocalStorage("ls.repository-id", repositoryId);
+        RepositoriesStub.stubOntopRepository(repositoryId);
+        RepositoriesStub.stubNameSpaces(repositoryId);
+    });
+
+    it('should not be able to toggle the sameAs button state if repository is virtual', () => {
+        // When I open the editor configured for virtual repository.
+        SparqlEditorSteps.visitSparqlEditorPage();
+
+        // Then I expect sameAs button to be on.
+        YasqeSteps.getExpandResultsOverSameAsButtonTooltip().should('have.attr', 'data-tooltip', 'Expand results over owl:sameAs: ON');
+        YasqeSteps.getExpandResultsOverSameAsButton().should('have.class', 'icon-same-as-on');
+
+        // When I click on inferred button
+        YasqeSteps.getExpandResultsOverSameAsButton().click({force: true});
+
+        // Then I expect sameAs button to not be toggled.
+        YasqeSteps.getExpandResultsOverSameAsButtonTooltip().should('have.attr', 'data-tooltip', 'Expand results over owl:sameAs: ON');
+        YasqeSteps.getExpandResultsOverSameAsButton().should('have.class', 'icon-same-as-on');
+    });
+
+    it('should not be able to toggle the inferred button state if repository is virtual', () => {
+        // When I open the editor configured for virtual repository.
+        SparqlEditorSteps.visitSparqlEditorPage();
+
+        // Then I expect inferred button to be on.
+        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'data-tooltip', 'Include inferred data in results: ON');
+        YasqeSteps.getActionButton(3).should('have.class', 'icon-inferred-on');
+
+        // When I click on inferred button
+        YasqeSteps.getActionButton(3).click({force: true});
+
+        // Then I expect inferred button to not be toggled.
+        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'data-tooltip', 'Include inferred data in results: ON');
+        YasqeSteps.getActionButton(3).should('have.class', 'icon-inferred-on');
+    });
+});
