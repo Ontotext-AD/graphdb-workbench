ab28eff09b9e4b38046f2a0e7af2e37a8a689bbf
Yordan Alexandrov <yordan.alexandrov@ontotext.com>
2023-10-19 14:53:37 +0300

    GDB-8986 Fix svg downloads
    ## What
    On pages class relationships, class hierarchy and domain range graph, the export svg button yields a broken file
    
    ## Why
    The selector for the svg export is too broad "svg" and selects all svgs on the screen
    
    ## How
    - added a parameter to the export function to take a selector and passed appropriate selectors

--- src/js/lib/common/svg-export.js
+++ src/js/lib/common/svg-export.js
@@ -1,22 +1,26 @@
+import * as d3 from "d3/build/d3";
+
 var SVG = SVG || {};
 
 SVG.Export = function () {
     function getCSSRules(cssSourceFilePath) {
-        var cssRules = $('link[href="' + cssSourceFilePath + '"]')[0].sheet.rules;
-        var cssStr = "";
+        const cssRules = $('link[href="' + cssSourceFilePath + '"]')[0].sheet.rules;
+        let cssStr = "";
         _.each(cssRules, function (value) {
             cssStr += value.cssText;
         });
         return cssStr;
     }
 
-    function generateBase64ImageSource() {
+    function generateBase64ImageSource(svgSelectorClass) {
+        if (!svgSelectorClass) {
+            svgSelectorClass = "svg";
+        }
+        console.log(svgSelectorClass);
         // add needed xml namespace
-        var exportSvg = d3.selectAll("svg")
-            .attr({
-                version: "1.1",
-                xmlns: "http://www.w3.org/2000/svg"
-            })
+        const exportSvg = d3.selectAll(svgSelectorClass)
+            .attr("version", "1.1")
+            .attr("xmlns", "http://www.w3.org/2000/svg")
             .node().parentNode.innerHTML;
 
         // convert selected html to base64
