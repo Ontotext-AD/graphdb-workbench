7922bbff67f184a747ff652012e3bfd51ee65b1a
martina.dimova <martina.dimova@ontotext.com>
2023-12-13 21:29:39 +0200

    GDB-9296 Improve error message when attempting to change security without total quorum
    * Made toast message extract error text message from its data.
    
    * Escaped close to immediate refreshing of security so toast message is visible.

--- src/js/angular/core/services/jwt-auth.service.js
+++ src/js/angular/core/services/jwt-auth.service.js
@@ -240,7 +240,7 @@ angular.module('graphdb.framework.core.services.jwtauth', [
                         toastr.success($translate.instant('jwt.auth.security.status', {status: ($translate.instant(enabled ? 'enabled.status' : 'disabled.status'))}));
                         AuthTokenService.clearAuthToken();
                         that.initSecurity();
-                        this.securityEnabled = enabled;
+                        that.securityEnabled = enabled;
                     }, function (err) {
                         toastr.error(err.data, $translate.instant('common.error'));
                     });
