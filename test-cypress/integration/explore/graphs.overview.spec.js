0078a9f587748d3710b64757533ca537a6ceb2a1
Vesela Petrova <vesela.petrova@ontotext.com>
2024-01-11 16:02:42 +0200

    GDB-9211: added a context link in the test case

--- test-cypress/integration/explore/graphs.overview.spec.js
+++ test-cypress/integration/explore/graphs.overview.spec.js
@@ -126,7 +126,7 @@ describe('Graphs overview screen validation', () => {
         cy.get('.export-repository-btn').click();
         cy.get('.export-repo-format-JSONLD').click();
         cy.get('[id=wb-JSONLD-mode]').select(5);
-        cy.get('[id=wb-JSONLD-context]').type('');
+        cy.get('[id=wb-JSONLD-context]').type('https://w3c.github.io/json-ld-api/tests/compact/0007-context.jsonld');
         cy.get('[id=wb-export-JSONLD]').click();
         cy.get('.modal-content').should('not.exist');
     });
