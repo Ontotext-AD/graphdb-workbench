be2f10f154d3fc762cc5e412799748d9a70bfabc
desislava.borisova <desislava.borisova@ontotext.com>
2024-03-12 16:44:42 +0200

    GDB-9657 - add methods for checking the result type, in order to match search type checks

--- src/js/angular/models/similarity/similarity-result-type.js
+++ src/js/angular/models/similarity/similarity-result-type.js
@@ -1,5 +1,13 @@
 export const SimilarityResultType = {
     'TERM_RESULT': 'termResult',
     'DOCUMENT_RESULT': 'documentResult',
-    'ENTITY_RESULT': 'entityResult'
+    'ENTITY_RESULT': 'entityResult',
+
+    'isResultDocumentType': (type) => {
+        return SimilarityResultType.DOCUMENT_RESULT === type;
+    },
+
+    'isResultTermType': (type) => {
+        return SimilarityResultType.TERM_RESULT === type;
+    }
 };
