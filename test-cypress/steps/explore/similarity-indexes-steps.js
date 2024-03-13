7091dbb81c481480c767c3cd0ffa9f9799db9d6f
Desi Borisova <158429017+DesiBorisova@users.noreply.github.com>
2024-03-13 14:20:32 +0200

    GDB-9657 - Set the Similarity search result type correctly (#1317)
    * GDB-9657 - add method for setting the result type correctly
    
    * GDB-9657 - add methods for checking the result type, in order to match search type checks
    
    * GDB-9657 - extend test to include search and result type dropdowns. Added classes in view. Steps extended.

--- test-cypress/steps/explore/similarity-indexes-steps.js
+++ test-cypress/steps/explore/similarity-indexes-steps.js
@@ -15,4 +15,30 @@ export class SimilarityIndexesSteps {
     static getEditButton(similarityIndexName) {
         return SimilarityIndexesSteps.getSimilarityIndexRow(similarityIndexName).find('.edit-query-btn');
     }
+
+    static getSearchTypeButton() {
+        return cy.get('.search-type-btn');
+    }
+
+    static getResultTypeButton() {
+        return cy.get('.result-type-btn');
+    }
+
+    static clickSearchTypeDropdown() {
+        this.getSearchTypeButton().click();
+    }
+
+    static clickResultTypeDropdown() {
+        this.getResultTypeButton().click();
+    }
+
+    static selectSearchTypeOption(type) {
+        this.clickSearchTypeDropdown();
+        cy.get('.dropdown-search-type .dropdown-item').contains(type).click();
+    }
+
+    static selectResultTypeOption(type) {
+        this.clickResultTypeDropdown();
+        cy.get('.dropdown-result-type .dropdown-item').contains(type).click();
+    }
 }
