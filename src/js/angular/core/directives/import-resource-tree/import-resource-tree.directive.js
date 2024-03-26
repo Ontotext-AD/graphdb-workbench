5d1a3d7cd9d8df91c5ba8c30ce85d276ba44eea2
boyan.toncev <boyan.tochev@ontotext.com>
2024-03-26 16:10:44 +0200

    What This update introduces the 'import-resource-tree' directive, designed to present RDF resources in a structured manner.
    Why
    The directive enhances the user experience by providing a clear and organized view of imported RDF resources.
    
    How
    The 'import-resource-tree' directive has been developed to facilitate the display of RDF resources. It requires an 'ImportResourceTreeElement' instance representing the resource tree, along with two callback functions: 'onImport' for handling import actions and 'onDelete' for managing deletion operations when users interact with the import or delete buttons.

---
+++ src/js/angular/core/directives/import-resource-tree/import-resource-tree.directive.js
@@ -0,0 +1,71 @@
+import 'angular/core/directives/import-resource-tree/recursive.directive';
+
+const modules = [
+    'graphdb.framework.core.directives.recursive'];
+
+angular
+    .module('graphdb.framework.core.directives.import-resource-tree', modules)
+    .directive('importResourceTree', importResourceTreeDirective);
+
+
+importResourceTreeDirective.$inject = [];
+
+function importResourceTreeDirective() {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/angular/core/directives/import-resource-tree/templates/import-resource-tree.html',
+        scope: {
+
+            /**
+             * @type {ImportResourceTreeElement}
+             */
+            importResources: '=',
+            onImport: '&',
+            onDelete: '&'
+        },
+        link: ($scope, element, attrs) => {
+            // =========================
+            // Public variables
+            // =========================
+            $scope.indent = 30;
+
+            // =========================
+            // Private variables
+            // =========================
+            let selectedResources = [];
+
+            // =========================
+            // Private functions
+            // =========================
+            const init = () => {
+                selectedResources.forEach((resource) => {
+                    const resourceByFullPath = $scope.importResources.getResourceName(resource.importResource.name);
+                    if (resourceByFullPath) {
+                        resourceByFullPath.selected = true;
+                    }
+                });
+
+                $scope.resources = $scope.importResources.toList();
+            };
+
+            $scope.selectionChanged = (($event, resource) => {
+                resource.setSelection(resource.selected);
+                selectedResources = $scope.importResources.getAllSelected();
+            });
+
+            // =========================
+            // Subscriptions
+            // =========================
+            const subscriptions = [];
+
+            subscriptions.push($scope.$watch('importResources', init));
+
+            const removeAllSubscribers = () => {
+                subscriptions.forEach((subscription) => subscription());
+            };
+
+            // Deregister the watcher when the scope/directive is destroyed
+            $scope.$on('$destroy', removeAllSubscribers);
+        }
+    };
+}
--- src/js/angular/import/app.js
+++ src/js/angular/import/app.js
@@ -5,6 +5,7 @@ import 'angular/import/directives';
 import 'angular/core/services/repositories.service';
 import 'ng-file-upload/dist/ng-file-upload.min';
 import 'ng-file-upload/dist/ng-file-upload-shim.min';
+import 'angular/core/directives/import-resource-tree/import-resource-tree.directive';
 
 const modules = [
     'toastr',
@@ -14,7 +15,8 @@ const modules = [
     'graphdb.framework.impex.import.directives',
     'graphdb.framework.impex.import.controllers',
     'graphdb.framework.core.services.repositories',
-    'graphdb.framework.core.directives'
+    'graphdb.framework.core.directives',
+    'graphdb.framework.core.directives.import-resource-tree'
 ];
 
 angular.module('graphdb.framework.impex.import', modules);
