73fe7d120cf07dba3858f0aac882dfa4e0c9b80d
Desi Borisova <158429017+DesiBorisova@users.noreply.github.com>
2024-03-27 17:59:43 +0200

    Revert "GDB 9664 Configure Swagger5 (#1324)" (#1330)
    This reverts commit f543bbb33ad26359ce82eb166cc07a1b8810baec.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -249,7 +249,7 @@ module.exports = {
                     loader: "file-loader",
                     options: {
                         name: '[name].[ext]',
-                        outputPath: 'res/swagger5/fonts'
+                        outputPath: 'res/swagger/fonts'
                     }
                 }
             }, {
