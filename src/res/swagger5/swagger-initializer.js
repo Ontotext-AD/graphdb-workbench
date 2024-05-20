3f2f2b831e51592597a700afb6d3d8be0e1d9857
boyantonchev <boyan.tonchev@ontotext.com>
2024-05-20 11:42:51 +0300

    This is the initial commit of task GDB-934.
    The Swagger implementation of cURL generation is as follows:
    
    The user clicks on the "Try it out" button of an endpoint.
    The user fills out the form (with at least all required fields).
    The user clicks on the "Execute" button.
    3.1. The onExecute function is called (located in "src/components/execute.jsx" in the Swagger project). It validates all filled properties.
    3.1.1. If there are errors, Swagger UI marks all incorrectly filled fields.
    3.1.2. If there are no errors, the state property "executeInProgress" of the component is changed, triggering the execution to proceed.
    3.2. The action "execute" is called (located in "src/core/plugins/spec/actions"). This function prepares some initial data needed for the actual action execution "executeRequest".
    3.2.1. In the "executeRequest" function, the passed req object is updated with fields needed for the actual server request.
    3.2.2. From the req object, a new "parsedRequest" object is created and stored in the store (specActions.setRequest(req.pathName, req.method, parsedRequest)).
    3.2.3. A request interceptor is added to the req object. This interceptor calls an external function (if provided) that can modify the request. THIS IS THE OBJECT NEEDED FOR cURL GENERATION.
    3.2.4. The backend request is called. As part of this call, the interceptor from 3.2.3 is executed.
    3.2.5. When the request is finished, the state is updated with the received response.
    3.2.6. Updating the state with the response triggers Swagger UI to display the cURL and the returned response.
    The Swagger UI API has many hooks where modifications can be made, but there is no way to stop the backend call and display only the generated cURL.
    
    This commit extends Swagger UI with the functionality to generate cURL without making a backend call.
    
    The "extendExecuteFunction" extends and modifies the "executeRequest" function from 3.2.1:
    
    executeRequest: changes are made to the function here;
    setRequest: this action is called with all needed parameters for cURL generation;
    afterLoad: this function is loaded once per Swagger UI instance. Here, a reference to the original function that calls the backend is made. A new function is set, which just returns a promise rejection instead of calling the backend.
    The "extendTryItOutButton" function is just an example of how a new button can be added next to "Try it out".
    
    What needs to be implemented:
    
    When we want to generate cURL, we need to set some parameters to state and then proceed with the execution.
    In the new function added in afterLoad, we need to decide whether to call the backend based on the previous parameter.
    In the setRequest, we need to clear the store.

--- src/res/swagger5/swagger-initializer.js
+++ src/res/swagger5/swagger-initializer.js
@@ -1,4 +1,116 @@
-window.onload = function() {
+window.onload = function () {
+
+    function extendExecuteFunction() {
+        return {
+            statePlugins: {
+                spec: {
+                    wrapActions: {
+                        executeRequest: (originalAction, system) => (req) => {
+                            originalAction(req).then(response => {
+                                const executionOff = true;
+                                if (executionOff) {
+                                    let path = req.pathName;
+                                    let method = req.method;
+                                    // system.specActions.clearRequest(path, method);
+                                    // system.specActions.clearResponse(path, method);
+                                    // system.specActions.updateLoadingStatus(null);
+                                }
+                                return response;
+                            }).catch(error => {
+                                console.error("Error in executeRequest", error);
+                                throw error;
+                            });
+                        },
+                        setRequest: (originalAction, system) => (path, method, request) => {
+                            console.log("Generated cURL Command:", system.fn.requestSnippetGenerator_curl_bash(system.Im.fromJS(request)));
+                            // calculate it dynamically
+                            const executionOff = true;
+                            if (executionOff) {
+                                let parsedMutatedRequest = Object.assign({}, request)
+                                system.specActions.setMutatedRequest(path, method, parsedMutatedRequest);
+                            }
+                            // Call the original action
+                            return originalAction(path, method, request);
+                        }
+                    }
+                }
+            },
+            afterLoad(system) {
+                let originExecute = system.fn['execute'];
+                system.fn['execute'] = (req) => {
+                    // calculate it dynamically
+                    const executionOff = true;
+                    if (executionOff) {
+                        return Promise.reject({});
+                    }
+                    return originExecute(req);
+                }
+            }
+        };
+    }
+
+    function extendTryItOutButton(system) {
+        return {
+            // Specify the name of the plugin
+            name: 'extend-try-it-out-button-plugin',
+
+            // Hook into the Swagger UI initialization process
+            wrapComponents: {
+                // Add custom logic to the TryItOutButton component
+                TryItOutButton: (Original, system) => {
+                    return class extends Original {
+                        render() {
+
+                            // Call the original render method to get the original renderer
+                            const originalRenderer = Original.prototype.render.call(this);
+                            // Define a function to generate cURL command
+                            const generateCurl = () => {
+
+                                console.log('################# Before render ########################3');
+                                console.log('Props: ', this.props);
+                                console.log('Spec: ', system.spec);
+
+                                let state = system.getState();
+                                console.log(state);
+
+                                let spec = state.get('spec');
+                                console.log(spec);
+
+
+                                const req = {
+                                    url: "http://example.com",
+                                    method: "POST",
+                                    body: JSON.stringify({
+                                        id: 0,
+                                        name: "doggie",
+                                        status: "available"
+                                    }, null, 2),
+                                    headers: {
+                                        Accept: "application/json",
+                                        "content-type": "application/json"
+                                    }
+                                };
+
+                                let curlCommand = system.fn.requestSnippetGenerator_curl_bash(system.Im.fromJS(req));
+                                // Display the cURL command
+                                console.log('Generated cURL Command:', curlCommand);
+                            };
+
+                            // Create your custom button element for generating cURL command
+                            const CurlButton = system.React.createElement('button', {
+                                onClick: generateCurl,
+                                style: {marginLeft: '8px'} // Add margin to the left for space
+                            }, 'Generate cURL');
+
+                            // Return the original rendering and the custom buttons wrapped in a container with flexbox layout
+                            return system.React.createElement('div', {style: {display: 'flex'}}, originalRenderer, CurlButton);
+                        }
+                    };
+                }
+            }
+        };
+    }
+
     window.uiRdf4j = SwaggerUIBundle({
         url: "../../rest/api/rdf4j",
         name: "RDF4J API",
@@ -19,7 +131,8 @@ window.onload = function() {
             SwaggerUIStandalonePreset
         ],
         plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl
+            SwaggerUIBundle.plugins.DownloadUrl,
+            extendExecuteFunction
         ],
         layout: "StandaloneLayout"
     });
@@ -44,8 +157,10 @@ window.onload = function() {
             SwaggerUIStandalonePreset
         ],
         plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl
+            SwaggerUIBundle.plugins.DownloadUrl,
+            extendExecuteFunction
         ],
         layout: "StandaloneLayout"
     });
+
 };
