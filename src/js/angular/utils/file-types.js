e20a8e2d56ffe375de3c8ae9c740cad30437acbe
Desi Borisova <158429017+DesiBorisova@users.noreply.github.com>
2024-06-06 15:24:32 +0300

    GDB-10301 - Enable the inference and sameAs for Ontop repo. Increase Yasgui version (#1420)
    * GDB-10301 - change Yasgui method name and add reset flag class
    
    GDB-10301 - increase Yasgui version and rename reset flags for consistency
    
    * GDB-10301 - increase Yasgui version and add translation for export menus to match Yasgui

--- src/js/angular/utils/file-types.js
+++ src/js/angular/utils/file-types.js
@@ -1,16 +1,16 @@
 angular.module('graphdb.workbench.utils.filetypes', [])
     .value('FileTypes', [
-        {name: 'JSON', type: 'application/rdf+json', extension: '.json'},
-        {name: 'JSON-LD', type: 'application/ld+json', extension: '.jsonld'},
-        {name: 'NDJSON-LD', type: 'application/x-ld+ndjson', extension: '.ndjsonld'},
-        {name: 'RDF-XML', type: 'application/rdf+xml', extension: '.rdf'},
-        {name: 'N3', type: 'text/rdf+n3', extension: '.n3'},
-        {name: 'N-Triples', type: 'text/plain', extension: '.nt'},
-        {name: 'N-Quads', type: 'text/x-nquads', extension: '.nq'},
-        {name: 'Turtle', type: 'text/turtle', extension: '.ttl'},
-        {name: 'Turtle*', type: 'application/x-turtlestar', extension: '.ttls'},
-        {name: 'TriX', type: 'application/trix', extension: '.trix'},
-        {name: 'TriG', type: 'application/x-trig', extension: '.trig'},
-        {name: 'TriG*', type: 'application/x-trigstar', extension: '.trigs'},
-        {name: 'Binary RDF', type: 'application/x-binary-rdf', extension: '.brf'}
+        {name: 'JSON', translateKey: 'download.as.json', type: 'application/rdf+json', extension: '.json'},
+        {name: 'JSON-LD', translateKey: 'download.as.jsonld', type: 'application/ld+json', extension: '.jsonld'},
+        {name: 'NDJSON-LD', translateKey: 'download.as.ndjsonld', type: 'application/x-ld+ndjson', extension: '.ndjsonld'},
+        {name: 'RDF-XML', translateKey: 'download.as.rdfxml', type: 'application/rdf+xml', extension: '.rdf'},
+        {name: 'N3', translateKey: 'download.as.n3', type: 'text/rdf+n3', extension: '.n3'},
+        {name: 'N-Triples', translateKey: 'download.as.ntriples', type: 'text/plain', extension: '.nt'},
+        {name: 'N-Quads', translateKey: 'download.as.nquads', type: 'text/x-nquads', extension: '.nq'},
+        {name: 'Turtle', translateKey: 'download.as.turtle', type: 'text/turtle', extension: '.ttl'},
+        {name: 'Turtle*', translateKey: 'download.as.turtlestar', type: 'application/x-turtlestar', extension: '.ttls'},
+        {name: 'TriX', translateKey: 'download.as.trix', type: 'application/trix', extension: '.trix'},
+        {name: 'TriG', translateKey: 'download.as.trig', type: 'application/x-trig', extension: '.trig'},
+        {name: 'TriG*', translateKey: 'download.as.trigstar', type: 'application/x-trigstar', extension: '.trigs'},
+        {name: 'Binary RDF', translateKey: 'download.as.binaryrdf', type: 'application/x-binary-rdf', extension: '.brf'}
     ]);
