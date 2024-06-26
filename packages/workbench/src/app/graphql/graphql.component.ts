407be2b7cfca0e2f6796fdab333acb8f8ada0296
svilen.velikov <svilen.velikov@ontotext.com>
2024-06-26 16:11:06 +0300

    Created new project with Angular18 for the new workbench
    (cherry picked from commit a35fb20ca61ff948b4a463ebc2904714f16c227f)

---
+++ packages/workbench/src/app/graphql/graphql.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphql',
+  standalone: true,
+  imports: [],
+  templateUrl: './graphql.component.html',
+  styleUrl: './graphql.component.scss'
+})
+export class GraphqlComponent {
+
+}
