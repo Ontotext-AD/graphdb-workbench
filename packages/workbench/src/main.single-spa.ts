3303176e68e7c49cf0fb4dcf4f9c0ce6a6d56658
svilen.velikov <svilen.velikov@ontotext.com>
2024-06-26 16:11:06 +0300

    Created new project with Angular18 for the new workbench

---
+++ packages/workbench/src/main.single-spa.ts
@@ -0,0 +1,30 @@
+import './../node_modules/zone.js';
+import { enableProdMode, NgZone } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Router, NavigationStart } from '@angular/router';
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+// bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+  },
+  template: '<app-root />',
+  Router,
+  NavigationStart,
+  NgZone,
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
