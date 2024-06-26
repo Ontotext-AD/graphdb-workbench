eec179bd90a58283c7fdadc4197b3848a5ccdc4e
svilen.velikov <svilen.velikov@ontotext.com>
2024-06-26 16:11:06 +0300

    Created new project with Angular18 for the new workbench
    (cherry picked from commit a35fb20ca61ff948b4a463ebc2904714f16c227f)

---
+++ packages/workbench/src/app/sparql/sparql.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sparql',
+  standalone: true,
+  imports: [],
+  templateUrl: './sparql.component.html',
+  styleUrl: './sparql.component.scss'
+})
+export class SparqlComponent {
+
+}
