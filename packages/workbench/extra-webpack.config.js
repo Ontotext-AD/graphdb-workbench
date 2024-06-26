568ef7dfc718614074f7b0989ac720e5e1e3f5ea
svilen.velikov <svilen.velikov@ontotext.com>
2024-06-26 16:11:06 +0300

    Created new project with Angular18 for the new workbench
    (cherry picked from commit a35fb20ca61ff948b4a463ebc2904714f16c227f)

---
+++ packages/workbench/extra-webpack.config.js
@@ -0,0 +1,11 @@
+const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+
+module.exports = (config, options) => {
+  const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
+
+  // Ensure Zone.js is included
+  // singleSpaWebpackConfig.externals = singleSpaWebpackConfig.externals || {};
+  // delete singleSpaWebpackConfig.externals['zone.js'];
+  // Feel free to modify this webpack config however you'd like to
+  return singleSpaWebpackConfig;
+};
