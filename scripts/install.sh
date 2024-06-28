8ab37f290f249c36371ead3c44b46fb9118b5289
svilen.velikov <svilen.velikov@ontotext.com>
2024-06-28 10:06:17 +0300

    GDB-10373 legacy workbench as single spa (#1451)
    * Upgrade of webpack to latest version 5 which we need as precondition for running module federation.
    
    * Upgraded webpack and webpack-cli
    * Upgraded all used webpack plugins and loaders
    
    * Changed the legacy workbench to be single spa module
    
    * Added build scripts
    
    * Switch order of main and vendor bundles import as this affects the css order that is important.
    Removed hardcoded rest url.
    
    * Add some minor adjustments in formating and added test navigation links that work in different ways - these will be changed later.
    
    ---------
    
    Co-authored-by: boyantonchev <boyan.tonchev@ontotext.com>
    
    (cherry picked from commit 3da20cf0fa57d6a68c8c550e3bebcb09261e949f)

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+echo ''
+echo '########################   Installing -- legacy-workbench --   ###########################'
+echo ''
+
+cd packages/legacy-workbench
+npm install
+
+echo ''
+echo '########################   Installing -- root-config --   ###########################'
+echo ''
+
+cd ..
+cd root-config
+npm install
+
+echo ''
+echo '########################   Installing -- workbench --   ###########################'
+echo ''
+
+cd ..
+cd workbench
+npm install
