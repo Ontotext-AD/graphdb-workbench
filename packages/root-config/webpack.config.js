dc5d2db591982b9991a865a4b014eac70284815c
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2024-07-05 13:46:19 +0300

    Removes hardcoded host and port from the legacy workbench's app.js file. (#1457)
    (cherry picked from commit f008c52033dd9716f94923feeb6f825cdbafd439)

--- packages/root-config/webpack.config.js
+++ packages/root-config/webpack.config.js
@@ -1,6 +1,11 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const host = 'localhost';
+const portHere = 9000;
+const portThere = 9001;
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "ontotext";
@@ -24,5 +29,35 @@ module.exports = (webpackConfigEnv, argv) => {
         },
       }),
     ],
+      devServer: {
+          // disableHostCheck: true,
+          allowedHosts: 'all',
+          // contentBase: path.join(__dirname, 'dist/'),
+          static: {
+              directory: path.join(__dirname, 'dist/')
+          },
+          compress: true,
+          port: portHere,
+          host: host,
+          headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+              "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization, x-graphdb-repository",
+          },
+          // needed to handle urls sent by open id providers that contain dots
+          historyApiFallback: {
+              disableDotRule: true
+          },
+          proxy: [{
+              context: ['/rest', '/repositories', '/protocol', '/rdf-bridge'],
+              target: 'http://' + host + ':' + portThere,
+              onProxyRes: (proxyRes) => {
+                  var key = 'www-authenticate';
+                  if (proxyRes.headers[key]) {
+                      proxyRes.headers[key] = proxyRes.headers[key].split(', ');
+                  }
+              }
+          }]
+      }
   });
 };
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,12 +1,42 @@
-# Cleans up the legacy-workbench project.
+echo ''
+echo '########################   Cleaning -- api --   ###########################'
+echo ''
+echo '- Delete "packages/api/dist"'
+rm -fr packages/api/dist
+echo '- Delete "packages/api/node_modules"'
+rm -fr packages/api/node_modules
+
+echo ''
+echo '########################   Cleaning -- Workbench --   ###########################'
+echo ''
+echo '- Delete "packages/workbench/dist"'
+rm -fr packages/workbench/dist
+echo '- Delete "packages/workbench/node_modules"'
+rm -fr packages/workbench/node_modules
+echo '- Delete "packages/workbench/.angular"'
+rm -fr packages/workbench/.angular
+
+echo ''
+echo '########################   Cleaning -- Legacy Workbench --   ###########################'
+echo ''
+echo '- Delete "packages/legacy-workbench/dist"'
 rm -fr packages/legacy-workbench/dist
+echo '- Delete "packages/legacy-workbench/node_modules"'
 rm -fr packages/legacy-workbench/node_modules
 
-# Cleans up the root-config project.
+
+echo ''
+echo '########################   Cleaning -- Root config --   ###########################'
+echo ''
+echo '- Delete "packages/root-config/dist"'
 rm -fr packages/root-config/dist
+echo '- Delete "packages/root-config/node_modules"'
 rm -fr packages/root-config/node_modules
 
-# Cleans up the workbench project.
-rm -fr packages/workbench/dist
-rm -fr packages/workbench/node_modules
-rm -fr packages/workbench/.angular
+echo ''
+echo '########################   Cleaning -- Root --   ###########################'
+echo ''
+echo '- Delete "node_modules"'
+rm -fr node_modules
+echo '- Delete "deploy"'
+rm -fr deploy
