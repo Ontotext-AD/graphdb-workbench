ce603de20b47ee1046685fc3fb990013c4e7432a
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2024-07-05 13:46:19 +0300

    Removes hardcoded host and port from the legacy workbench's app.js file. (#1457)
    (cherry picked from commit f008c52033dd9716f94923feeb6f825cdbafd439)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,21 @@ echo ''
 
 cd packages/api
 npm run build
+
+echo ''
+echo '########################   Building -- Legacy Workbench --  ###########################'
+echo ''
+cd ../legacy-workbench
+npm run build
+
+echo ''
+echo '########################   Building -- Workbench --  ###########################'
+echo ''
+cd ../workbench
+npm run build
+
+echo ''
+echo '########################   Building -- Root config --  ###########################'
+echo ''
+cd ../root-config
+npm run build
