0982dd034c183c2550a283dcb8b76d3a72a35ecd
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2024-07-05 15:24:27 +0300

    GDB-10463 workbench lyaout and navigation (#1458)
    * Add navbar MF
    
    * Register the navbar MF
    
    * Fix legacy-workbench app mounting in proper place in the DOM. Previously it was attached to the body, now it is inside the main where the single spa includes it by default.
    
    * Fix some style issues
    
    * navbar project is added in the build, clean and install scripts
    
    * Extract separate script for building the api module.
    Fixed the legacy-workbench module initialization to prevent cyclic reinitialization of the workbench.
    
    (cherry picked from commit bf4ff1f952af80dab80104aa07385cecf35056a3)

---
+++ packages/navbar/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+    const defaultConfig = {
+        mode: 'development',
+        entry: "./src/navbar.js",
+        output: {
+            path: path.resolve(process.cwd(), "dist"),
+            filename: "navbar.js",
+            libraryTarget: "system",
+            publicPath: "",
+        },
+        resolve: {
+            extensions: [".js"],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/navbar.html",
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "html-loader"
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                }
+            ],
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist/')
+            },
+            headers: { "Access-Control-Allow-Origin": "*" },
+            allowedHosts: "all",
+            historyApiFallback: true,
+            port: 9005
+        },
+        optimization: {
+            minimize: false,
+        }
+    };
+
+    return defaultConfig;
+};
