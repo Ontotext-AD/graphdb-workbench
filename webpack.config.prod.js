59a17b744e19afe31f221696a4c7a300ff2aab7a
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2024-07-18 18:20:03 +0300

    GDB-10512: run the host and legacy workbench on a single server (#1476)
    * GDB-10512: Investigate how to run the host and legacy workbench on one server
    
    What
    Updates projects to build/start the root-config and legacy-workbench projects on one destination/server.
    
    Why
    The root-config is the main project that orchestrates all other ones. The legacy-workbench uses relative paths for endpoints. If both projects run on one server, no changes will be needed for the legacy-workbench to work.
    
    How
    Added webpack to the root container of all projects. Added configurations for development and production builds.
    
    # Known issues that will be fixed in next requests
    - version replacements for js files
    - license report
    - when a resource is changed the rebuild is trigered but browser page not refreshed.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,9 @@
-const PACKAGE = require('./package.json');
 const path = require('path');
 const {merge} = require('webpack-merge');
-const commonConfig = require('./webpack.config.common');
+const commonConfig = require('./webpack.config.common')();
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-// const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(commonConfig, {
     mode: 'production',
@@ -18,9 +15,11 @@ module.exports = merge(commonConfig, {
         }
     },
     output: {
-        filename: '[name].[contenthash].bundle.js',
+        filename: '[name].[contenthash].js',
         chunkFilename: '[name].[contenthash].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: "system",
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -50,21 +49,7 @@ module.exports = merge(commonConfig, {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/template.html',
-            favicon: 'src/img/icon.png',
-            templateParameters: {
-                version: PACKAGE.version
-            }
-            // TODO: enable this once completed with the fixes
-            // minify: {
-            //     removeAttributeQuotes: true,
-            //     collapseWhitespace: true,
-            //     removeComments: true
-            // }
-        }),
         new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
-        // new OptimizeCssAssetsPlugin(),
         new CssMinimizerPlugin(),
         new CleanWebpackPlugin()
     ]
