bd99cbeed508548d2a872d76fabc297707d70c4e
Desi Borisova <158429017+DesiBorisova@users.noreply.github.com>
2024-07-30 11:56:17 +0300

    Increase yasgui version and add prefix test (related GDB-9968) (#1488) (#1490)
    * Increase yasgui version and add prefix test
    
    * Refactor test and command. Now using waitUntil
    
    * Refactor test for better readability
    
    (cherry picked from commit bbb27ebff27694a4dfb97d1fb27f476b827994a7)

--- test-cypress/integration/sparql-editor/sparql-editor.spec.js
+++ test-cypress/integration/sparql-editor/sparql-editor.spec.js
@@ -52,4 +52,30 @@ describe('Sparql editor', () => {
         // And yasr tabs should not be visible
         YasrSteps.getResultHeader().should('not.be.visible');
     });
+
+    it('Should paste and add prefixes', () => {
+        // Given I have opened the workbench
+        // When I visit the sparql editor page
+        SparqlEditorSteps.visitSparqlEditorPage();
+        YasqeSteps.clearEditor();
+        const prefix = 'PREFIX afn: <http://jena.apache.org/ARQ/function#>\n';
+        const query = 'select distinct ?x ?Person  where {\n' +
+            '?x a afn:Person .\n' +
+            '?x afn:preferredLabel ?Person .\n' +
+            '?doc afn:containsMention / pub-old:hasInstance ?x .\n' +
+            '}';
+        // I paste a query into the editor
+        cy.pasteIntoCodeMirror('.CodeMirror', query);
+        // Then I expect the prefixes to be added automatically
+        cy.get('.CodeMirror').then((codeMirrorElement) => {
+            const codeMirror = codeMirrorElement[0].CodeMirror;
+            // Wait, so that Yasqe can have time to replace the existing query with the new one
+            cy.waitUntil(() => {
+                return codeMirror.getValue() === prefix + query;
+            }).then(() => {
+                const value = codeMirror.getValue();
+                expect(value).to.equal(prefix + query);
+            });
+        });
+    });
 });
