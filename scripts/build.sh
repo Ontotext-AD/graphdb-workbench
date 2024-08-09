d50a11a8830c2f618e60013ae0ea27b2a902159b
Teodossi Dossev <teodossi.dossev@ontotext.com>
2024-08-09 13:07:50 +0300

    feat(GDB-10638) Add Jenkins pipeline
    ## WHAT:
    This commit introduces a Jenkins pipeline for the project, adding automation for build, test, lint, and validation processes across the projects. Along with the Jenkinsfile, several scripts have been added or modified to facilitate the build, testing, and cleaning operations. Additionally, TypeScript and ESLint configurations have been updated to improve code quality and consistency.
    
    ## WHY:
    To automate the CI/CD process, ensuring that the codebase is consistently built, tested, and validated before being merged.
    
    ## HOW:
    
    - Jenkins Pipeline: A new Jenkinsfile is added to define the stages for installing dependencies, linting, validating, building, testing, and running Cypress tests.
    - Build Scripts: New shell scripts (install.sh, build.sh, clean.sh, etc.) are introduced for handling installation, building, and cleaning tasks in a modular and reusable manner.
    - Linting and Testing: New ESLint configurations and Jest setup for different packages, along with scripts for running linting and tests.
    - Configuration Updates: TypeScript configurations are updated to enhance strictness and compatibility, and ESLint configurations are made more consistent across packages.
    - Cleanup: Removal of outdated configuration files and cleanup of unnecessary code.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,29 @@
-echo ''
-echo '########################   Building -- api --   ###########################'
-echo ''
+#!/bin/bash
 
-cd packages/api
-npm run build
+# This script should be run from the root directory of the project.
+
+# Source the error handling script
+. scripts/error-handling.sh
+
+build_package() {
+    local package=$1
+    local description
+    description=$(echo "$package" | sed 's/\// -- /g')
+
+    echo ''
+    echo "########################   Building -- ${description} --   ###########################"
+    echo ''
+
+    npm run build --prefix "packages/${package}"
+
+    handle_error "Building ${description}"
+}
+
+# Build packages
+build_package "api"
+build_package "shared-components"
+build_package "workbench"
 
 echo ''
-echo '########################   Building -- shared-components --   ###########################'
+echo '########################   All packages built successfully!   ###########################'
 echo ''
-
-cd packages/shared-components
-npm run build
-
-echo ''
-echo '########################   Building -- Workbench --  ###########################'
-echo ''
-cd ../workbench
-npm run build
