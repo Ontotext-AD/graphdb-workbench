5cf859b07bae68b8fb0962643137bc48079c8a1c
Teodossi Dossev <teodossi.dossev@ontotext.com>
2024-10-02 14:04:06 +0300

    feat(GDB-10848) Implement Cluster Management View #1531

---
+++ src/js/angular/clustermanagement/directives/cluster-list.directive.js
@@ -0,0 +1,199 @@
+import {Location} from "../../models/clustermanagement/cluster";
+
+const modules = [];
+angular.module('graphdb.framework.clustermanagement.directives.cluster-list', modules)
+    .directive('clusterList', ClusterListComponent);
+
+ClusterListComponent.$inject = ['$translate', '$timeout', '$repositories', 'productInfo', 'toastr', 'RemoteLocationsService', 'ClusterContextService'];
+
+function ClusterListComponent($translate, $timeout, $repositories, productInfo, toastr, RemoteLocationsService, ClusterContextService) {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/angular/clustermanagement/templates/cluster-list.html',
+        link: ($scope, element, attrs) => {
+            // =========================
+            // Private variables
+            // =========================
+            const subscriptions = [];
+
+            // =========================
+            // Public variables
+            // =========================
+            $scope.rowHeights = {};
+            $scope.editedNodeIndex = undefined;
+            $scope.allSuggestions = [];
+            $scope.loader = false;
+            $scope.cluster = undefined;
+            $scope.clusterView = undefined;
+            $scope.errors = [];
+            $scope.newLocation = '';
+
+            /**
+             * Adds a new (empty) node to the list of cluster nodes for editing.
+             */
+            $scope.addNode = () => {
+                $scope.clusterView.push({});
+                $scope.editedNodeIndex = $scope.clusterView.length - 1;
+            };
+
+            /**
+             * Filters suggestions based on user input for node's endpoint.
+             * @param {Object} node - The node object with an endpoint property.
+             */
+            $scope.filterSuggestions = (node) => {
+                const query = node.endpoint && node.endpoint.toLowerCase();
+                if (query) {
+                    $scope.suggestions = $scope.allSuggestions.filter((item) => item.toLowerCase().includes(query));
+                    $scope.showDropdown = true;
+                } else {
+                    $scope.suggestions = [];
+                    $scope.showDropdown = false;
+                }
+            };
+
+            /**
+             * Selects a suggestion from the dropdown and updates the node's endpoint.
+             * @param {Object} node - The node object to update.
+             * @param {string} suggestion - The selected suggestion for the node's endpoint.
+             */
+            $scope.selectSuggestion = (node, suggestion) => {
+                node.endpoint = suggestion;
+                $scope.suggestions = [];
+                $scope.showDropdown = false;
+            };
+
+            /**
+             * Hides the suggestions dropdown after a timeout, used after user selection or clicking away.
+             */
+            $scope.hideSuggestions = () => {
+                $timeout(() => {
+                    $scope.showDropdown = false;
+                }, 200);
+            };
+
+            /**
+             * Saves a node to the cluster based on the provided endpoint.
+             * If the node is available in the cluster, it is added, otherwise, a new location is created and added.
+             * @param {string} endpoint - The endpoint of the node to save.
+             */
+            $scope.saveNode = (endpoint) => {
+                const node = $scope.cluster.getAvailable().filter((available) => available.endpoint === endpoint)[0];
+                if (node) {
+                    ClusterContextService.addLocation($scope.cluster, node);
+                    $scope.editedNodeIndex = undefined;
+                    return;
+                }
+                const newLocation = RemoteLocationsService.createNewLocation(endpoint);
+                $scope.setLoader(true, $translate.instant('cluster_management.update_cluster_group_dialog.messages.connecting_node'));
+
+                RemoteLocationsService.addLocationHttp(newLocation)
+                    .then((location) => {
+                        if (location.error) {
+                            handleErrors(location.error);
+                            ClusterContextService.updateClusterView();
+                            return;
+                        }
+                        if (location) {
+                            ClusterContextService.addLocation($scope.cluster, new Location(location));
+                        }
+                    })
+                    .catch(function (error) {
+                        handleErrors(error.data, error.status);
+                        ClusterContextService.updateClusterView();
+                    })
+                    .finally(() => {
+                        $scope.editedNodeIndex = undefined;
+                        $scope.setLoader(false);
+                    });
+            };
+
+            /**
+             * Deletes a node from the cluster.
+             * @param {number} index - The index of the node to delete.
+             */
+            $scope.deleteNode = (index) => {
+                // Not implemented
+            };
+
+            /**
+             * Checks if the current cluster configuration is valid.
+             * The configuration is considered valid if there are at least two nodes and no nodes are being edited.
+             * @return {boolean} True if the configuration is valid, false otherwise.
+             */
+            $scope.isClusterConfigurationValid = () => {
+                const isNotInEditMode = $scope.editedNodeIndex === undefined;
+                const validNodes = $scope.cluster.getViewModel().filter((node) => node.endpoint && node.endpoint.length > 0);
+                const hasValidNodes = validNodes.length >= 2;
+                return isNotInEditMode && hasValidNodes;
+            };
+
+            /**
+             * Sets the loader state with a message.
+             * The loader state is shown with a delay to avoid flickering for short operations.
+             * @param {boolean} loader - Whether to show the loader.
+             * @param {string} [message] - The message to show with the loader.
+             */
+            $scope.setLoader = (loader, message) => {
+                $timeout.cancel($scope.loaderTimeout);
+                if (loader) {
+                    $scope.loaderTimeout = $timeout(() => {
+                        $scope.loader = loader;
+                        $scope.loaderMessage = message;
+                    }, 300);
+                } else {
+                    $scope.loader = false;
+                }
+            };
+
+            // =========================
+            // Private functions
+            // =========================
+            const onClusterViewChanged = (cluster) => {
+                $scope.cluster = cluster;
+                $scope.clusterView = cluster.getAttached();
+                $scope.allSuggestions = cluster.getAvailableNodeEndpoints();
+
+                // If there are no cluster nodes, add the local node
+                if ($scope.clusterView.length === 0) {
+                    const localNode = $scope.locations.find((location) => location.isLocal);
+                    if (localNode) {
+                        $scope.saveNode(localNode.endpoint, 0);
+                    }
+                }
+            };
+
+            const handleErrors = (data, status) => {
+                let failMessage;
+                $scope.errors.splice(0);
+                if (status === 400) {
+                    $scope.errors.push(...data);
+                } else if (status === 409) {
+                    $scope.errors.push(data);
+                } else if (data.message || typeof data === 'string') {
+                    failMessage = data.message || data;
+                }
+                toastr.error(failMessage, $translate.instant('cluster_management.cluster_page.notifications.create_failed'));
+            };
+
+            // =========================
+            // Subscriptions
+            // =========================
+
+            /**
+             * Removes all listeners and unsubscribes from events when the directive is destroyed.
+             */
+            function removeAllListeners() {
+                subscriptions.forEach((subscription) => subscription());
+            }
+
+            const unwatch = $scope.$watchGroup(['clusterNodes', 'editedNodeIndex'], function (newValues, oldValues) {
+                const isValid = $scope.isClusterConfigurationValid();
+                ClusterContextService.updateClusterValidity(isValid);
+            });
+
+            subscriptions.push(unwatch);
+            subscriptions.push(ClusterContextService.onClusterViewChanged(onClusterViewChanged));
+            $scope.$on('$destroy', removeAllListeners);
+        }
+    };
+}
---
+++ src/js/angular/models/clustermanagement/cluster.js
@@ -0,0 +1,404 @@
+/**
+ * Represents a location with relevant metadata.
+ */
+export class Location {
+    /**
+     * @param {string} endpoint - The endpoint of the location.
+     * @param {string} rpcAddress - The RPC address of the location.
+     * @param {string} error - Any error associated with the location.
+     * @param {boolean} isAvailable - Whether the location is available.
+     * @param {boolean} isLocal - Whether the location is local.
+     */
+    constructor(endpoint, rpcAddress, error = null, isAvailable = false, isLocal = false) {
+        this._endpoint = endpoint;
+        this._rpcAddress = rpcAddress;
+        this._error = error;
+        this._isAvailable = isAvailable;
+        this._isLocal = isLocal;
+    }
+
+    /** @return {boolean} Whether the location is local. */
+    get isLocal() {
+        return this._isLocal;
+    }
+
+    /** @return {string} The endpoint of the location. */
+    get endpoint() {
+        return this._endpoint;
+    }
+
+    /** @return {string} The RPC address of the location. */
+    get rpcAddress() {
+        return this._rpcAddress;
+    }
+
+    /** @return {string} Any error related to the location. */
+    get error() {
+        return this._error;
+    }
+
+    /** @return {boolean} Whether the location is available. */
+    get isAvailable() {
+        return this._isAvailable;
+    }
+
+    /**
+     * Maps JSON data to a Location instance.
+     * @param {Object} json - The JSON data.
+     * @return {Location} The mapped Location instance.
+     */
+    static fromJSON({endpoint, rpcAddress, error = null, isAvailable = false, isLocal = false}) {
+        return new Location(endpoint, rpcAddress, error, isAvailable, isLocal);
+    }
+}
+
+/**
+ * Represents a node with its associated state and metadata.
+ */
+export class Node {
+    /**
+     * @param {string} address - The address of the node.
+     * @param {string} nodeState - The current state of the node.
+     * @param {number} term - The term of the node.
+     * @param {string} syncStatus - The synchronization status of the node.
+     * @param {number} lastLogTerm - The last log term of the node.
+     * @param {number} lastLogIndex - The last log index of the node.
+     * @param {string} endpoint - The endpoint associated with the node.
+     * @param {string} recoveryStatus - The recovery status of the node.
+     */
+    constructor(address, nodeState, term, syncStatus, lastLogTerm, lastLogIndex, endpoint, recoveryStatus) {
+        this._address = address;
+        this._nodeState = nodeState;
+        this._term = term;
+        this._syncStatus = syncStatus;
+        this._lastLogTerm = lastLogTerm;
+        this._lastLogIndex = lastLogIndex;
+        this._endpoint = endpoint;
+        this._recoveryStatus = recoveryStatus;
+    }
+
+    /** @return {string} The address of the node. */
+    get address() {
+        return this._address;
+    }
+
+    /** @return {string} The current state of the node. */
+    get nodeState() {
+        return this._nodeState;
+    }
+
+    /** @return {number} The term of the node. */
+    get term() {
+        return this._term;
+    }
+
+    /** @return {string} The synchronization status of the node. */
+    get syncStatus() {
+        return this._syncStatus;
+    }
+
+    /** @return {number} The last log term of the node. */
+    get lastLogTerm() {
+        return this._lastLogTerm;
+    }
+
+    /** @return {number} The last log index of the node. */
+    get lastLogIndex() {
+        return this._lastLogIndex;
+    }
+
+    /** @return {string} The endpoint of the node. */
+    get endpoint() {
+        return this._endpoint;
+    }
+
+    /** @return {string} The recovery status of the node. */
+    get recoveryStatus() {
+        return this._recoveryStatus;
+    }
+
+    /**
+     * Maps JSON data to a Node instance.
+     * @param {Object} json - The JSON data.
+     * @return {Node} The mapped Node instance.
+     */
+    static fromJSON({address, nodeState, term, syncStatus, lastLogTerm, lastLogIndex, endpoint, recoveryStatus}) {
+        return new Node(address, nodeState, term, syncStatus, lastLogTerm, lastLogIndex, endpoint, recoveryStatus);
+    }
+}
+
+/**
+ * Represents a link between nodes.
+ */
+export class Link {
+    /**
+     * @param {string} id - The ID of the link.
+     * @param {string} source - The source node of the link.
+     * @param {string} target - The target node of the link.
+     * @param {string} status - The status of the link.
+     */
+    constructor(id, source, target, status) {
+        this._id = id;
+        this._source = source;
+        this._target = target;
+        this._status = status;
+    }
+
+    /**
+     * Maps JSON data to a Link instance.
+     * @param {Object} json - The JSON data.
+     * @return {Link} The mapped Link instance.
+     */
+    static fromJSON({id, source, target, status}) {
+        return new Link(id, source, target, status);
+    }
+}
+
+/**
+ * Represents the cluster model containing locations, nodes, and links.
+ */
+export class ClusterModel {
+    /**
+     * @param {Location[]} locations - The list of locations.
+     * @param {boolean} hasCluster - Whether the cluster exists.
+     * @param {Node[]} nodes - The list of nodes.
+     * @param {Link[]} links - The list of links.
+     */
+    constructor(locations, hasCluster, nodes, links) {
+        this._locations = locations;
+        this._hasCluster = hasCluster;
+        this._nodes = nodes;
+        this._links = links;
+    }
+
+    /** @return {Location[]} The list of locations. */
+    get locations() {
+        return this._locations;
+    }
+
+    /** @param {Location[]} value The new list of locations. */
+    set locations(value) {
+        this._locations = value;
+    }
+
+    /** @return {boolean} Whether the cluster exists. */
+    get hasCluster() {
+        return this._hasCluster;
+    }
+
+    /** @param {boolean} value Whether the cluster exists. */
+    set hasCluster(value) {
+        this._hasCluster = value;
+    }
+
+    /** @return {Node[]} The list of nodes. */
+    get nodes() {
+        return this._nodes;
+    }
+
+    /** @param {Node[]} value The new list of nodes. */
+    set nodes(value) {
+        this._nodes = value;
+    }
+
+    /** @return {Link[]} The list of links. */
+    get links() {
+        return this._links;
+    }
+
+    /** @param {Link[]} value The new list of links. */
+    set links(value) {
+        this._links = value;
+    }
+
+    /**
+     * Maps JSON data to a ClusterModel instance.
+     * @param {Object} json - The JSON data.
+     * @return {ClusterModel} The mapped ClusterModel instance.
+     */
+    static fromJSON(json) {
+        const locations = json.locations.map(Location.fromJSON);
+        const nodes = json.nodes.map(Node.fromJSON);
+        const links = json.links.map(Link.fromJSON);
+
+        return new ClusterModel(locations, json.hasCluster, nodes, links);
+    }
+}
+
+/**
+ * ViewModel to manage and present the cluster model data.
+ */
+export class ClusterViewModel {
+    /**
+     * @param {Object} clusterModel - The raw cluster model JSON.
+     */
+    constructor(clusterModel) {
+        this._clusterModel = ClusterModel.fromJSON(clusterModel);
+        this._addToCluster = [];
+        this._deleteFromCluster = [];
+    }
+
+    /**
+     * Gets nodes attached to the cluster.
+     * @return {ClusterNodeViewModel[]} The list of attached nodes in view model format.
+     */
+    getAttached() {
+        const nodes = this._clusterModel.nodes;
+        const locations = this._clusterModel.locations;
+
+        const locationMap = new Map();
+        locations.forEach((location) => {
+            locationMap.set(location.endpoint, location);
+        });
+
+        const filteredNodes = nodes.filter(
+            (node) => !this._deleteFromCluster.some((delNode) => delNode.endpoint === node.endpoint)
+        );
+
+        return filteredNodes.map((node) => {
+            const location = locationMap.get(node.endpoint);
+            return ClusterUtil.toNodeLocationViewModel(location, node);
+        }).concat(this._addToCluster);
+    }
+
+    /**
+     * Gets available locations that can be added to the cluster.
+     * @return {ClusterNodeViewModel[]} The list of available locations in view model format.
+     */
+    getAvailable() {
+        const nodes = this._clusterModel.nodes;
+        const locations = this._clusterModel.locations;
+        return locations.filter(
+            (location) =>
+                !nodes.some((node) => node.endpoint === location.endpoint) &&
+                location.isAvailable &&
+                !this._deleteFromCluster.some((delNode) => delNode.endpoint === location.endpoint)
+        ).map((location) => ClusterUtil.toNodeLocationViewModel(location));
+    }
+
+    /**
+     * Gets the endpoints of the available nodes that are not yet part of the cluster.
+     *
+     * @return {string[]} The list of available node endpoints.
+     */
+    getAvailableNodeEndpoints() {
+        return this.getAvailable().map((node) => node.endpoint);
+    }
+
+    /**
+     * Adds a location to the cluster.
+     * @param {Location} location - The location to add.
+     */
+    addToCluster(location) {
+        this._addToCluster.push(
+            new Location(
+                location.endpoint,
+                location.rpcAddress,
+                location.error,
+                location.isAvailable,
+                location.isLocal
+            )
+        );
+    }
+
+    /**
+     * Marks a node for deletion from the cluster.
+     * @param {Node} node - The node to delete.
+     */
+    deleteFromCluster(node) {
+        this._deleteFromCluster.push(node);
+    }
+
+    /**
+     * Gets the view model of the current cluster.
+     * @return {Node[]} The list of nodes in the current view model.
+     */
+    getViewModel() {
+        const nodes = [...this._clusterModel.nodes, ...this._addToCluster];
+        return nodes.filter((node) => !this._deleteFromCluster.some((delNode) => delNode.endpoint === node.endpoint));
+    }
+
+    /**
+     * Gets nodes added to the cluster.
+     * @return {Location[]} The list of nodes added to the cluster.
+     */
+    getAddToCluster() {
+        return this._addToCluster;
+    }
+}
+
+/**
+ * Represents the view model for a cluster node.
+ */
+export class ClusterNodeViewModel {
+    /**
+     * @param {string} address - The node's address.
+     * @param {string} nodeState - The state of the node.
+     * @param {number} term - The term of the node.
+     * @param {string} syncStatus - The synchronization status of the node.
+     * @param {number} lastLogTerm - The last log term.
+     * @param {number} lastLogIndex - The last log index.
+     * @param {string} recoveryStatus - The recovery status.
+     * @param {string} endpoint - The endpoint for the node or location.
+     * @param {string} rpcAddress - The RPC address for the node or location.
+     * @param {boolean} isLocal - Whether the node or location is local.
+     * @param {string} error - Error state, if any.
+     * @param {boolean} isAvailable - Whether the node or location is available.
+     */
+    constructor({
+                    address = null,
+                    nodeState = null,
+                    term = null,
+                    syncStatus = null,
+                    lastLogTerm = null,
+                    lastLogIndex = null,
+                    recoveryStatus = null,
+                    endpoint = null,
+                    rpcAddress = null,
+                    isLocal = null,
+                    error = null,
+                    isAvailable = null
+                } = {}) {
+        this.address = address;
+        this.nodeState = nodeState;
+        this.term = term;
+        this.syncStatus = syncStatus;
+        this.lastLogTerm = lastLogTerm;
+        this.lastLogIndex = lastLogIndex;
+        this.recoveryStatus = recoveryStatus;
+        this.endpoint = endpoint;
+        this.rpcAddress = rpcAddress;
+        this.isLocal = isLocal;
+        this.error = error;
+        this.isAvailable = isAvailable;
+    }
+}
+
+
+/**
+ * Utility class for transforming cluster data to a view model format.
+ */
+class ClusterUtil {
+    /**
+     * Converts location and node data into a ClusterNodeViewModel instance.
+     * @param {Location} location - The location data.
+     * @param {Node} [node] - The node data (optional).
+     * @return {ClusterNodeViewModel} The view model instance.
+     */
+    static toNodeLocationViewModel(location, node) {
+        return new ClusterNodeViewModel({
+            address: node ? node.address : null,
+            nodeState: node ? node.nodeState : null,
+            term: node ? node.term : null,
+            syncStatus: node ? node.syncStatus : null,
+            lastLogTerm: node ? node.lastLogTerm : null,
+            lastLogIndex: node ? node.lastLogIndex : null,
+            recoveryStatus: node ? node.recoveryStatus : null,
+            endpoint: node ? node.endpoint : location ? location.endpoint : null,
+            rpcAddress: node ? node.endpoint : location ? location.rpcAddress : null,
+            isLocal: location ? location.isLocal : null,
+            error: location ? location.error : null,
+            isAvailable: location ? location.isAvailable : null
+        });
+    }
+}
