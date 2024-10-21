dc65ff1ce3588cbde4469d7721a32d72da68ce67
boyantonchev <boyan.tonchev@ontotext.com>
2024-10-21 11:16:50 +0300

    ## What  Reorganizing the rdfResourceSearch component code without making any functional changes.
    ## Why
     To improve readability and facilitate future refactoring.
    
    ##How
     Functions and variables have been moved into separate code blocks based on whether they are private or public.

--- src/js/angular/core/directives/rdfresourcesearch/rdf-resource-search.directive.js
+++ src/js/angular/core/directives/rdfresourcesearch/rdf-resource-search.directive.js
@@ -16,45 +16,29 @@ function rdfResourceSearchDirective($rootScope, $timeout,
         restrict: 'AE',
         link: function ($scope, element) {
 
+
+            // =========================
+            // Private variables
+            // =========================
+
             const hiddenXSUpClass = 'hidden-xs-up';
 
-            function refreshRepositoryInfo() {
-                if ($scope.getActiveRepository()) {
-                    $scope.getNamespacesPromise = RDF4JRepositoriesRestService.getNamespaces(
-                        $scope.getActiveRepository())
-                        .success(function () {
-                            checkAutocompleteStatus();
-                        });
-                }
-            }
+            // =========================
+            // Public functions
+            // =========================
 
-            function checkAutocompleteStatus() {
-                if ($licenseService.isLicenseValid()) {
-                    $scope.getAutocompletePromise = AutocompleteRestService.checkAutocompleteStatus();
-                }
-            }
-
-            $scope.$on('autocompleteStatus', function () {
-                checkAutocompleteStatus();
-            });
-
-            // Rather then rely on securityInit we monitory repositoryIsSet which is guaranteed to be called
-            // after security was initialized. This way we avoid a race condition when the newly logged in
-            // user doesn't have read access to the active repository.
-            $scope.$on('repositoryIsSet', refreshRepositoryInfo);
-
-            $scope.getActiveRepository = function () {
-                return $repositories.getActiveRepository();
-            };
-
-            $scope.showInput = function () {
+            $scope.showInput = () => {
                 createSearchInput()
-                    .then(function () {
-                        $rootScope.$broadcast('rdfResourceSearchExpanded');
-                    })
+                    .then(() => $rootScope.$broadcast('rdfResourceSearchExpanded'));
             };
 
-            $scope.hideInput = function () {
+            $scope.onKeyDown = function (event) {
+                if (event.key === 'Escape') {
+                    $scope.hideInput();
+                }
+            };
+
+            $scope.hideInput = () => {
                 element.find('.search-rdf-input').removeClass('show-rdf-search-box');
                 element.find('.close-rdf-search-btn').addClass(hiddenXSUpClass);
                 $timeout(function () {
@@ -64,28 +48,49 @@ function rdfResourceSearchDirective($rootScope, $timeout,
                 }, 200);
             };
 
-            function onWindowClick(event) {
-                if (!(event.target.id === "search-box" || $(event.target).closest("#search-box").length)) {
-                    $scope.hideInput();
-                }
-            }
-
-            $scope.onKeyDown = function (event) {
-                if (event.keyCode === 27) {
-                    $scope.hideInput();
-                }
+            // TODO: remove this from here, it controls the visibility of component and have to be in main template
+            $scope.isNotLoading = function () {
+                return !$scope.isLoadingLocation() || $scope.isLoadingLocation() && $location.url() === '/repository';
             };
 
+            // TODO: remove this from here, it controls the visibility of component and have to be in main template
+            $scope.getActiveRepository = () => {
+                return $repositories.getActiveRepository();
+            };
+
+            // TODO: remove this from here, When tha page is home page and button is clicked then it is hided
             $scope.isHomePage = function () {
                 return $location.url() === '/';
             };
 
-            $scope.isNotLoading = function () {
-                return !$scope.isLoadingLocation() || $scope.isLoadingLocation() && $location.url() === '/repository';
-            }
+            // =========================
+            // Private functions
+            // =========================
 
-            function createSearchInput() {
-                return new Promise(resolve => {
+            const refreshRepositoryInfo = () => {
+                if ($scope.getActiveRepository()) {
+                    $scope.getNamespacesPromise = RDF4JRepositoriesRestService.getNamespaces(
+                        $scope.getActiveRepository())
+                        .success(function () {
+                            checkAutocompleteStatus();
+                        });
+                }
+            };
+
+            const checkAutocompleteStatus = () => {
+                if ($licenseService.isLicenseValid()) {
+                    $scope.getAutocompletePromise = AutocompleteRestService.checkAutocompleteStatus();
+                }
+            };
+
+            const onWindowClick = (event) => {
+                if (!(event.target.id === "search-box" || $(event.target).closest("#search-box").length)) {
+                    $scope.hideInput();
+                }
+            };
+
+            const createSearchInput = () => {
+                return new Promise((resolve) => {
                     element.find('.search-rdf-btn')
                         .addClass(hiddenXSUpClass);
                     element.find('.search-rdf-input').addClass('show-rdf-search-box');
@@ -111,9 +116,28 @@ function rdfResourceSearchDirective($rootScope, $timeout,
                     }
                     setTimeout(() => {
                         resolve(true);
-                    })
+                    });
                 });
-            }
+            };
+
+            // =========================
+            // Subscriptions
+            // =========================
+            const subscriptions = [];
+
+            const removeAllSubscribers = () => {
+                subscriptions.forEach((subscription) => subscription());
+            };
+
+            subscriptions.push($scope.$on('autocompleteStatus', checkAutocompleteStatus));
+
+            // Rather then rely on securityInit we monitory repositoryIsSet which is guaranteed to be called
+            // after security was initialized. This way we avoid a race condition when the newly logged in
+            // user doesn't have read access to the active repository.
+            subscriptions.push($scope.$on('repositoryIsSet', refreshRepositoryInfo));
+
+            // Deregister the watcher when the scope/directive is destroyed
+            $scope.$on('$destroy', removeAllSubscribers);
         }
     };
 }
