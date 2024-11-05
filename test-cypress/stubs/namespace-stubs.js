15bc755564ea651e5036c515134f17e5a8322b0b
Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>
2024-11-05 11:11:39 +0200

    GDB-11134 fix error handling in namespace view (#1661)
    ## What
    Fixing the error handling in the namespaces view.
    
    ## Why
    Error handling in the namespace view is practically missing which makes the UI to behave in unexpected ways. For example, when user tries to edit a namespace, but the server returns some error, the UI updated, although it mustn't.
    
    ## How
    * Implemented error handling for create and edit namespace operation.
    * Did some minor refactoring and removed some duplications.
    * Implemented tests.
    
    Updated jsdoc

--- test-cypress/stubs/namespace-stubs.js
+++ test-cypress/stubs/namespace-stubs.js
@@ -7,4 +7,9 @@ export class NamespaceStubs {
     static stubNameSpaceResponse(repositoryId, fixture, withDelay = 0) {
         cy.intercept(`/repositories/${repositoryId}/namespaces`, {fixture, delay: withDelay}).as(`${repositoryId}-stub-namespaces`);
     }
+
+    static stubErrorOnNamespaceUpdate(repositoryId) {
+        cy.intercept('PUT', `/repositories/${repositoryId}/namespaces/*`,
+            {statusCode: 500, body: 'Internal Server Error'}).as(`stub-namespace-update-error`);
+    }
 }
