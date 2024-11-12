fef357357d907930217b8f8d182369bf52b0cb3e
boyan-tonchev <108174872+boyan-tonchev@users.noreply.github.com>
2024-11-12 10:56:10 +0200

    GDB-11134 fix error handling in JDBC view (#1678) (#1695)
    * GDB-11134 fix error handling in JDBC view
    
    ## What
    Fix error handling in JDBC view.
    
    ## Why
    The promise from the update request for the JDBC config was not returned by the method used in the controller which made the handler to misbehave.
    
    ## How
    Changed the update method to return the promise so that the handler to be able to react on the success and errors.
    
    * Moved specs for the JDBC view in separate folder
    
    * Installed cypress plugin for ESLint to help with the analysis and get rid of the warnings in the test specs for undefined cypress stuff.
    
    * Refactored and fixed jdbc tests and added tests to cover the scenarios with server errors during save and update.
    
    (cherry picked from commit cd4bfbf576c49a7ad80dcae0e27588ca18328309)
    
    Co-authored-by: Svilen Velikov <51084653+svilenvelikov@users.noreply.github.com>

--- test-cypress/integration/setup/jdbc/jdbc.spec.js
+++ test-cypress/integration/setup/jdbc/jdbc.spec.js
@@ -1,22 +1,19 @@
-import {JdbcSteps} from "../../steps/setup/jdbc-steps";
-import {JdbcCreateSteps} from "../../steps/setup/jdbc-create-steps";
-import {ModalDialogSteps} from "../../steps/modal-dialog-steps";
-import {YasqeSteps} from "../../steps/yasgui/yasqe-steps";
+import {JdbcSteps} from "../../../steps/setup/jdbc-steps";
+import {JdbcCreateSteps} from "../../../steps/setup/jdbc-create-steps";
+import {ModalDialogSteps} from "../../../steps/modal-dialog-steps";
+import {YasqeSteps} from "../../../steps/yasgui/yasqe-steps";
+import {JdbcStubs} from "../../../stubs/jdbc/jdbc-stubs";
+import {ApplicationSteps} from "../../../steps/application-steps";
 
 const FILE_TO_IMPORT = '200-row-allianz.ttl';
-const EDIT_QUERY = "PREFIX ex:<http://example.com/#>\n" +
-    "PREFIX base:<http://example/base/>\n" +
-    "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>\n" +
-    "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-    "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
-    "\n" +
-    "select ?id ?fraud_score ?customer_loyalty where { \n" +
-    "\t?id rdf:type ex:Customer;\n" +
-    "      \tex:fraudScore ?fraud_score;\n" +
-    "       \tex:customerLoyalty ?customer_loyalty_id.\n" +
-    "    ?customer_loyalty_id rdfs:label ?customer_loyalty.\n" +
-    "    # !filter\n" +
-    "}  ";
+
+// The closing brace is intentionally omitted because when cypress types in the query, the editor will automatically add it.
+const EDIT_QUERY = `
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?id ?label {
+        ?id rdfs:label ?label
+        #!filter
+`;
 
 describe('JDBC configuration', () => {
 
@@ -34,7 +31,7 @@ describe('JDBC configuration', () => {
         cy.deleteRepository(repositoryId);
     });
 
-    it('Should not create a new JDBC configuration click on cancel button', () => {
+    it('Should be able to cancel JDBC configuration creation', () => {
         // When I am on JDBC configurations page and click on create a new table configuration button.
         JdbcSteps.clickOnCreateJdbcConfigurationButton();
 
@@ -82,7 +79,8 @@ describe('JDBC configuration', () => {
         // When I click on edit button,
         JdbcSteps.clickOnEditButton();
         // change the query,
-        YasqeSteps.pasteQuery(EDIT_QUERY);
+        YasqeSteps.clearEditor();
+        YasqeSteps.writeInEditor(EDIT_QUERY);
         // and click on save button.
         JdbcCreateSteps.clickOnSave();
 
@@ -113,13 +111,6 @@ describe('JDBC configuration', () => {
         JdbcSteps.clickOnDeleteButton();
 
         // Then I expect to be asked to confirm the deletion,
-        // and click on cancel dialog button.
-        ModalDialogSteps.clickOnCancelButton();
-
-        // When I click on delete button.
-        JdbcSteps.clickOnDeleteButton();
-
-        // Then I expect to be asked to confirm the deletion,
         // and when click on confirm dialog button.
         ModalDialogSteps.clickOnConfirmButton();
 
@@ -127,4 +118,43 @@ describe('JDBC configuration', () => {
         JdbcSteps.getJDBCConfigurations().should('contain', 'No tables are defined');
     });
 
+    it('Should show error notification on server error during jdbc configuration creation', () => {
+        // Given that the server will return an error on saving the JDBC configuration
+        JdbcStubs.stubJdbcCreateError();
+        // And I have configured a new JDBC configuration
+        JdbcSteps.clickOnCreateJdbcConfigurationButton();
+        JdbcCreateSteps.verifyUrl();
+        JdbcCreateSteps.typeTableName('JdbcTest');
+        JdbcCreateSteps.openColumnTypesTab();
+        JdbcCreateSteps.getColumnSuggestionRows().should('have.length', 2);
+        // When I click on save button.
+        JdbcCreateSteps.clickOnSave();
+        // Then I expect to see a notification with the error message.
+        ApplicationSteps.getErrorNotifications().should('contain', 'Could not save SQL table configuration');
+        // And the configuration to not be created.
+        cy.url().should('include', '/jdbc/configuration/create');
+        JdbcCreateSteps.getSaveButton().should('be.visible').and('be.enabled');
+    });
+
+    it('Should show error notification on server error during jdbc configuration creation', () => {
+        // Given that the server will return an error on saving the JDBC configuration
+        JdbcStubs.stubJdbcUpdateError();
+        // And I have configured and saved a new JDBC configuration
+        JdbcSteps.clickOnCreateJdbcConfigurationButton();
+        JdbcCreateSteps.verifyUrl();
+        JdbcCreateSteps.typeTableName('JdbcTest2');
+        JdbcCreateSteps.openColumnTypesTab();
+        JdbcCreateSteps.getColumnSuggestionRows().should('have.length', 2);
+        JdbcCreateSteps.clickOnSave();
+        JdbcSteps.verifyUrl();
+        JdbcSteps.getJDBCConfigurationResults().should('have.length', 1);
+        // When I edit the configuration
+        JdbcSteps.clickOnEditButton();
+        YasqeSteps.clearEditor();
+        YasqeSteps.writeInEditor(EDIT_QUERY);
+        // And click on save button.
+        JdbcCreateSteps.clickOnSave();
+        // Then I expect to see a notification with the error message.
+        ApplicationSteps.getErrorNotifications().should('contain', 'Could not save SQL table configuration');
+    });
 });
