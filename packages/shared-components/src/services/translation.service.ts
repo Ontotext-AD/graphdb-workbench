4a4107c8cb1b096b4840b412b238282385204431
boyantonchev <boyan.tonchev@ontotext.com>
2024-11-18 12:43:35 +0200

    Enrich the Translation Service
    ## What
    Enhance the Translation service to support language bundles in hierarchical structure, allowing for hierarchical keys and nested objects within the translation files.
    
    ## Why
    This change simplifies the management and maintenance of labels, especially for complex UIs with multiple nested elements. With this approach, translations can be organized into logical groups and sub-groups, making it easier to locate, update, and expand the labels as needed.
    
    A sample translation file might look like this:
    
    ```
    {
    "visual.graph.label": "Visual graph",
    "view.rdf.rank.title": "RDF Rank",
    "language_selector": {
       "toggle_menu": {
          "label": "Select language",
          "tooltip": ""
       }
     }
    }
    ```
    
    ## How
    Enhance the translation service to attempt extracting a translation from a hierarchical object if a simple key lookup does not return a value.

--- packages/shared-components/src/services/translation.service.ts
+++ packages/shared-components/src/services/translation.service.ts
@@ -4,13 +4,18 @@ import {ServiceProvider, LanguageService} from "@ontotext/workbench-api";
 import {Subscription} from '@reactivex/rxjs/dist/package';
 import {TranslationParameter} from '../models/translation/translation-parameter';
 import {TranslationCallback, TranslationObserver} from '../models/translation/translation-observer';
+import {sanitizeHTML} from '../utils/html-utils';
+
+type TranslationBundle = {
+  [key: string]: string | TranslationBundle;
+};
 
 /**
  * Service responsible for translation operations in the component.
  */
 class TranslationServiceClassDefinition {
 
-  private bundle = {en, fr}
+  private bundle: {[key: string]: TranslationBundle} = {en, fr}
   private readonly languageChangeSubscription: Subscription;
   private currentLanguage = LanguageService.DEFAULT_LANGUAGE;
 
@@ -58,20 +63,100 @@ class TranslationServiceClassDefinition {
     return this.translateInLocale(key, this.currentLanguage, parameters);
   }
 
+  /**
+   * Translates a given key into the specified locale. If the translation is not available
+   * in the specified locale, it falls back to the default language. If parameters are provided,
+   * they will be applied to the translation.
+   *
+   * @param key - The translation key, which can be dot-separated for nested translations.
+   * @param locale - The target locale for translation (e.g., 'en', 'fr').
+   * @param parameters - Optional parameters to be substituted into the translation string.
+   * @returns The translated string with parameters applied if available, or the key itself if not found.
+   */
   public translateInLocale(key: string, locale: string, parameters?: TranslationParameter[]): string {
-    let translation = this.bundle[locale][key];
+    // Attempt to retrieve the translation from the specified locale's bundle.
+    let translation = this.translateFromBundle(this.bundle[locale], key);
+
+    // If not found, fall back to the default language bundle.
     if (!translation) {
-      // Fallback to the default language
-      translation = this.bundle[LanguageService.DEFAULT_LANGUAGE][key];
+      translation = this.translateFromBundle(this.bundle[LanguageService.DEFAULT_LANGUAGE], key);
     }
 
+    // If translation was found, apply any parameters provided and return the result.
     if (translation) {
       translation = this.applyParameters(translation, parameters);
-      return translation;
+      return sanitizeHTML(translation);
     }
 
     console.warn(`Missing translation for [${key}] key in [${this.currentLanguage}] locale`);
     return key;
+  }
+
+  /**
+   * Retrieves a translation from a specified translation bundle using the given key.
+   * If the key represents a nested JSON path, it attempts to resolve the key as such.
+   *
+   * @param bundle - The translation bundle for the specified locale.
+   * @param key - The key to translate, which may be a simple key or dot-separated for nested keys.
+   * @returns The translation string if found, or `undefined` if not available.
+   */
+  private translateFromBundle(bundle: TranslationBundle, key: string): string | undefined {
+    if (!bundle) {
+      return undefined;
+    }
+    // Try to retrieve the translation directly if it is defined as a whole string,
+    // for instance as "some_function.btn.label".
+    let translation = bundle[key];
+
+    // If not found, attempt to retrieve the translation if it is defined as a JSON object.
+    if (typeof translation !== 'string') {
+      translation = this.translateAsJsonObject(bundle, key);
+    }
+    return translation;
+  }
+
+  /**
+   * Resolves a nested translation key by treating it as a path in a JSON object.
+   * For example, if the key is "some.key.path", this method will navigate through
+   * the JSON object structure to retrieve the value at that path.
+   *
+   * @param bundle - The translation bundle containing nested keys.
+   * @param key - The dot-separated key representing the path to the translation.
+   * @returns The translation string if found, or `undefined` if the path does not exist.
+   */
+  private translateAsJsonObject(bundle: TranslationBundle, key: string): string {
+    let path = key.split('.');
+    return this.getTranslation(bundle, path);
+  }
+
+  /**
+   * Recursively navigates through a JSON object based on a path array to retrieve
+   * a translation string. Each element in the path array represents a nested key.
+   *
+   * @param jsonObject - The current JSON object or nested object being searched.
+   * @param path - An array representing the path to the translation, where each element is a key.
+   * @returns The translation string if found, or `undefined` if the path does not exist.
+   */
+  private getTranslation(jsonObject: TranslationBundle, path: string[]) {
+    // Stop the recursion if path or reach the inner element
+    if (!path || !jsonObject) {
+      return undefined;
+    }
+
+    const jsonOrTranslation = jsonObject[path.splice(0, 1)[0]];
+
+    if (path.length === 0) {
+      return jsonOrTranslation;
+    }
+
+    if (typeof jsonOrTranslation !== 'string') {
+      // continue if the inner element is not reached.
+      if (path.length > 0) {
+        return this.getTranslation(jsonOrTranslation, path);
+      }
+      return jsonOrTranslation;
+    }
+    return undefined;
   }
 
   private applyParameters(translation: string, parameters: TranslationParameter[]): string {
