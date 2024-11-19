b938293c09ddd6256ab815c50689c298d2f7d934
boyantonchev <boyan.tonchev@ontotext.com>
2024-11-19 16:26:43 +0200

    GDB-11227: Missing Icons for Cluster Legend
    ## What
    Icons are missing from the cluster legend in the "Cluster Management" view.
    
    ## Why
    We recently started using a new pro version of Font Awesome, which introduces a new font-family name: "Font Awesome 6 Pro". Initially, both the new and old versions of Font Awesome were included, so the icons were displayed correctly. However, a recent commit removed the old Font Awesome version, breaking the icon visualization. The difference between the two versions lies in the font-family name, likely for backward compatibility. The old font-family name was "FontAwesome", while the new one is "Font Awesome 6 Pro".
    
    ## How
    - Updated the font-family name to the new version.
    - Fixed solid icons by including the solid CSS file in the vendor bundle.

--- src/vendor.js
+++ src/vendor.js
@@ -4,6 +4,7 @@ import 'lib/awesome_me/css/fontawesome.min.css';
 import 'lib/awesome_me/css/regular.min.css';
 import 'lib/awesome_me/css/sharp-regular.min.css';
 import 'lib/awesome_me/css/custom-icons.min.css';
+import 'lib/awesome_me/css/solid.min.css';
 import './css/lib/animate/animate.css';
 import 'shepherd.js/dist/css/shepherd.css';
 import './css/shepherd-custom.css';
