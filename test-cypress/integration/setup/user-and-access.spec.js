55a969f4131c86c283973844076776299edc5817
desislava.borisova <desislava.borisova@ontotext.com>
2024-11-22 17:15:15 +0200

    GDB-11243 - fix context when using this keyword
    fix jenkins
    
    revert jenkinsfile
    
    GDB-11243 - update GDB version in env
    
    GDB-11243 - add afterEach hook to switch security off

--- test-cypress/integration/setup/user-and-access.spec.js
+++ test-cypress/integration/setup/user-and-access.spec.js
@@ -1,4 +1,7 @@
 import {UserAndAccessSteps} from "../../steps/setup/user-and-access-steps";
+import {ModalDialogSteps} from "../../steps/modal-dialog-steps";
+import {ToasterSteps} from "../../steps/toaster-steps";
+import {RepositoriesStubs} from "../../stubs/repositories/repositories-stubs";
 
 describe('User and Access', () => {
 
@@ -26,6 +29,17 @@ describe('User and Access', () => {
                     UserAndAccessSteps.deleteUser(username);
                 }
             });
+        });
+    });
+
+    afterEach(() => {
+        UserAndAccessSteps.visit();
+        UserAndAccessSteps.getToggleSecurityCheckbox()
+            .then(($toggle) => {
+            if ($toggle.prop('checked')) {
+                // Uncheck the security toggle button at the end of each test, if it is checked
+                UserAndAccessSteps.toggleSecurity();
+            }
         });
     });
 
@@ -120,6 +134,33 @@ describe('User and Access', () => {
         UserAndAccessSteps.getUsersTable().should('be.visible');
         UserAndAccessSteps.getSplashLoader().should('not.be.visible');
         UserAndAccessSteps.deleteUser("adminWithNoPassword");
+    });
+
+    it('should toggle free access after Admin has logged in', () => {
+        // Given I have available repositories to allow Free Access for
+        RepositoriesStubs.stubRepositories();
+        // When I enable security
+        UserAndAccessSteps.toggleSecurity();
+        // When I log in as an Admin
+        UserAndAccessSteps.loginWithUser("admin", DEFAULT_ADMIN_PASSWORD);
+        // Then the page should load
+        UserAndAccessSteps.getSplashLoader().should('not.be.visible');
+        UserAndAccessSteps.getUsersTable().should('be.visible');
+        // The Free Access toggle should be OFF
+        UserAndAccessSteps.getFreeAccessSwitchInput().should('not.be.checked');
+        // When I toggle Free Access ON
+        UserAndAccessSteps.toggleFreeAccess();
+        // Then I click OK in the modal
+        ModalDialogSteps.clickOKButton();
+        // Then the toggle button should be ON
+        UserAndAccessSteps.getFreeAccessSwitchInput().should('be.checked');
+        // And I should see a success message
+        ToasterSteps.verifySuccess('Free access has been enabled.');
+        UserAndAccessSteps.getUsersTable().should('be.visible');
+        // When I toggle Free Access OFF
+        UserAndAccessSteps.toggleFreeAccess();
+        // Then I should see a success message
+        ToasterSteps.verifySuccess('Free access has been disabled.');
     });
 
     function createUser(username, password, role) {
