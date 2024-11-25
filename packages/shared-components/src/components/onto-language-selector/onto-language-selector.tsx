2934a6a4b4cf01158aca642fc155cf79c20a437a
boyantonchev <boyan.tonchev@ontotext.com>
2024-11-25 19:18:10 +0200

    GDB-10570: Introduce Language Selector
    What
    Added a language selector to the header.
    
    Why
    To provide the ability to change the language of the workbench.
    
    How
    Implemented it as a Stencil component.

---
+++ packages/shared-components/src/components/onto-language-selector/onto-language-selector.tsx
@@ -0,0 +1,71 @@
+import {Component, Host, h, State} from '@stencil/core';
+import {DropdownItem} from '../../models/dropdown/dropdown-item';
+import {ServiceProvider, LanguageService} from "@ontotext/workbench-api";
+import {DropdownItemAlignment} from '../../models/dropdown/dropdown-item-alignment';
+import {Subscription} from '@reactivex/rxjs/dist/package';
+
+@Component({
+  tag: 'onto-language-selector',
+  styleUrl: 'onto-language-selector.scss',
+  shadow: false,
+})
+export class OntoLanguageSelector {
+  private languageService: LanguageService;
+  private items: DropdownItem[] = [];
+  private readonly onLanguageChangeSubscription: Subscription;
+
+  /**
+   * Holds the currently selected language, such as 'en' or 'fr'.
+   */
+  @State() currentLanguage: string;
+
+  constructor() {
+    this.languageService = ServiceProvider.get(LanguageService);
+    this.onLanguageChangeSubscription = this.languageService.onLanguageChanged()
+      .subscribe((newLanguage) => this.changeLanguage(newLanguage));
+    this.items = this.getLanguageDropdownOptions();
+  }
+
+  disconnectedCallback(): void {
+    if (this.onLanguageChangeSubscription) {
+      this.onLanguageChangeSubscription.unsubscribe();
+    }
+  }
+
+  render() {
+    return (
+      <Host>
+        <onto-dropdown
+          class='onto-language-selector'
+          onValueChanged={(newLanguage: any) => this.onLanguageChanged(newLanguage)}
+          dropdownButtonNameLabelKey={!this.currentLanguage ? 'language_selector.toggle_menu.label' : undefined}
+          dropdownButtonName={this.currentLanguage ? this.currentLanguage : undefined}
+          dropdownButtonTooltipLabelKey={this.currentLanguage ? 'language_selector.toggle_menu.tooltip' : 'language_selector.toggle_menu.label'}
+          dropdownAlignment={DropdownItemAlignment.RIGHT}
+          iconClass='icon-translation'
+          items={this.items}>
+        </onto-dropdown>
+      </Host>
+    );
+  }
+
+  private changeLanguage(newLanguage: string): void {
+    this.currentLanguage = newLanguage;
+    this.items = this.getLanguageDropdownOptions();
+  }
+
+  private onLanguageChanged(newLanguageEvent: CustomEvent): void {
+    this.languageService.changeLanguage(newLanguageEvent.detail);
+  }
+
+  private getLanguageDropdownOptions(): DropdownItem[] {
+    return this.languageService.getSupportedLanguages().map((locale) => {
+      const iconClass = this.currentLanguage === locale ? 'icon-tick' : '';
+      return new DropdownItem()
+        .setNameLabelKey(`language_selector.language.${locale}.label`)
+        .setTooltipLabelKey(`language_selector.language.${locale}.tooltip.` + (this.currentLanguage === locale ? 'selected' : 'not_selected'))
+        .setIconClass(iconClass)
+        .setValue(locale);
+    });
+  }
+}
