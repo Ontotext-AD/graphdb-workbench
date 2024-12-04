0dea62675717df29840881b8a59777748945aa6c
boyantonchev <boyan.tonchev@ontotext.com>
2024-12-04 09:45:21 +0200

    GDB-10569: WB Migration: Implement the repository change selector in the header
    ## What
    Implemented a repository selector component that allows the selected repository to be changed within the workbench.
    
    ## Why
    This functionality exists in the legacy workbench and has been migrated to the new system.
    
    ## How
    The selector is implemented as a Stencil component that utilizes a dropdown component. Implemented Repository and Location services, along with the corresponding models. The dropdown component has been updated to support Stencil components as dropdown items.
    
    updated libraries
    
    migrated eslint config to the latest format
    
    updated webpack-config-single-spa library

---
+++ packages/api/src/models/repositories/index.ts
@@ -0,0 +1,4 @@
+export {Repository} from './repository';
+export {RepositoryList} from './repository-list';
+export {RepositoryState} from './repository-state';
+export {RepositoryType} from './repository-type';
---
+++ packages/shared-components/cypress/e2e/header/repository-selector.cy.js
@@ -0,0 +1,29 @@
+import {RepositorySelectorSteps} from "../../steps/header/repository-selector-steps";
+import {RepositoriesStubs} from "../../stubs/repositories-stubs";
+
+describe("Repository Selector", () => {
+
+  it('Should select repository', () => {
+    RepositoriesStubs.stubLoadAllRepositories();
+    // When I visit a page with repository selector in it.
+    RepositorySelectorSteps.visit();
+    // Then I expect to see only toggle selector button with default label, because the repository is not selected,
+    RepositorySelectorSteps.getRepositorySelectorToggleButton()
+      .should('be.visible')
+      .and('contain', 'Choose repository');
+    // and the menu with repository items be not visible.
+    RepositorySelectorSteps.getRepositorySelectorItemMenu().should('not.be.visible');
+
+    // When I click on repository button
+    RepositorySelectorSteps.toggleRepositorySelector();
+    // Ten I expect the menu with repository items be visible.
+    RepositorySelectorSteps.getRepositorySelectorItemMenu().should('be.visible');
+
+    // When I select a repository
+    RepositorySelectorSteps.selectRepositorySelectorItem(2);
+    // Then I expect the menu with repositories be not visible
+    RepositorySelectorSteps.getRepositorySelectorItemMenu().should('not.be.visible');
+    // and selector button to contains the id of selected repository
+    RepositorySelectorSteps.getRepositorySelectorToggleButton().contains('starwars4');
+  });
+});
