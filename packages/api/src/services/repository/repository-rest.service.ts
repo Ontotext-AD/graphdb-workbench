f7123c25f92388c4c66814f23896aeac2ba09347
boyantonchev <boyan.tonchev@ontotext.com>
2024-12-06 15:57:39 +0200

    GDB-10569: WB Migration: implement better deep copy functionality
    ## What
    Implement deep copy functionality.
    
    ## Why
    We use subscriptions to handle changes to certain values. When a context property value is changed, all subscribers are notified of the change and retrieve the new value. To ensure that each subscriber works with an independent copy of the value, we use deep copies. This guarantees that there are no side effects when one subscriber modifies the value. If a value needs to be changed, the client modifies its copy and updates it through the corresponding model's context service.
    
    ## How
    Create an abstract model class that implements the Copyable interface. This class will provide a copy method. All application models must extend this base model class. The class will also include generic methods common to all models.
    Create an abstract ModelList class for managing lists of models. This class will include generic methods for working with arrays of models.

--- packages/api/src/services/repository/repository-rest.service.ts
+++ packages/api/src/services/repository/repository-rest.service.ts
@@ -1,7 +1,7 @@
 import {Service} from '../service';
 
 export class RepositoryRestService implements Service {
-  getRepositories(): Promise<Record<string, []>> {
+  getRepositories(): Promise<Record<string, unknown[]>> {
     // TODO: Implement a uniform method to add required headers and handle 401 errors by redirecting to the login page.
     return fetch('/rest/repositories/all')
       .then((response) => response.json());
