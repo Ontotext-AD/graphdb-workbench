c65b2afe5e0606737de323dd3fdbbc635263deda
boyantonchev <boyan.tonchev@ontotext.com>
2024-12-06 15:57:39 +0200

    GDB-10569: WB Migration: implement better deep copy functionality
    ## What
    Implement deep copy functionality.
    
    ## Why
    We use subscriptions to handle changes to certain values. When a context property value is changed, all subscribers are notified of the change and retrieve the new value. To ensure that each subscriber works with an independent copy of the value, we use deep copies. This guarantees that there are no side effects when one subscriber modifies the value. If a value needs to be changed, the client modifies its copy and updates it through the corresponding model's context service.
    
    ## How
    Create an abstract model class that implements the Copyable interface. This class will provide a copy method. All application models must extend this base model class. The class will also include generic methods common to all models.
    Create an abstract ModelList class for managing lists of models. This class will include generic methods for working with arrays of models.

---
+++ packages/api/src/services/context.servce.spec.ts
@@ -0,0 +1,116 @@
+import {ContextService} from './context.service';
+
+// Expose protected methods for testing
+class TestContextService extends ContextService {
+  public updateProperty<T>(propertyName: string, value: T): void {
+    this.updateContextProperty(propertyName, value);
+  }
+
+  public getProperty<T>(propertyName: string): T | undefined {
+    return this.getContextPropertyValue(propertyName);
+  }
+
+  public subscribeToProperty<T>(propertyName: string, callback: (value?: T) => void): () => void {
+    return this.subscribe(propertyName, callback);
+  }
+}
+
+describe('ContextService', () => {
+  let contextService: TestContextService;
+
+  beforeEach(() => {
+    contextService = new TestContextService();
+  });
+
+  test('should be called immediately when the callback function is invoked with the last value of the property', () => {
+    const valueOfTestProperty = {a: 1, b: [1, 2]};
+    const propertyName = 'testProperty';
+
+    // Given the property is updated before the callback function is registered
+    contextService.updateProperty(propertyName, valueOfTestProperty);
+
+    // When a callback function is subscribed
+    const callBackFunction = jest.fn();
+    contextService.subscribeToProperty(propertyName, callBackFunction);
+
+    // Then the callback function should be called with the last value
+    expect(callBackFunction).toHaveBeenLastCalledWith(valueOfTestProperty);
+  });
+
+  test('should return the current value in the context', () => {
+    const valueOfTestProperty = {a: 1, b: [1, 2]};
+    const propertyName = 'testProperty';
+
+    // When the property is updated
+    contextService.updateProperty(propertyName, valueOfTestProperty);
+
+    // Then the context should return a copy of the new value
+    const propertyValue = contextService.getProperty(propertyName);
+    expect(propertyValue).toEqual(valueOfTestProperty);
+    expect(propertyValue).not.toBe(valueOfTestProperty);
+  });
+
+  test('should continue calling other callback functions if one is unsubscribed', () => {
+    const valueOfTestProperty = {a: 1, b: [1, 2]};
+    const propertyName = 'testProperty';
+
+    const firstCallBackFunction = jest.fn();
+    const secondCallBackFunction = jest.fn();
+
+    // Given two subscribed functions for 'testProperty' changes
+    const unsubscribeFunction = contextService.subscribeToProperty(propertyName, firstCallBackFunction);
+    contextService.subscribeToProperty(propertyName, secondCallBackFunction);
+
+    // Clear previous mock calls for clean state
+    firstCallBackFunction.mockClear();
+    secondCallBackFunction.mockClear();
+
+    // When one function is unsubscribed
+    unsubscribeFunction();
+    // and the property is updated
+    contextService.updateProperty(propertyName, valueOfTestProperty);
+
+    // Then only the unsubscribed function should not be called
+    expect(firstCallBackFunction).toHaveBeenCalledTimes(0);
+    expect(secondCallBackFunction).toHaveBeenCalledTimes(1);
+  });
+
+  test('should call all callback functions with the changed value', () => {
+    const valueOfTestProperty = {a: 1, b: [1, 2]};
+    const propertyName = 'testProperty';
+
+    const firstCallBackFunction = jest.fn();
+    const secondCallBackFunction = jest.fn();
+
+    // Given two subscribed functions for 'testProperty' changes
+    contextService.subscribeToProperty(propertyName, firstCallBackFunction);
+    contextService.subscribeToProperty(propertyName, secondCallBackFunction);
+
+    // When the property is updated
+    contextService.updateProperty(propertyName, valueOfTestProperty);
+
+    // Then both callback functions should be called with the value of the property
+    expect(firstCallBackFunction).toHaveBeenLastCalledWith(valueOfTestProperty);
+    expect(secondCallBackFunction).toHaveBeenLastCalledWith(valueOfTestProperty);
+  });
+
+  test('should call each callback function with its own copy of the value', () => {
+    const valueOfTestProperty = {a: 1, b: [1, 2]};
+    const propertyName = 'testProperty';
+
+    const firstCallBackFunction = jest.fn();
+    const secondCallBackFunction = jest.fn();
+
+    // Given two subscribed functions for 'testProperty' changes
+    contextService.subscribeToProperty(propertyName, firstCallBackFunction);
+    contextService.subscribeToProperty(propertyName, secondCallBackFunction);
+
+    // When the property is updated
+    contextService.updateProperty(propertyName, valueOfTestProperty);
+
+    // Then both callback functions should be called with their own copy of the value
+    expect(firstCallBackFunction.mock.lastCall[0]).not.toBe(valueOfTestProperty);
+    expect(secondCallBackFunction.mock.lastCall[0]).not.toBe(valueOfTestProperty);
+    expect(firstCallBackFunction.mock.lastCall[0]).not.toBe(secondCallBackFunction.mock.lastCall[0]);
+  });
+});
--- packages/api/src/services/repository-location/repository-location-rest.service.ts
+++ packages/api/src/services/repository-location/repository-location-rest.service.ts
@@ -1,7 +1,7 @@
 import {Service} from '../service';
 
 export class RepositoryLocationRestService implements Service {
-  loadActiveLocation(): Promise<Response> {
+  getActiveRepositoryLocation(): Promise<Response> {
     // TODO: Implement a uniform method to add required headers and handle 401 errors by redirecting to the login page.
     return fetch('/rest/locations/active')
       .then((response) => response.json());
