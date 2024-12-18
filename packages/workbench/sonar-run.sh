c295ff806098dd71f07e1bfcaa5001774db370a5
svilen <svilen.velikov@ontotext.com>
2024-12-18 11:00:30 +0200

    GDB-10638 fixing sonar task running on ci

--- packages/workbench/sonar-run.sh
+++ packages/workbench/sonar-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure that the required environment variables are set
-if [ -z "$BRANCH_NAME" ]; then
+if [ -z "env.BRANCH_NAME" ]; then
     echo "Error: BRANCH_NAME environment variable is not set."
     exit 1
 fi
@@ -12,9 +12,9 @@ ghprbTargetBranch=${ghprbTargetBranch:-""}
 ghprbPullId=${ghprbPullId:-""}
 
 # Run SonarQube analysis based on the branch name
-if [ "$BRANCH_NAME" == "master" ]; then
+if [ "env.BRANCH_NAME" == "master" ]; then
     echo "Running SonarQube analysis on master branch"
-    node sonar-project.js --branch="$BRANCH_NAME"
+    node sonar-project.js --branch="env.BRANCH_NAME"
 else
     echo "Running SonarQube analysis on pull request"
     node sonar-project.js --branch="$ghprbSourceBranch" --target-branch="$ghprbTargetBranch" --pull-request-id="$ghprbPullId"
