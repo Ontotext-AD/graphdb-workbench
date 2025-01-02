c95a575796afbba874c7ad4385892af3eb6f1741
plamen-yordanov <plamen.yordanov@ontotext.com>
2025-01-02 11:15:10 +0200

    feat(GDB-10571):implement workbench footer component
    ## WHAT
    Implemented dynamic data loading for footer component
    
    ## WHY
    To populate the versions, mentioned in the footer dynamically.
    
    ## HOW
    Added a request to `/rest/info/version?local=1` upon `single-spa:first-mount`.
    If the request is successful, we update the license context with the product info.
    The `onto-footer` component now gets the data from the context and visualises it, once it is available.

--- packages/shared-components/cypress/steps/footer/footer-steps.js
+++ packages/shared-components/cypress/steps/footer/footer-steps.js
@@ -8,4 +8,20 @@ export class FooterSteps extends BaseSteps {
   static getFooter() {
     return cy.get('.footer-component');
   }
+
+  static getGraphDBLink() {
+    return FooterSteps.getFooter().contains('a', 'GraphDB').first();
+  }
+
+  static getSesameLink() {
+    return FooterSteps.getFooter().contains('a', 'RDF4J').first();
+  }
+
+  static getOntotextAdLink() {
+    return FooterSteps.getFooter().contains('a', 'Ontotext AD').first();
+  }
+
+  static loadProductInfo() {
+    return cy.get('#load-product-info').click();
+  }
 }
