b4de4654df5825d14c9de03d910b4dd8156d919e
desislava.borisova <desislava.borisova@ontotext.com>
2025-01-06 15:21:15 +0200

    GDB-11319 - create alert banner for denied permission
    GDB-11319 - move alert styles to global stylesheet and update shadow option for the component
    
    GDB-11319 - style the default slot to be hidden
    
    GDB-11319 - style permission banner like in legacy WB
    
    GDB-11319 - restore legacy WB banner
    
    GDB-11319 - remove shadow element fetchers from test steps

---
+++ packages/shared-components/cypress/e2e/permission-banner/permission-banner.cy.js
@@ -0,0 +1,23 @@
+import {PermissionBannerSteps} from "../../steps/permission-banner/permission-banner-steps";
+
+describe('onto-permission-banner', () => {
+  beforeEach(() => {
+    // Given I visit the page where the component is rendered
+    PermissionBannerSteps.visit()
+  });
+
+  it('should render the component with the correct structure', () => {
+    // Then I expect the component exists
+    PermissionBannerSteps.getPermissionBanner().should('exist');
+    PermissionBannerSteps.getMainContainer().should('exist');
+
+    // And the alert message structure should be correct
+    PermissionBannerSteps.getAlertText()
+      .within(() => {
+        PermissionBannerSteps.getTranslationLabelElement()
+          .should('contain.text', ':host{display:inline}Please choose another menu item or login as a different user.');
+        PermissionBannerSteps.getTranslationLabelElement()
+          .should('contain.text', 'Please choose another menu item or login as a different user.');
+      });
+  });
+});
