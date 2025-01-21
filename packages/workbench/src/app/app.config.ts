8024c8b9551c342cd619a5a91a440af6c9b76997
plamen-yordanov <plamen.yordanov@ontotext.com>
2025-01-21 10:25:56 +0200

    feat(GDB-11508): Add transloco i18n to workbench module
    ## What
    Add transloco i18n to workbench module
    
    ## Why
    So the workbench can have dynamic i18n support
    
    ## How
    - Added `@jsverse/transloco` package
    - Added `transloco-bootstrap.ts`, where transloco is configured and set to listen to language change
    - Moved single-spa `start()` after `bootstrap.js` promises are resolved, to ensure bundles are available, before other modules start.
    - Added `en.json` and `fr.json` bundle files for the workbench module
    
    Also:
     - Split bootstrap functions into separate modules, to avoid cluttering `ontotext-root-config.js`
     - Removed modules from the `workbench` micro-frontend to have a standalone component structure
     - Moved the `path` of `@ontotext/workbench-api` inside the base `tsconfig.json`, since both `app` and `spec` files use the same path. This fixed the issue, where typescript wasn't able to resolve imports from `@ontotext/workbench-api`

--- packages/workbench/src/app/app.config.ts
+++ packages/workbench/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {bootstrapProviders} from './bootstrap/bootstrap';
+import {APP_BASE_HREF} from '@angular/common';
+import {getSingleSpaExtraProviders} from 'single-spa-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    ...getSingleSpaExtraProviders(),
+    ...bootstrapProviders,
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ]
 };
