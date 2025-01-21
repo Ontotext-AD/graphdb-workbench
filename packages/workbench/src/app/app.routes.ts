9bd7c422e5fe0234d29c5a0d7de4cf72c347c0a4
plamen-yordanov <plamen.yordanov@ontotext.com>
2025-01-21 10:25:56 +0200

    feat(GDB-11508): Add transloco i18n to workbench module
    ## What
    Add transloco i18n to workbench module
    
    ## Why
    So the workbench can have dynamic i18n support
    
    ## How
    - Added `@jsverse/transloco` package
    - Added `transloco-bootstrap.ts`, where transloco is configured and set to listen to language change
    - Moved single-spa `start()` after `bootstrap.js` promises are resolved, to ensure bundles are available, before other modules start.
    - Added `en.json` and `fr.json` bundle files for the workbench module
    
    Also:
     - Split bootstrap functions into separate modules, to avoid cluttering `ontotext-root-config.js`
     - Removed modules from the `workbench` micro-frontend to have a standalone component structure
     - Moved the `path` of `@ontotext/workbench-api` inside the base `tsconfig.json`, since both `app` and `spec` files use the same path. This fixed the issue, where typescript wasn't able to resolve imports from `@ontotext/workbench-api`

--- packages/workbench/src/app/app.routes.ts
+++ packages/workbench/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {SparqlComponent} from './sparql/sparql.component';
+import {GraphqlComponent} from './graphql/graphql.component';
+import {EmptyRouteComponent} from './empty-route/empty-route.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'sparql-new', component: SparqlComponent},
+  {path: 'graphql', component: GraphqlComponent},
+  {path: "**", component: EmptyRouteComponent}
+];
