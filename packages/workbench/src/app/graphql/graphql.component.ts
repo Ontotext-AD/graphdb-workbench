50615a2ad865c641f27265bb2a97bd7f1e6dc256
plamen-yordanov <plamen.yordanov@ontotext.com>
2025-01-21 10:25:56 +0200

    feat(GDB-11508): Add transloco i18n to workbench module
    ## What
    Add transloco i18n to workbench module
    
    ## Why
    So the workbench can have dynamic i18n support
    
    ## How
    - Added `@jsverse/transloco` package
    - Added `transloco-bootstrap.ts`, where transloco is configured and set to listen to language change
    - Moved single-spa `start()` after `bootstrap.js` promises are resolved, to ensure bundles are available, before other modules start.
    - Added `en.json` and `fr.json` bundle files for the workbench module
    
    Also:
     - Split bootstrap functions into separate modules, to avoid cluttering `ontotext-root-config.js`
     - Removed modules from the `workbench` micro-frontend to have a standalone component structure
     - Moved the `path` of `@ontotext/workbench-api` inside the base `tsconfig.json`, since both `app` and `spec` files use the same path. This fixed the issue, where typescript wasn't able to resolve imports from `@ontotext/workbench-api`

--- packages/workbench/src/app/graphql/graphql.component.ts
+++ packages/workbench/src/app/graphql/graphql.component.ts
@@ -1,10 +1,13 @@
 import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {AuthenticationService, RepositoryContextService, ServiceProvider, RepositoryList} from "@ontotext/workbench-api";
+import {TranslocoPipe} from '@jsverse/transloco';
 
 @Component({
   selector: 'app-graphql',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslocoPipe
+  ],
   templateUrl: './graphql.component.html',
   styleUrl: './graphql.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
