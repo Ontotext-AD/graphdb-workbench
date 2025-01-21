dd45958a7a6a7a3394b392df00e24bb69d173656
plamen-yordanov <plamen.yordanov@ontotext.com>
2025-01-21 10:25:56 +0200

    feat(GDB-11508): Add transloco i18n to workbench module
    ## What
    Add transloco i18n to workbench module
    
    ## Why
    So the workbench can have dynamic i18n support
    
    ## How
    - Added `@jsverse/transloco` package
    - Added `transloco-bootstrap.ts`, where transloco is configured and set to listen to language change
    - Moved single-spa `start()` after `bootstrap.js` promises are resolved, to ensure bundles are available, before other modules start.
    - Added `en.json` and `fr.json` bundle files for the workbench module
    
    Also:
     - Split bootstrap functions into separate modules, to avoid cluttering `ontotext-root-config.js`
     - Removed modules from the `workbench` micro-frontend to have a standalone component structure
     - Moved the `path` of `@ontotext/workbench-api` inside the base `tsconfig.json`, since both `app` and `spec` files use the same path. This fixed the issue, where typescript wasn't able to resolve imports from `@ontotext/workbench-api`

---
+++ packages/workbench/src/app/bootstrap/bootstrap.ts
@@ -0,0 +1,8 @@
+import {translocoBootstrapProviders} from './transloco/transloco-bootstrap';
+
+/**
+ * List of bootstrap providers for the application.
+ */
+export const bootstrapProviders = [
+  ...translocoBootstrapProviders
+]
