d937aa05161d41d870cf38c73e75329f3c5d0c7c
svilen <svilen.velikov@ontotext.com>
2025-01-29 14:38:05 +0200

    Changes compilation target of the javascript to ecmascript 2022

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -216,6 +216,13 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
                 test: /\.mjs$/,
                 include: /node_modules/,
                 type: 'javascript/auto'
