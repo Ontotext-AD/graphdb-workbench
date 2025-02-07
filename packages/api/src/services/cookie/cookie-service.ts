297f568d4f016e8024f9d5aec81df05ebbb6a8f8
plamen-yordanov <plamen.yordanov@ontotext.com>
2025-02-07 13:29:01 +0200

    GDB-11320 Implement cookie consent component
    ## What
    Implement cookie consent component and cookie policy dialog, along with needed periphery
    
    ## Why
    To notify users, that we collect data and ask for consent
    
    ## How
    - Copied `ontotext-dialog-web-component`  and the `HtmlUtil` from yasgui, with some adjustments
    - Created `onto-cookie-consent` component, which is displayed in the footer
    - Created `cookie-policy-dialog`, which holds information on types of cookies and toggles user consent
    - Created `toggle-switch` component to hold encapsulate toggling functionality
    - Replaced `css` with `sass` in `root-config` package, in order to organize styles more efficiently
    - Added partial files for global styles for `_alert`, `_animations`, `_button`.
    - Added `sass-loader` to handle the files
    
    The cookie consent component will be shown, if tracking is allowed and the current user has not consented to the cookie policy yet. This means, whenever the license is free/evaluation and we are not in dev mode
    
    ## Testing
    jest unit tests and cypress tests

---
+++ packages/api/src/services/cookie/cookie-service.ts
@@ -0,0 +1,30 @@
+import {Service} from '../../providers/service/service';
+import {AuthenticatedUser} from '../../models/security';
+import {CookieConsent} from '../../models/cookies';
+import {ServiceProvider} from '../../providers';
+import {SecurityContextService, SecurityService} from '../security';
+
+/**
+ * Service class for handling cookie-related operations.
+ */
+export class CookieService implements Service {
+
+  /**
+   * Accepts the cookie policy for the authenticated user.
+   *
+   * @returns {Promise<void>} - A promise that resolves when the request to the backend has passed.
+   */
+  acceptCookiePolicy(): Promise<void> {
+    const user = this.setAcceptedCookiePolicy();
+    return ServiceProvider.get(SecurityService).updateUserData(user);
+  }
+
+  private setAcceptedCookiePolicy(): AuthenticatedUser {
+    const user = ServiceProvider.get(SecurityContextService).getAuthenticatedUser() || new AuthenticatedUser({});
+    const cookieConsent = new CookieConsent(user?.appSettings?.COOKIE_CONSENT as CookieConsent);
+
+    cookieConsent.policyAccepted = true;
+    user.appSettings.COOKIE_CONSENT = cookieConsent;
+    return user;
+  }
+}
---
+++ packages/shared-components/src/utils/html-util.ts
@@ -0,0 +1,72 @@
+const INTERACTIVE_ELEMENTS_SELECTOR = 'button, [href], input:not([type="hidden"]), select, textarea, [tabindex]:not([tabindex="-1"])';
+
+export class HtmlUtil {
+  /**
+   * Setter for the document body overflow property.
+   *
+   * @param newOverflow - the new overflow value.
+   *
+   * @return the old overflow value.
+   */
+  static setDocumentBodyOverflow(newOverflow: string): string {
+    const oldOverflow = document.body.style.overflow;
+    document.body.style.overflow = newOverflow;
+    return oldOverflow;
+  }
+
+  /**
+   * Hides the document body overflow.
+   *
+   * @return the value of overflow before set it to hidden.
+   */
+  static hideDocumentBodyOverflow(): string {
+    return HtmlUtil.setDocumentBodyOverflow('hidden');
+  }
+
+  /**
+   * Focuses the next element within a specified parent element based on the <code>activeElementSelector</code>, that selects all elements to be focused.
+   *
+   * @param parentElement - The parent element containing the focusable elements.
+   * @param activeElementSelector - The CSS selector for identifying focusable elements.
+   */
+  static focusNextElement(parentElement: HTMLElement, activeElementSelector = INTERACTIVE_ELEMENTS_SELECTOR): void {
+    const focusableElements: any [] = Array.from(parentElement.querySelectorAll(activeElementSelector));
+    if (focusableElements.length > 0) {
+      const currentIndex = focusableElements.indexOf(document.activeElement);
+      const nextIndex = (currentIndex + 1) % focusableElements.length;
+      const nextActiveElement = focusableElements[nextIndex] as HTMLElement;
+      if (nextActiveElement) {
+        nextActiveElement.focus();
+      }
+    }
+  }
+
+  /**
+   * Focuses the previous element within a specified parent element based on the <code>activeElementSelector</code>, that selects all elements to be focused.
+   *
+   * @param parentElement - The parent element containing the focusable elements.
+   * @param activeElementSelector - The CSS selector for identifying focusable elements.
+   */
+  static focusPreviousElement(parentElement: HTMLElement, activeElementSelector = INTERACTIVE_ELEMENTS_SELECTOR): void {
+    const focusableElements: any[] = Array.from(parentElement.querySelectorAll(activeElementSelector));
+    if (focusableElements.length > 0) {
+      const currentIndex = focusableElements.indexOf(document.activeElement);
+      const previousIndex = (currentIndex - 1 + focusableElements.length) % focusableElements.length;
+      const previousActiveElement = focusableElements[previousIndex] as HTMLElement;
+      if (previousActiveElement) {
+        previousActiveElement.focus();
+      }
+    }
+  }
+
+  static preventLeavingDialog(hostElement: HTMLElement, ev: KeyboardEvent) {
+    if (ev.key === 'Tab') {
+      ev.preventDefault();
+      if (ev.shiftKey) {
+        HtmlUtil.focusPreviousElement(hostElement);
+      } else {
+        HtmlUtil.focusNextElement(hostElement);
+      }
+    }
+  }
+}
