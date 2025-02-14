67e2ceab5fb80b673cb0c6126de36b898fc38557
plamen-yordanov <plamen.yordanov@ontotext.com>
2025-02-14 13:45:13 +0200

    GDB-11536 Fix menu items not shown in prod mode
    ## What
    Fix menu items, which weren't being shown sometimes in prod mode
    
    ## Why
    Because menu items should be shown in those circumstances
    
    ## How
    Previously the `ontotext-root-config` waited for the navbar to be available and added the menu items to it. The `onto-layout` component listens for changes to the security context and authenticated user. Everytime a change is made, the menu items' visibility is recalculated. In prod mode, the requests sometimes finish, before root-config sets the menu items of the navbar. Leaving nothing to be filtered and no elements are displayed.
    Now the initial population of the menu items is moved into `onto-layout`, immediately after the navbar reference is resolved. Then we recalculate the visibility of the items. This way it doesn't matter in what order the operations execute.
    
    ## Testing
    Existing

--- packages/shared-components/global.d.ts
+++ packages/shared-components/global.d.ts
@@ -1,7 +1,10 @@
-export {}
+import {PluginRegistry} from './src/models/plugin/plugin-registry';
 
 declare global {
   interface Window {
     wbDevMode: boolean;
+    PluginRegistry: PluginRegistry
   }
 }
+
+export {}
