35cffdecbd7ee3c76403c8cca523d1e1d99c4540
boyantonchev <boyan.tonchev@ontotext.com>
2025-02-17 09:57:17 +0200

    GDB-11281: Trying to delete a other user's saved query results in an incorrect error
    ## What
    An incorrect error message, "Query 'Shared with other users' doesn't exist", appears when attempting to delete or edit a query created by another user.
    
    ## Why
    There are no restrictions on actions that the currently logged-in user can perform on a saved query.
    
    ## How
    Iterate over all saved queries and mark them as readonly if the query owner is different from the currently logged-in user.

---
+++ test-cypress/integration/sparql-editor/saved-query/readonly-query.spec.js
@@ -0,0 +1,59 @@
+import {SparqlEditorSteps} from "../../../steps/sparql-editor-steps";
+import {YasguiSteps} from "../../../steps/yasgui/yasgui-steps";
+import {QueryStubs} from "../../../stubs/yasgui/query-stubs";
+import {UserAndAccessSteps} from "../../../steps/setup/user-and-access-steps";
+import {SavedQuery} from "../../../steps/yasgui/saved-query";
+import {SavedQueriesDialog} from "../../../steps/yasgui/saved-queries-dialog";
+
+const USER_NAME = 'saved_query_user';
+const USER_ADMINISTRATOR = 'admin';
+const PASSWORD = 'root';
+
+describe('Readonly saved query', () => {
+
+    let repositoryId;
+
+    beforeEach(() => {
+        repositoryId = 'sparql-editor-' + Date.now();
+        QueryStubs.stubQueryCountResponse();
+        cy.createRepository({id: repositoryId});
+        cy.presetRepository(repositoryId);
+        QueryStubs.stubDefaultQueryResponse(repositoryId);
+        cy.createUser({username: USER_NAME, password: PASSWORD});
+        UserAndAccessSteps.visit();
+        UserAndAccessSteps.toggleSecurity();
+    });
+
+    afterEach(() => {
+        UserAndAccessSteps.logout();
+        cy.deleteRepository(repositoryId);
+        UserAndAccessSteps.visit();
+        UserAndAccessSteps.loginWithUser(USER_ADMINISTRATOR, PASSWORD);
+        UserAndAccessSteps.toggleSecurity();
+        cy.deleteUser(USER_NAME);
+    });
+
+    it('Should not allow modifying a saved query if it is readonly', () => {
+        // Given: There is a public saved query created by a user.
+        UserAndAccessSteps.loginWithUser(USER_NAME, PASSWORD);
+        SparqlEditorSteps.visitSparqlEditorPage();
+        YasguiSteps.getYasgui().should('be.visible');
+        const savedQueryName = SavedQuery.generateQueryName();
+        SavedQuery.create(savedQueryName);
+        UserAndAccessSteps.logout();
+
+        // When: I log in with another user
+        UserAndAccessSteps.loginWithUser(USER_ADMINISTRATOR, PASSWORD);
+        // and open the popup with the saved query.
+        SparqlEditorSteps.visitSparqlEditorPage();
+        YasguiSteps.showSavedQueries();
+
+        // Then: I expect:
+        // 1. The delete button should not be visible because a saved query can only be deleted by the user who created it.
+        SavedQueriesDialog.getDeleteQueryButtonByName(savedQueryName).should('not.exist');
+        // 2. The edit button should not be visible because a saved query can only be edited by the user who created it.
+        SavedQueriesDialog.getEditQueryButtonByName(savedQueryName).should('not.exist');
+        // 3. The share button should be visible because a public saved query should be visible to all users.
+        SavedQueriesDialog.getShareQueryButtonByName(savedQueryName).should('exist');
+    });
+});
