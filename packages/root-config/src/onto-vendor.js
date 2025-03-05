f5affa97566e89c70642192e84620f5fa6524174
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-03-05 13:59:58 +0200

    GDB-11839 Migrate font awesome stylesheets
    ## What
    Migrate font awesome styles to the new workbench
    
    ## Why
    To have them available at a global level throughout the application
    
    ## How
    - Copied the css and font files for font awesome in `root-config` package
    - Imported them in root-config via `ontotext-vendor.js`
    - Removed the imports of these files in the legacy workbench
    - Removed the NPM dependency to font awesome in root-config and shared-components

---
+++ packages/root-config/src/onto-vendor.js
@@ -0,0 +1,5 @@
+import './styles/css/font-awesome/solid.min.css';
+import './styles/css/font-awesome/regular.min.css';
+import './styles/css/font-awesome/custom-icons.min.css';
+import './styles/css/font-awesome/fontawesome.min.css';
+import './styles/css/font-awesome/sharp-regular.min.css';
