16d9828c0a84d73886e0584400de0e8e69b42905
Yordan Alexandrov <yordan.alexandrov@ontotext.com>
2025-03-06 14:45:43 +0200

    GDB-11826 Extend create/edit agent options
    ## What
    Extend the create and edit agent steps to be able to configure which fields to be filled
    
    ## Why
    This will allow the guide creator to add specificity to the guide steps
    
    ## How
    - added options for name, model and user instructions to both create and edit steps
    - added `disable` option for extraction method, which specifies if the method should be enabled or disabled
    - extracted agent configuration into its own complex step to reduce repeating
    - extracted `end-on-api-key-error` and added a check if the toaster is for the api-key
    - added another core step `wait-for-element-to-show` which waits for an element to be shown before proceeding with next step

--- src/js/angular/guides/steps/complex/ttyg/common/sparql-search-method/plugin.js
+++ src/js/angular/guides/steps/complex/ttyg/common/sparql-search-method/plugin.js
@@ -1,34 +1,52 @@
 PluginRegistry.add('guide.step', [
     {
-        guideBlockName: 'enable-sparql-search-method',
+        guideBlockName: 'sparql-search-method',
         getSteps: (options, services) => {
             const GuideUtils = services.GuideUtils;
-            options.mainAction = 'enable-sparql-search-method';
+            options.mainAction = 'sparql-search-method';
+
+            const shouldToggleOff = options.disable;
+            const toggleSelector = GuideUtils.getGuideElementSelector('query-method-sparql_search-input');
+
+            if (shouldToggleOff) {
+                return [{
+                    guideBlockName: 'clickable-element',
+                    options: angular.extend({}, {
+                        content: 'guide.step_plugin.sparql-search-method.disable-toggle',
+                        class: 'toggle-sparql-search-guide-dialog',
+                        url: '/ttyg',
+                        showOn: () => GuideUtils.isChecked(toggleSelector),
+                        elementSelector: GuideUtils.getGuideElementSelector('query-method-sparql_search'),
+                        clickableElementSelector: toggleSelector,
+                        onNextValidate: () => Promise.resolve(!GuideUtils.isChecked(toggleSelector))
+                    }, options)
+                }]
+            }
 
             return [
                 {
                     guideBlockName: 'info-message',
                     options: angular.extend({}, {
-                        content: `guide.step_plugin.enable-sparql-search-method.content`,
+                        content: `guide.step_plugin.sparql-search-method.content`,
                         class: 'info-sparql-search-guide-dialog',
-                        url: '/ttyg'
+                        url: '/ttyg',
                     }, options)
                 },
                 {
                     guideBlockName: 'clickable-element',
                     options: angular.extend({}, {
-                        content: 'guide.step_plugin.enable-sparql-search-method.enable-toggle',
-                        class: 'enable-toggle-sparql-search-guide-dialog',
+                        content: 'guide.step_plugin.sparql-search-method.enable-toggle',
+                        class: 'toggle-sparql-search-guide-dialog',
                         url: '/ttyg',
                         elementSelector: GuideUtils.getGuideElementSelector('query-method-sparql_search'),
-                        clickableElementSelector: GuideUtils.getGuideElementSelector('query-method-sparql_search-input'),
-                        onNextValidate: () => Promise.resolve(GuideUtils.isChecked(GuideUtils.getGuideElementSelector('query-method-sparql_search-input')))
+                        clickableElementSelector: toggleSelector,
+                        onNextValidate: () => Promise.resolve(GuideUtils.isChecked(toggleSelector))
                     }, options)
                 },
                 {
                     guideBlockName: 'clickable-element',
                     options: angular.extend({}, {
-                        content: 'guide.step_plugin.enable-sparql-search-method.enable-ontology-from-graph',
+                        content: 'guide.step_plugin.sparql-search-method.enable-ontology-from-graph',
                         class: 'enable-ontology-from-graph-guide-dialog',
                         url: '/ttyg',
                         elementSelector: GuideUtils.getGuideElementSelector('sparql-ontology-graph-option'),
@@ -39,7 +57,7 @@ PluginRegistry.add('guide.step', [
                 {
                     guideBlockName: 'input-element',
                     options: angular.extend({}, {
-                        content: `guide.step_plugin.enable-sparql-search-method.type-ontology-graph-name`,
+                        content: `guide.step_plugin.sparql-search-method.type-ontology-graph-name`,
                         class: 'input-ontology-graph-name-guide-dialog',
                         url: '/ttyg',
                         elementSelector: GuideUtils.getGuideElementSelector('sparql-ontology-graph-input'),
