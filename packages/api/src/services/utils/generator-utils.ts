790f4237ff773319395c95c65479a2ef995b5dba
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-04-10 17:06:11 +0300

    GDB-11314 Add autocomplete and filter to RDF search
    ## What
    Add autocomplete check and filtering, upon typing in the RDF search component
    
    ## Why
    As part of the step by step migration of the component to the new workbench
    
    ## How
    - Added request to check the state of the autocomplete on startup. Also added an event to update the context of the autocomplete state, on repository change
    
    - Added toast notification, whenever a user starts typing in the search component, without turning on autocomplete. The warning appears only once and contains a link, which redirects to the autocomplete page
    
    - Added requests, when autocomplete is enabled and the user is typing. The filtered results are then displayed in the component.
    
    - Enhanced the toast config to include an `onClick` function and a `removeOnClick` boolean property. The first is currently used to navigate to the autocomplete page, when the toast notification is clicked. The second is to close the toast upon click, as this is the current behavior in the legacy workbench
    
    ## Testing
    jest/cypress

--- packages/api/src/services/utils/generator-utils.ts
+++ packages/api/src/services/utils/generator-utils.ts
@@ -15,4 +15,19 @@ export class GeneratorUtils {
   static uuid(): string {
     return window.crypto.randomUUID();
   }
+
+  /**
+   * Returns a hash code from a string
+   * @param  {String} str The string to hash.
+   * @return {Number}    A 32bit integer
+   */
+  static hashCode(str: string): number {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+      const chr = str.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  }
 }
