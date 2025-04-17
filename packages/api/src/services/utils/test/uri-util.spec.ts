b66fd104b9c3c9b3005bfabd04e5bff203e654ea
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-04-17 09:59:10 +0300

    GDB-11314 Implement RDF suggestion selection
    ## What
    Implement RDF search suggestion selection
    
    ## Why
    To have the ability to view a resource in detail
    
    ## How
    Added loading of repository namespaces on repositoryId change. This is needed to be able to expand prefixes
    Added support for `application/sparql-results+json` content type in `http-service`, since this is what the namespaces request returns
    Added event, when a suggestion is clicked. If the suggestion is a prefix it will be "expanded" into the input menu but not selected. If the suggestion is a complete result, the event will be thrown and notify listeners
    Added context property, since there could be more than one search component in one page. This way the event listener knows which event to capture.
    When the `rdf-search` component captures the event it will open the resource in a new tab. Depending on the value of the radio buttons, the resource can be opened in table view or graph visualization view.
    
    ## Testing
    cypress/jest

--- packages/api/src/services/utils/test/uri-util.spec.ts
+++ packages/api/src/services/utils/test/uri-util.spec.ts
@@ -9,4 +9,14 @@ describe('UriUtil', () => {
   test('shortenIri should shortens a IPv4 address by extracting the hostname and port', () => {
     expect(UriUtil.shortenIri('http://192.168.1.1:3000/path')).toBe('192.168.1.1:3000');
   });
+
+  test('should create a redirect URL for autocomplete', () => {
+    const autocompleteUrl = '/resource';
+    const resourceUrl = 'http://example.com/resource/123';
+    const expectedRedirectUrl = `${autocompleteUrl}?uri=http%3A%2F%2Fexample.com%2Fresource%2F123`;
+
+    const result = UriUtil.createAutocompleteRedirect(autocompleteUrl, resourceUrl);
+
+    expect(result).toBe(expectedRedirectUrl);
+  });
 });
