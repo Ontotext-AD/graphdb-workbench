7418aaaad1d11294e5b3ffcd514af173f0776348
Yordan Alexandrov <yordan.alexandrov@graphwise.ai>
2025-04-24 11:39:26 +0300

    GDB-12152 Display confirmation dialog when changing the language on the SPARQL view
    ## What
    Display confirmation dialog when changing the language on the SPARQL view
    
    ## Why
    Since the page must be reloaded in order to change the language, the user might lose data about running queries in the SPARQL editor
    
    ## How
    - defined a new `BeforeChangeValidationPromise` function which takes a value and returns a Promise which resolves to a boolean
    - added an array of `BeforeChangeValidationPromise` in `ValueContext` to hold validation functions
    - refactored `subscribe` method of `ValueContext` to accept validation functions and also to remove them in the unsubscribe function
    - added a new async method `canUpdate` in `ValueContext` which calls the validation functions and returns true if they all resolve to true or false if some resolves to false or if the promise is rejected
    - added a new method `validateAndUpdateContextProperty` in `ContextService` to be used to set a context property only after it is validated by the validation functions of the context
    - refactored `updateSelectedLanguage` in `LanguageContextService` to use the new `validateAndUpdateContextProperty`
    - refactored logic in the  `SparqlEditorCtrl` to listen to `LanguageContextService` language change trigger and add a validation function to show a modal for user confirmation
    
    ## Also in this MR
    - added a `.editorconfig` to the legacy workbench package because of different formatting
    - fixed a typo in `SparqlTemplateCreateCtrl`

--- packages/api/src/services/context/context.service.ts
+++ packages/api/src/services/context/context.service.ts
@@ -1,6 +1,7 @@
 import {ValueContext} from '../../models/context/value-context';
 import {ValueChangeCallback} from '../../models/context/value-change-callback';
 import {Service} from '../../providers/service/service';
+import {BeforeChangeValidationPromise} from '../../models/context/before-change-validation-promise';
 
 /**
  * Abstract service that manages the context for various properties and allows for value retrieval, updates
@@ -29,6 +30,27 @@ export abstract class ContextService<TFields extends Record<string, unknown>> im
   }
 
   /**
+   * Validates and updates the value of a specific property if validation passes.
+   * This method first checks if the value can be updated and only updates the value if the validation succeeds.
+   *
+   * @param propertyName The name of the property to be validated and updated.
+   * @param value The new value to be validated and potentially assigned to the property.
+   *
+   * @returns A promise that resolves to a boolean indicating whether the validation passed and the update was performed.
+   *          Returns true if the value was successfully updated, false otherwise.
+   *
+   * @template T The type of the value to be validated and set.
+   */
+  async validateAndUpdateContextProperty<T>(propertyName: string, value: T): Promise<boolean> {
+    const valueContext = this.getOrCreateValueContext(propertyName);
+    const canUpdate = await valueContext.canUpdate(value);
+    if (canUpdate) {
+      valueContext.setValue(value);
+    }
+    return canUpdate;
+  }
+
+  /**
    * Retrieves the current value of a specific property.
    *
    * @param propertyName The name of the property to retrieve the value for.
@@ -43,24 +65,26 @@ export abstract class ContextService<TFields extends Record<string, unknown>> im
   }
 
   /**
-   * Register the <code>callbackFunction</code> that will be called when the value of property with <code>propertyName</code> changes.
-   * It immediately calls the callback with the current value and then subscribes the callback for future changes to the property.
-   *
-   * @param propertyName The name of the property to subscribe to.
-   * @param callbackFunction The callback function to be called when the property value changes.
-   *                         It will receive the current value of the property as its argument.
-   *
-   * @returns A function that can be called to unsubscribe from the property updates.
-   *
-   * @template T The type of the value that the callback function will receive.
+ * Registers a <code>callbackFunction</code> function that will be called when the value of a property with <code>propertyName</code> changes.
+ * It immediately calls the callback with the current value and then subscribes the callback for future changes to the property.
+ *
+ * @param propertyName The name of the property to subscribe to.
+ * @param callbackFunction The callback function to be called when the property value changes.
+ *                         It will receive the current value of the property as its argument.
+ * @param beforeChangeValidationPromise Optional promise that can be used to validate the new value before it is set.
+ *                                     If the promise resolves to false, the value change will be rejected.
+ *
+ * @returns A function that can be called to unsubscribe from the property updates.
+ *
+ * @template T The type of the value that the callback function will receive.
    */
-  protected subscribe<T>(propertyName: string, callbackFunction: ValueChangeCallback<T | undefined>): () => void {
+  protected subscribe<T>(propertyName: string, callbackFunction: ValueChangeCallback<T | undefined>, beforeChangeValidationPromise?: BeforeChangeValidationPromise<T>): () => void {
     if (callbackFunction) {
       // Call the callback immediately with the current value
       callbackFunction(this.getContextPropertyValue(propertyName));
     }
     // Return the unsubscribe function from the context
-    return this.getOrCreateValueContext<T>(propertyName).subscribe(callbackFunction);
+    return this.getOrCreateValueContext<T>(propertyName).subscribe(callbackFunction, beforeChangeValidationPromise);
   }
 
   /**
