2033767241306f384117802310a3a5771acbdb04
Teodossi Dossev <teodossi.dossev@graphwise.ai>
2025-05-12 13:42:25 +0300

    fix(GDB-12246): move subscription logic to header to fix lost updates after routing
    ## What
    Moved repository list and selection logic from <onto-repository-selector> into its parent (<onto-header>), making the selector a fully controlled, stateless component
    
    Also enabled liveReload in the Webpack dev server and added file watchers for shared components and API packages.
    
    ## Why
    Fixes a bug where newly created repositories didn't show up in the dropdown after creation.
    
    The issue was caused by lifecycle conflicts between Stencil and Single SPA.
    Stencil components trigger disconnectedCallback when detached by Single SPA routing. This was causing active subscriptions to be dropped silently, leading to stale or empty repository state in both <onto-header> and <onto-repository-selector> after navigation.
    
    To prevent that, subscriptions were moved to connectedCallback in the parent so they are reinstated on re-attachment. Additionally, the selector was refactored into a dumb component, as it shouldn't contain business logic
    
    liveReload was also enabled in the dev setup to simplify development and eliminate the need for full reloads or restarts
    
    ## How
    - Removed internal repository list and selection logic from <onto-repository-selector>.
    - Made the component fully controlled via items, currentRepository, and dropdownButtonTooltip props.
    - Moved subscriptions and dropdown item computation to <onto-header>
    - Enabled liveReload and added nodemon watchers for API and shared components
    - Updated scripts in package.json

--- packages/api/src/services/storage/local-storage-subscription-handler.service.ts
+++ packages/api/src/services/storage/local-storage-subscription-handler.service.ts
@@ -38,7 +38,6 @@ export class LocalStorageSubscriptionHandlerService implements Service {
    */
   private resolveHandler(namespace: string, propertyName: string): ContextService<Record<string, unknown>> | undefined {
     if (!namespace) {
-      // eslint-disable-next-line no-console
       console.warn('Namespace is required to resolve a context property change handler.');
       return;
     }
@@ -47,7 +46,6 @@ export class LocalStorageSubscriptionHandlerService implements Service {
         return service.canHandle(propertyName);
       });
     if (!handler) {
-      // eslint-disable-next-line no-console
       console.warn(`No context property change handler found for namespace: ${namespace} and property: ${propertyName}`);
       return;
     }
