04e2c86afc0ded364f4990e877081e25f91e9ef6
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-05-21 09:34:31 +0300

    GDB-12173 Add initial state tests for SPARQL view
    ## What
    Add initial state tests for SPARQL view
    
    ## Why
    To ensure the view starts up correctly with and without selected repository
    
    ## How
    Added spec files with and without selected repository, which assert the initial state in those cases
    
    ## Testing
    cypress

--- e2e-tests/steps/error-steps.js
+++ e2e-tests/steps/error-steps.js
@@ -6,4 +6,19 @@ export class ErrorSteps {
     static verifyError(errorMessage) {
         ErrorSteps.getErrorElement().contains(errorMessage);
     }
+
+    static getRepositoryErrors() {
+        return cy.get('.card.repository-errors')
+    }
+
+    static getNoConnectedRepoMessage() {
+        return this.getRepositoryErrors().find('.alert.lead.alert-info');
+    }
+
+    static verifyNoConnectedRepoMessage() {
+        this.getRepositoryErrors().should('be.visible');
+        this.getNoConnectedRepoMessage()
+            .should('be.visible')
+            .and('contain', 'Some functionalities are not available because you are not connected to any repository.');
+    }
 }
