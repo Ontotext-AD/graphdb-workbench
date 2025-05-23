2b9c975f4327dec25601c1c2f4d321561780db7c
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-05-23 14:17:54 +0300

    GDB-12173 Add initial state tests for class relations
    ## What
    Add initial state tests for class relations
    
    ## Why
    To ensure the view starts up correctly with and without selected repository and with imported data in the repository
    
    ## How
    Added spec files with and without selected repository, as well as with imported data, which assert the initial state of the view
    
    ## Testing
    cypress

--- e2e-tests/e2e-legacy/explore/class-relationships/class-relationships.spec.js
+++ e2e-tests/e2e-legacy/explore/class-relationships/class-relationships.spec.js
@@ -1,9 +1,9 @@
-import ClassViewsSteps, {ALL_GRAPHS, GRAPH_FILE, NEWS_GRAPH} from "../../steps/class-views-steps";
+import ClassViewsSteps, {ALL_GRAPHS, GRAPH_FILE, NEWS_GRAPH} from "../../../steps/class-views-steps";
+import {ClassRelationshipsSteps} from "../../../steps/explore/class-relationships-steps";
 
 const FILE_TO_IMPORT = 'wine.rdf';
 
-// TODO: Fix me. Broken due to migration (Error: beforeEach)
-describe.skip('Class relations screen validation', () => {
+describe('Class relations screen validation', () => {
 
     let repositoryId;
 
@@ -14,11 +14,10 @@ describe.skip('Class relations screen validation', () => {
 
         cy.importServerFile(repositoryId, FILE_TO_IMPORT);
 
-        cy.visit('/relationships');
-        cy.window();
+        ClassRelationshipsSteps.visit();
         // The diagram and the list should be visible in order to assume the page is ready
-        cy.get('#dependencies-chord').should('be.visible');
-        cy.get('#wb-dependencies-classInClasses').should('be.visible');
+        ClassRelationshipsSteps.getDependenciesDiagram().should('be.visible');
+        ClassRelationshipsSteps.getDependenciesList().should('be.visible');
     });
 
     afterEach(() => {
@@ -27,47 +26,43 @@ describe.skip('Class relations screen validation', () => {
 
     it('Test initial state', function () {
         // Filter field should be visible
-        getFilterField().should('be.visible');
+        ClassRelationshipsSteps.getFilterField().should('be.visible');
         // Dependencies label should be present and showing proper text
-        cy.get('.available-dependencies-label')
+        ClassRelationshipsSteps.getAvailableDependenciesLabel()
             .should('be.visible')
             .contains('Showing the dependencies between 10 classes');
         // Directions filter buttons should be visible
-        getDirectionFilter().should('be.visible');
+        ClassRelationshipsSteps.getDirectionFilter().should('be.visible');
         // And the "All" radio button must be selected by default
-        verifySelectedDirectionFilter('all');
+        ClassRelationshipsSteps.verifySelectedDirectionFilter('all');
         // The toolbar and its buttons should be visible
-        getRelationsToolbar().should('be.visible').within(() => {
-            cy.get('.remove-all-classes-btn').should('be.visible');
-            cy.get('.reload-diagram-btn').should('be.visible');
-            cy.get('.export-diagram-btn').should('be.visible');
-        });
+        ClassRelationshipsSteps.verifyRelationsToolbarContent();
         // Class relations list should be visible and have 10 classes selected
-        getDependenciesList().should('be.visible').within(() => {
+        ClassRelationshipsSteps.getDependenciesList().should('be.visible').within(() => {
             cy.get('.item .row.active').should('have.length', 10);
         });
         // Diagram should be visible and showing 10 classes
-        cy.get('#dependencies-chord').should('be.visible').within(() => {
+        ClassRelationshipsSteps.getDependenciesDiagram().should('be.visible').within(() => {
             cy.get('.group').should('have.length', 10);
         });
     });
 
     it('Search for a class', function () {
         // Expect 39 rows initially to be visible
-        verifyListLength(39);
+        ClassRelationshipsSteps.verifyListLength(39);
         // Filter by partial name
-        filterByClass(':Wine');
+        ClassRelationshipsSteps.filterByClass(':Wine');
         // Expecting 6 rows to be present
-        verifyListLength(6);
+        ClassRelationshipsSteps.verifyListLength(6);
         // Clear the filter and expect all rows to be visible again
-        getFilterField().clear();
-        verifyListLength(39);
+        ClassRelationshipsSteps.getFilterField().clear();
+        ClassRelationshipsSteps.verifyListLength(39);
     });
 
     it('Test class relationships for given graph', () => {
         cy.importServerFile(repositoryId, GRAPH_FILE, {"context": NEWS_GRAPH});
         // Should re-enter page to display Graph dropdown
-        cy.visit('/relationships');
+        ClassRelationshipsSteps.visit();
         ClassViewsSteps.verifyDataChangedWarning();
         ClassViewsSteps.verifyGraphIsDisplayed(ALL_GRAPHS);
 
@@ -80,32 +75,4 @@ describe.skip('Class relations screen validation', () => {
         ClassViewsSteps.selectGraphFromDropDown(NEWS_GRAPH);
         ClassViewsSteps.verifyGraphIsDisplayed(NEWS_GRAPH);
     });
-
-    function getFilterField() {
-        return cy.get('.search-filter-field');
-    }
-
-    function filterByClass(name) {
-        getFilterField().type(name);
-    }
-
-    function getDirectionFilter() {
-        return cy.get('.direction-filter');
-    }
-
-    function verifySelectedDirectionFilter(name) {
-        getDirectionFilter().find('input[type=radio]:checked').should('have.value', name);
-    }
-
-    function getRelationsToolbar() {
-        return cy.get('.relations-toolbar');
-    }
-
-    function getDependenciesList() {
-        return cy.get('#wb-dependencies-classInClasses');
-    }
-
-    function verifyListLength(count) {
-        getDependenciesList().find('.item .row').should('have.length', count);
-    }
 });
