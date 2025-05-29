276994f253afd9b27c541f235abec314f9783fdd
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-05-29 09:19:48 +0300

    fix comments
    Added `data-test-id` attribute to explore submenus to be used for test specific selection
    Added a cypress command, which can be called to retrieve an element by its `data-test-id` attribute. The command can be used directly from the `BaseSteps`, when an element isn't scoped, or it can be chained and used to find an element within a scope

--- packages/legacy-workbench/src/js/angular/export/plugin.js
+++ packages/legacy-workbench/src/js/angular/export/plugin.js
@@ -20,7 +20,8 @@ PluginRegistry.add('main.menu', {
             order: 0,
             role: 'IS_AUTHENTICATED_FULLY',
             parent: 'Explore',
-            guideSelector: 'sub-menu-graph-overview'
+            guideSelector: 'sub-menu-graph-overview',
+            testSelector: 'sub-menu-graph-overview'
         }
     ]
 });
