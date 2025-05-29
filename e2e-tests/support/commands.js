be1b8088d6879e0537958fb9a5ac4c7b8d4934ae
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-05-29 09:19:48 +0300

    fix comments
    Added `data-test-id` attribute to explore submenus to be used for test specific selection
    Added a cypress command, which can be called to retrieve an element by its `data-test-id` attribute. The command can be used directly from the `BaseSteps`, when an element isn't scoped, or it can be chained and used to find an element within a scope
    
    (cherry picked from commit d910be1bd925d459e747bf2da3cb50e96bca090c)

--- e2e-tests/support/commands.js
+++ e2e-tests/support/commands.js
@@ -86,3 +86,22 @@ Cypress.Commands.add("hideToastContainer", (url, formData) => {
     cy.get('.toast-success')
         .then((toastContainer) => toastContainer && toastContainer.remove());
 });
+
+
+/**
+ * Get element by testId. Can be chained to another cypress command, or used directly
+ *
+ * @Example
+ * MySteps.getSomething().getByTestId('my-test-id');
+ * Or
+ * cy.getByTestId('my-test-id');
+ */
+Cypress.Commands.add('getByTestId', {prevSubject: 'optional'}, (subject, testId) => {
+    return subject
+        ? cy.wrap(subject).find(buildTestIdAttr(testId))
+        : cy.get(buildTestIdAttr(testId));
+});
+
+function buildTestIdAttr(testId) {
+    return `[data-test-id="${testId}"]`;
+}
