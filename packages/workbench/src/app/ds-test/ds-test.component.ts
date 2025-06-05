8c22fcb33028b94c2a5df29c22d5bbcbc4e6e67e
Yordan Alexandrov <yordan.alexandrov@graphwise.ai>
2025-06-05 10:34:57 +0300

    Implemented components in the `DsTestComponent`

--- packages/workbench/src/app/ds-test/ds-test.component.ts
+++ packages/workbench/src/app/ds-test/ds-test.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {Ripple} from "primeng/ripple";
+import {InputText} from "primeng/inputtext";
+import {InputNumber} from "primeng/inputnumber";
+import {FormsModule} from "@angular/forms";
+import {Password} from "primeng/password";
+import {Textarea} from "primeng/textarea";
+import {Card} from "primeng/card";
 
 @Component({
     selector: 'app-ds-test',
-    imports: [],
+  imports: [ButtonModule, Ripple, InputText, InputNumber, FormsModule, Password, Textarea, Card],
     templateUrl: './ds-test.component.html',
     styleUrl: './ds-test.component.scss'
 })
 export class DsTestComponent {
+  numberValue = 123456;
+  passwordValue = 'secret password';
+  textAreaValue = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus velit non turpis placerat, id ullamcorper nunc dignissim. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus velit non turpis placerat, id ullamcorper nunc dignissim. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus velit non turpis placerat, id ullamcorper nunc dignissim. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus velit non turpis placerat, id ullamcorper nunc dignissim. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus velit non turpis placerat, id ullamcorper nunc dignissim. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus velit non turpis placerat, id ullamcorper nunc dignissim. Nulla facilisi. Donec sagittis est non';
 
 }
