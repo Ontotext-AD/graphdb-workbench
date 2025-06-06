18e748b8541a135180f9811133deee36d18aebf7
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-06-06 14:21:50 +0300

    GDB-12505 Fix enabled request on repository deletion
    ## What
    Fix unnecessary autocomplete `/enabled` check, when the selected repository is deleted
    
    ## Why
    The backend throws an error, since the repository does not exist anymore
    
    ## How
    Made `updateSelectedRepository` to return the resolved promise of `validatePropertyChange`. Previously, when a repository was deleted, `updateSelectedRepository` was called, but the validations from `validatePropertyChange` happen in a promise, in a separate thread tick. Then `repositoryIsSet` was broadcasted with wrong store data. This caused the application to make a request for autocomplete enabled
    
    ## Testing
    n/a

--- packages/legacy-workbench/src/js/angular/core/services/repositories.service.js
+++ packages/legacy-workbench/src/js/angular/core/services/repositories.service.js
@@ -396,21 +396,24 @@ repositories.service('$repositories', ['toastr', '$rootScope', '$timeout', '$loc
             eventEmitterService.emit('repositoryWillChangeEvent', eventData, (eventData) => {
                 if (!eventData.cancel) {
                     const repositoryContextService = ServiceProvider.get(RepositoryContextService);
-                    // this will update the values in the local storage and trigger context change for other opened tabs
-                    repositoryContextService.updateSelectedRepository(repo);
-                    this.setRepositoryHeaders(repo);
-                    $rootScope.$broadcast('repositoryIsSet', {newRepo: true});
-
                     // if the current repo is unreadable by the currently logged in user (or free access user)
                     // we unset the repository
-                    if (repo && !$jwtAuth.canReadRepo(repo) && !$jwtAuth.hasGraphqlReadRights(repo)) {
-                        this.setRepository('');
+                    if (!repo || (!$jwtAuth.canReadRepo(repo) && !$jwtAuth.hasGraphqlReadRights(repo))) {
+                        repositoryContextService.updateSelectedRepository(undefined);
+                    } else {
+                        repo.isNew = true;
+                        repositoryContextService.updateSelectedRepository(repo);
                     }
-                    // reset denied permissions (different repo, different rights)
-                    $rootScope.deniedPermissions = {};
                 }
             });
         };
+
+        this.onRepositorySet = function (newRepository) {
+            this.setRepositoryHeaders(newRepository);
+            $rootScope.$broadcast('repositoryIsSet', {newRepo: newRepository?.isNew});
+            // reset denied permissions (different repo, different rights)
+            $rootScope.deniedPermissions = {};
+        }
 
         this.getDefaultRepository = function () {
             return this.hasActiveLocation() ? this.location.defaultRepository : '';
