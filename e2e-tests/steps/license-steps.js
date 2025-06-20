dfc8fc4bc9bba4998efffdfeba13076b1703c5fb
boyantonchev <boyan.tonchev@graphwise.ai>
2025-06-20 11:51:15 +0300

    GDB-12173 Add initial state tests for the License view
    ## What
    Added tests to verify the initial state of the License view.
    
    ## Why
    To ensure that the view loads correctly and prevent navigation issues during the view migration process.
    
    ## How
    Added a test for the view that verifies:
    - Access via the navigation menu;
    - Direct access via URL;
    - Proper initialization and rendering in the expected initial state
    
    (cherry picked from commit e0c6ab9794a52ee33428ce0a503954f0136342c6)

--- e2e-tests/steps/license-steps.js
+++ e2e-tests/steps/license-steps.js
@@ -1,25 +1,107 @@
-export class LicenseSteps {
+import {BaseSteps} from "./base-steps";
+
+export class LicenseSteps extends BaseSteps {
     static visit() {
         cy.visit('/license');
     }
 
-    static getLicense() {
-        return cy.get('.license-container');
+    static getLicensePage() {
+        return this.getByTestId('license-info-page');
+    }
+
+    static getLicenseAlertButton() {
+        return this.getByTestId('onto-license-alert');
     }
 
     static getLicenseHeader() {
-        return LicenseSteps.getLicense().find('.card-header');
+        return LicenseSteps.getLicensePage().find('.card-header');
+    }
+
+    static getRemoveLicenseButton() {
+        return LicenseSteps.getLicensePage().getByTestId('remove-license-button');
+    }
+
+    static getSetNewLicenseButton() {
+        return LicenseSteps.getLicensePage().getByTestId('set-new-license-link');
+    }
+
+    static getNoValidLicenseAlert() {
+        return LicenseSteps.getLicensePage().getByTestId('no-valid-license-alert');
+    }
+
+    static getNoLicenseAlertTitle() {
+        return LicenseSteps.getLicensePage().getByTestId('no-license-title');
+    }
+
+    static getValidLicenseAlertTitle() {
+        return LicenseSteps.getLicensePage().getByTestId('valid-license-title');
+    }
+
+    static getLicenseContent() {
+        return LicenseSteps.getLicensePage().getByTestId('license-content');
     }
 
     static getHardcodedAlertMessage() {
-        return cy.get('.license-hardcoded-alert-message');
+        return cy.getByTestId('license-hardcoded-alert-message');
     }
 
-    static getRemoveLicenseLicenseButton() {
-        return LicenseSteps.getLicense().find('.revert-to-free-license-btn');
+    static verifyLicenseNotSet() {
+        LicenseSteps.getLicensePage().should('be.visible');
+        LicenseSteps.getLicenseAlertButton().should('exist');
+        LicenseSteps.getRemoveLicenseButton().should('be.visible').should('be.disabled');
+        LicenseSteps.getSetNewLicenseButton().should('be.visible');
+        LicenseSteps.getNoValidLicenseAlert().should('exist');
+        LicenseSteps.getNoLicenseAlertTitle().should('exist');
+        LicenseSteps.getValidLicenseAlertTitle().should('not.exist');
+        LicenseSteps.getLicenseContent().should('not.exist');
+        LicenseSteps.getHardcodedAlertMessage().should('not.exist');
     }
 
-    static getSetNewLicenseElement() {
-        return LicenseSteps.getLicense().find('.set-new-license-link');
+    static verifyNoValidLicense() {
+       LicenseSteps.getLicensePage().should('be.visible');
+       LicenseSteps.getLicenseAlertButton().should('be.visible');
+       LicenseSteps.getRemoveLicenseButton().should('exist');
+       LicenseSteps.getSetNewLicenseButton().should('be.visible');
+       LicenseSteps.getNoValidLicenseAlert().should('be.visible');
+       LicenseSteps.getNoLicenseAlertTitle().should('not.exist');
+       LicenseSteps.getValidLicenseAlertTitle().should('exist');
+       LicenseSteps.getLicenseContent().should('exist');
+       LicenseSteps.getHardcodedAlertMessage().should('not.exist');
+    }
+
+    static verifyValidLicense() {
+        LicenseSteps.getLicensePage().should('be.visible');
+        LicenseSteps.getLicenseAlertButton().should('not.exist');
+        LicenseSteps.getRemoveLicenseButton().should('be.visible').should('be.enabled');
+        LicenseSteps.getSetNewLicenseButton().should('be.visible');
+        LicenseSteps.getNoValidLicenseAlert().should('not.exist');
+        LicenseSteps.getNoLicenseAlertTitle().should('not.exist');
+        LicenseSteps.getValidLicenseAlertTitle().should('exist');
+        LicenseSteps.getLicenseContent().should('exist');
+        LicenseSteps.getHardcodedAlertMessage().should('not.exist');
+    }
+
+    static verifyNoValidLicenseHardcoded() {
+        LicenseSteps.getLicensePage().should('be.visible');
+        LicenseSteps.getLicenseAlertButton().should('be.visible');
+        LicenseSteps.getRemoveLicenseButton().should('not.exist');
+        LicenseSteps.getSetNewLicenseButton().should('not.exist');
+        LicenseSteps.getNoValidLicenseAlert().should('exist');
+        LicenseSteps.getNoLicenseAlertTitle().should('not.exist');
+        LicenseSteps.getValidLicenseAlertTitle().should('exist');
+        LicenseSteps.getLicenseContent().should('exist');
+        LicenseSteps.getHardcodedAlertMessage().should('exist');
+    }
+
+    static verifyValidLicenseHardcoded() {
+        LicenseSteps.getLicensePage().should('be.visible');
+        LicenseSteps.getLicenseAlertButton().should('not.exist');
+        LicenseSteps.getRemoveLicenseButton().should('not.exist');
+        LicenseSteps.getSetNewLicenseButton().should('not.exist');
+        LicenseSteps.getNoValidLicenseAlert().should('not.exist');
+        LicenseSteps.getNoLicenseAlertTitle().should('not.exist');
+        LicenseSteps.getValidLicenseAlertTitle().should('exist');
+        LicenseSteps.getLicenseContent().should('exist');
+        LicenseSteps.getHardcodedAlertMessage().should('exist');
     }
 }
