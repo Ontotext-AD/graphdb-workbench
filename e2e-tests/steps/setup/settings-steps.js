3b0b1a24df6902d67958a4f506c7f0cbfffa1382
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-06-23 10:51:50 +0300

    GDB-12173 Add initial state specs for my settings
    ## What
    Added tests which assert the initial state of my settings view.
    
    ## Why
    To ensure the page loads correctly
    
    ## How
    Added tests, which verify the state of the view by navigating via URL and by visiting it through the navigation menu. Added tests with a selected repo and without any repositories
    
    ## Testing
    cypress

--- e2e-tests/steps/setup/settings-steps.js
+++ e2e-tests/steps/setup/settings-steps.js
@@ -3,8 +3,89 @@ export class SettingsSteps {
         cy.visit('/settings');
     }
 
+    static getSettingsPage() {
+        return cy.get('#wb-user');
+    }
+
+    static getLoginPanel() {
+        return this.getSettingsPage().find('.login-credentials')
+    }
+
+    static getPasswordField() {
+        return this.getLoginPanel().find('#wb-user-password:password');
+    }
+
+    static getConfirmPasswordField() {
+        return this.getLoginPanel().find('#wb-user-confirmpassword:password');
+    }
+
+    static getSparqlEditorPanel() {
+        return this.getSettingsPage()
+            .find('.sparql-editor-settings');
+    }
+
+    static getSameAsLabel() {
+        return this.getSparqlEditorPanel().find('.sameas-label')
+    }
+
+    static getSameAsToggle() {
+        return this.getSparqlEditorPanel()
+            .find('#sameas-on')
+            .find('.switch:checkbox');
+    }
+
+    static getInferenceToggle() {
+        return this.getSparqlEditorPanel()
+            .find('#inference-on')
+            .find('.switch:checkbox');
+    }
+
+    static getInferenceLabel() {
+        return this.getSparqlEditorPanel().find('.inference-label');
+    }
+
+    static getCountCheckbox() {
+        return this.getSparqlEditorPanel().find('#defaultCount:checkbox');
+    }
+
+    static getIgnoreSharedCheckbox() {
+        return this.getSparqlEditorPanel().find('#ignore-shared:checkbox');
+    }
+
+    static getUserRolePanel() {
+        return this.getSettingsPage().find('.user-role');
+    }
+
+    static getUserRoleRadioButton() {
+        return this.getUserRolePanel().find('#roleAdmin:radio');
+    }
+
+    static getRepoManagerRadioButton() {
+        return this.getUserRolePanel().find('#roleRepoAdmin:radio');
+    }
+
+    static getAdminRadioButton() {
+        return this.getUserRolePanel().find('#roleUser:radio');
+    }
+
     static getCookiePolicyButton() {
-        return cy.get('.show-cookie-policy-btn');
+        return this.getSettingsPage().find('.show-cookie-policy-btn');
+    }
+
+    static getUserRepositoryTable() {
+        return this.getSettingsPage().find('.user-repositories .table');
+    }
+
+    static getUserRepository(name) {
+        return this.getUserRepositoryTable().find(`.repository-name:contains('${name}')`).closest('tr');
+    }
+
+    static getReadRightsCheckbox(repositoryId) {
+        return this.getUserRepository(repositoryId).find('.read-rights .read:checkbox');
+    }
+
+    static getWriteRightsCheckbox(repositoryId) {
+        return this.getUserRepository(repositoryId).find('.write-rights .write:checkbox');
     }
 
     static clickCookiePolicyLink() {
