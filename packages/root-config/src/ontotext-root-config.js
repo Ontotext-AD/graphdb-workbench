8275efb596c8f9e5d2b782e90a9aaa92c9132a79
Yordan Alexandrov <yordan.alexandrov@graphwise.ai>
2025-07-04 12:40:39 +0300

    GDB-12633 SPARQL editor copied query urls not working
    ## What
    Fixed component not loading again after unmount/mount
    
    ## Why
    The problem happens because SingleSpa refires a `popstate` event if the URL changes, even if only the query parameters have changed. This causes AngularJS to redraw which unmount/mounts the StencilJS components.
    With the latest release of `ontotext-yasgui-web-component`, there is reinitialization of the component after it was mounted again
    
    How
    - Updated `ontotext-yasgui-web-component`

--- packages/root-config/src/ontotext-root-config.js
+++ packages/root-config/src/ontotext-root-config.js
@@ -94,10 +94,3 @@ bootstrapWorkbench()
     console.error('Error during bootstrap of workbench', error);
   });
 
-// window.addEventListener("single-spa:routing-event", (evt) => {
-//     console.log("single-spa finished mounting/unmounting applications!");
-//     console.log(evt.detail.originalEvent); // PopStateEvent
-//     console.log(evt.detail.newAppStatuses); // { app1: MOUNTED, app2: NOT_MOUNTED }
-//     console.log(evt.detail.appsByNewStatus); // { MOUNTED: ['app1'], NOT_MOUNTED: ['app2'] }
-//     console.log(evt.detail.totalAppChanges); // 2
-// });
