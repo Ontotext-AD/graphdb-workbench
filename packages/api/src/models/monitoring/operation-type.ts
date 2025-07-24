cc390bc318a51c8c35328b6fbdd33fb36ba57a28
Yordan Alexandrov <yordan.alexandrov@graphwise.ai>
2025-07-24 15:42:28 +0300

    GDB-12791 Fix wrong redirect from operations notification component
    ## What
    Fix wrong redirect to `import` when user clicks on running imports while importing in cluster
    
    ## Why
    The redirect was wrong to `imports`, rather than `import` when user clicks on running imports
    
    How
    - fixed typo

--- packages/api/src/models/monitoring/operation-type.ts
+++ packages/api/src/models/monitoring/operation-type.ts
@@ -1,7 +1,7 @@
 export enum OperationType {
   UPDATES = 'updates',
   QUERIES = 'queries',
-  IMPORTS = 'imports',
+  IMPORT = 'imports',
   BACKUP_AND_RESTORE = 'backupAndRestore',
   CLUSTER_HEALTH = 'clusterHealth'
 }
