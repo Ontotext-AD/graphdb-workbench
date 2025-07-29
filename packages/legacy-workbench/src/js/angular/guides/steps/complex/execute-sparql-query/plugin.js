3f4964d70d0b373254373d562a4e87e24b8dd1c6
boyantonchev <boyan.tonchev@graphwise.ai>
2025-07-29 14:50:29 +0300

    GDB-12790: Add Query Result Visualization Step in Interactive Guides
    ## What
    A new interactive guide step has been introduced. This step explains the Visual Graph button in the SPARQL Query & Update view.
    
    ## Why
    It provides an opportunity to show users that, when a CONSTRUCT query is executed, the results can be visualized in the Visual Graph view.
    
    ## How
    A new step called "visualize-sparql-query" has been implemented. It guides users on how to execute a CONSTRUCT query and visualize the result in the Visual Graph view.
    Two steps have been extracted from the existing "execute-sparql-query" step and are now reused in both "visualize-sparql-query" and "execute-sparql-query". These extracted steps can also be reused as standalone steps when needed.

--- packages/legacy-workbench/src/js/angular/guides/steps/complex/execute-sparql-query/plugin.js
+++ packages/legacy-workbench/src/js/angular/guides/steps/complex/execute-sparql-query/plugin.js
@@ -1,11 +1,153 @@
 const createCopyToEditorListener = (YasguiComponentDirectiveUtil, sparqlDirectiveSelector, query) => {
     return (event) => {
         event.preventDefault();
-        YasguiComponentDirectiveUtil.setQuery(sparqlDirectiveSelector, query).then(() => {});
+        YasguiComponentDirectiveUtil.setQuery(sparqlDirectiveSelector, query).then(() => {
+        });
     }
 }
 
+const SPARQL_EDITOR_DEFAULT_TITLE = 'view.sparql-editor.title';
+
 PluginRegistry.add('guide.step', [
+    {
+        guideBlockName: 'sparql-results-visual-button',
+        getSteps: (options, services) => {
+            const GuideUtils = services.GuideUtils;
+            return [
+                {
+                    guideBlockName: 'clickable-element',
+                    options: {
+                        content: 'guide.step_plugin.execute-sparql-query.visual-sparql-results.content',
+                        // If mainAction is set the title will be set automatically
+                        ...(options.mainAction ? {} : {title: SPARQL_EDITOR_DEFAULT_TITLE}),
+                        url: '/sparql',
+                        elementSelector: GuideUtils.CSS_SELECTORS.SPARQL_VISUAL_BUTTON_SELECTOR,
+                        disablePreviousFlow: true,
+                        class: 'visual-sparql-results-button-guide-dialog',
+                        scrollToHandler: GuideUtils.scrollToTop,
+                        onNextClick: () => GuideUtils.clickOnElement(GuideUtils.CSS_SELECTORS.SPARQL_VISUAL_BUTTON_SELECTOR)(),
+                        ...options
+                    }
+                }
+            ]
+        }
+    },
+    {
+        guideBlockName: 'sparql-editor-run-button',
+        getSteps: (options, services) => {
+            const SPARQL_DIRECTIVE_SELECTOR = '#query-editor';
+            const GuideUtils = services.GuideUtils;
+            const YasguiComponentDirectiveUtil = services.YasguiComponentDirectiveUtil;
+            return [
+                {
+                    guideBlockName: 'clickable-element',
+                    options: {
+                        // If mainAction is set the title will be set automatically
+                        ...(options.mainAction ? {} : {title: SPARQL_EDITOR_DEFAULT_TITLE}),
+                        content: 'guide.step_plugin.execute-sparql-query.run-sparql-query.content',
+                        url: '/sparql',
+                        elementSelector: GuideUtils.CSS_SELECTORS.SPARQL_RUN_BUTTON_SELECTOR,
+                        disablePreviousFlow: true,
+                        class: 'yasgui-run-button-guide-dialog',
+                        onNextClick: (guide) => YasguiComponentDirectiveUtil.getOntotextYasguiElementAsync(SPARQL_DIRECTIVE_SELECTOR)
+                            .then((yasgui) => {
+                                yasgui.query();
+                                guide.next();
+                            }),
+                        scrollToHandler: GuideUtils.scrollToTop,
+                        ...options
+                    }
+                }
+            ]
+        }
+    },
+    {
+        guideBlockName: 'sparql-editor',
+        getSteps: (options, services) => {
+            const $translate = services.$translate;
+            const SPARQL_DIRECTIVE_SELECTOR = '#query-editor';
+            const GuideUtils = services.GuideUtils;
+            const YasguiComponentDirectiveUtil = services.YasguiComponentDirectiveUtil;
+
+            const code = document.createElement('code');
+            const copy = document.createElement('button');
+            const copyToEditorButtonClass = 'guide-copy-to-editor-query-button';
+            copy.className = `btn btn-sm btn-secondary ${copyToEditorButtonClass}`;
+            copy.innerText = $translate.instant('guide.step_plugin.execute-sparql-query.copy-to-editor.button');
+            copy.setAttribute('ng-click', 'copyQuery()');
+            const query = options.query;
+            const copyToEditorListener = createCopyToEditorListener(YasguiComponentDirectiveUtil, SPARQL_DIRECTIVE_SELECTOR, query);
+            code.innerText = query;
+
+            let stepHTMLElement;
+
+            return [
+                {
+                    guideBlockName: 'input-element',
+                    options: {
+                        // If mainAction is set the title will be set automatically
+                        ...(options.mainAction ? {} : {title: SPARQL_EDITOR_DEFAULT_TITLE}),
+                        content: 'guide.step_plugin.execute-sparql-query.query-editor.content',
+                        url: '/sparql',
+                        elementSelector: GuideUtils.CSS_SELECTORS.SPARQL_EDITOR_SELECTOR,
+                        class: 'yasgui-query-editor-guide-dialog',
+                        disablePreviousFlow: true,
+                        queryAsHtmlCodeElement: '<div class="shepherd-code">' + code.outerHTML + copy.outerHTML + '</div>',
+                        beforeShowPromise: () => YasguiComponentDirectiveUtil.getOntotextYasguiElementAsync(SPARQL_DIRECTIVE_SELECTOR)
+                            .then(() => GuideUtils.waitFor(GuideUtils.CSS_SELECTORS.SPARQL_EDITOR_SELECTOR, 3))
+                            .then(() => GuideUtils.deferredShow(500)())
+                            .catch((error) => {
+                                services.toastr.error(services.$translate.instant('guide.unexpected.error.message'));
+                                throw error;
+                            }),
+                        onNextValidate: () => YasguiComponentDirectiveUtil.getOntotextYasguiElementAsync(SPARQL_DIRECTIVE_SELECTOR)
+                            .then((yasgui) => yasgui.getQuery().then((query) => ({yasgui, queryFromEditor: query})))
+                            .then(({yasgui, queryFromEditor}) => {
+                                yasgui.setQuery(query);
+                                return true;
+                            }),
+                        scrollToHandler: GuideUtils.scrollToTop,
+                        extraContent: options.queryExtraContent,
+                        show: (_guide) => () => {
+                            stepHTMLElement = _guide.currentStep.el.querySelector(`.${copyToEditorButtonClass}`);
+                            stepHTMLElement.addEventListener('click', copyToEditorListener);
+                        },
+                        hide: () => () => {
+                            if (stepHTMLElement) {
+                                stepHTMLElement.removeEventListener('click', copyToEditorListener);
+                            }
+                        },
+                        ...options
+                    }
+                }
+            ]
+        }
+    },
+    {
+        guideBlockName: 'sparql-results-explain',
+        getSteps: (options, services) => {
+            const GuideUtils = services.GuideUtils;
+            return [
+                {
+                    guideBlockName: 'read-only-element',
+                    options: {
+                        // If mainAction is set the title will be set automatically
+                        ...(options.mainAction ? {} : {title: SPARQL_EDITOR_DEFAULT_TITLE}),
+                        content: 'guide.step_plugin.execute-sparql-query.result-explain.content',
+                        url: '/sparql',
+                        placement: 'top',
+                        elementSelector: GuideUtils.CSS_SELECTORS.SPARQL_RESULTS_SELECTOR,
+                        class: 'yasgui-query-results-guide-dialog',
+                        fileName: options.fileName,
+                        scrollToHandler: GuideUtils.scrollToTop,
+                        extraContent: options.resultExtraContent,
+                        canBePaused: false,
+                        ...options
+                    }
+                }
+            ]
+        }
+    },
     {
         guideBlockName: 'execute-sparql-query',
         getSteps: (options, services) => {
@@ -18,20 +160,14 @@ PluginRegistry.add('guide.step', [
             const RoutingUtil = services.RoutingUtil;
             options.mainAction = 'execute-sparql-query';
 
-            const code = document.createElement('code');
-            const copy = document.createElement('button');
-            const copyToEditorButtonClass = 'guide-copy-to-editor-query-button';
-            copy.className = `btn btn-sm btn-secondary ${copyToEditorButtonClass}`;
-            copy.innerText = $translate.instant('guide.step_plugin.execute-sparql-query.copy-to-editor.button');
-            copy.setAttribute('ng-click', 'copyQuery()');
-
             const steps = [
                 {
                     guideBlockName: 'click-main-menu',
-                    options: angular.extend({}, {
+                    options: {
                         menu: 'sparql',
-                        showIntro: true
-                    }, options)
+                        showIntro: true,
+                        ...options
+                    }
                 }
             ];
 
@@ -43,19 +179,13 @@ PluginRegistry.add('guide.step', [
             options.queries.forEach((queryDef, index) => {
                 const query = queryDef.query;
                 queries[index] = query;
-                code.innerText = query;
-                options.queryAsHtmlCodeElement = '<div class="shepherd-code">' + code.outerHTML + copy.outerHTML + '</div>';
-
-                const copyToEditorListener = createCopyToEditorListener(YasguiComponentDirectiveUtil, SPARQL_DIRECTIVE_SELECTOR, query);
-                let stepHTMLElement;
 
                 steps.push({
-                    guideBlockName: 'input-element',
-                    options: angular.extend({}, {
-                        content: 'guide.step_plugin.execute-sparql-query.query-editor.content',
-                        url: '/sparql',
-                        elementSelector: GuideUtils.CSS_SELECTORS.SPARQL_EDITOR_SELECTOR,
-                        class: 'yasgui-query-editor-guide-dialog',
+                    guideBlockName: 'sparql-editor',
+                    options: {
+                        query,
+                        queryExtraContent: queryDef.queryExtraContent,
+                        disablePreviousFlow: false,
                         beforeShowPromise: () => YasguiComponentDirectiveUtil.getOntotextYasguiElementAsync(SPARQL_DIRECTIVE_SELECTOR)
                             .then(() => GuideUtils.waitFor(GuideUtils.CSS_SELECTORS.SPARQL_EDITOR_SELECTOR, 3))
                             .then(() => GuideUtils.deferredShow(500)())
@@ -80,7 +210,7 @@ PluginRegistry.add('guide.step', [
                                 }
                                 overwriteQuery = true;
                                 return true;
-                                }),
+                            }),
                         initPreviousStep: () => {
                             if (index === 0) {
                                 return YasguiComponentDirectiveUtil.setQuery(SPARQL_DIRECTIVE_SELECTOR, defaultQuery);
@@ -95,33 +225,13 @@ PluginRegistry.add('guide.step', [
                             return GuideUtils.waitFor(GuideUtils.CSS_SELECTORS.SPARQL_EDITOR_SELECTOR)
                                 .then(() => YasguiComponentDirectiveUtil.executeSparqlQuery("#query-editor", query));
                         },
-                        scrollToHandler: GuideUtils.scrollToTop,
-                        extraContent: queryDef.queryExtraContent,
-                        show: (_guide) => () => {
-                            stepHTMLElement = _guide.currentStep.el.querySelector(`.${copyToEditorButtonClass}`);
-                            stepHTMLElement.addEventListener('click', copyToEditorListener);
-                        },
-                        hide: () => () => {
-                            if (stepHTMLElement) {
-                                stepHTMLElement.removeEventListener('click', copyToEditorListener);
-                            }
-                        }
-                    }, options)
+                        ...options
+                    }
                 });
                 steps.push({
-                    guideBlockName: 'clickable-element',
-                    options: angular.extend({}, {
-                        content: 'guide.step_plugin.execute-sparql-query.run-sparql-query.content',
-                        url: '/sparql',
-                        elementSelector: GuideUtils.CSS_SELECTORS.SPARQL_RUN_BUTTON_SELECTOR,
-                        class: 'yasgui-run-button-guide-dialog',
-                        onNextClick: (guide) => YasguiComponentDirectiveUtil.getOntotextYasguiElementAsync(SPARQL_DIRECTIVE_SELECTOR)
-                                .then((yasgui) => {
-                                    yasgui.query();
-                                    guide.next();
-                                }),
-                        scrollToHandler: GuideUtils.scrollToTop,
-                        canBePaused: false,
+                    guideBlockName: 'sparql-editor-run-button',
+                    options: {
+                        disablePreviousFlow: false,
                         initPreviousStep: (services, stepId) => {
                             const previousStep = services.ShepherdService.getPreviousStepFromHistory(stepId);
                             return previousStep.options.initPreviousStep(services, previousStep.options.id)
@@ -134,21 +244,14 @@ PluginRegistry.add('guide.step', [
 
                                     return YasguiComponentDirectiveUtil.executeSparqlQuery("#query-editor", query);
                                 });
-                        }
-                    }, options)
+                        },
+                        ...options
+                    }
                 });
                 steps.push({
-                    guideBlockName: 'read-only-element',
-                    options: angular.extend({}, {
-                        content: 'guide.step_plugin.execute-sparql-query.result-explain.content',
-                        url: '/sparql',
-                        placement: 'top',
-                        elementSelector: GuideUtils.CSS_SELECTORS.SPARQL_RESULTS_SELECTOR,
-                        class: 'yasgui-query-results-guide-dialog',
-                        fileName: options.fileName,
-                        scrollToHandler: GuideUtils.scrollToTop,
+                    guideBlockName: 'sparql-results-explain',
+                    options: {
                         extraContent: queryDef.resultExtraContent,
-                        canBePaused: false,
                         initPreviousStep: (services, stepId) => {
                             if ('sparql' !== RoutingUtil.getCurrentRoute()) {
                                 RoutingUtil.navigate('/sparql');
@@ -160,9 +263,45 @@ PluginRegistry.add('guide.step', [
                             const previousStep = services.ShepherdService.getPreviousStepFromHistory(stepId);
                             return previousStep.options.initPreviousStep(services, previousStep.options.id)
                                 .then(() => YasguiComponentDirectiveUtil.setQuery(SPARQL_DIRECTIVE_SELECTOR, query));
-                        }
-                    }, options)
+                        },
+                        ...options
+                    }
                 });
+            });
+
+            return steps;
+        }
+    },
+
+    {
+        guideBlockName: 'visualise-sparql-query',
+        getSteps: (options) => {
+            const steps = [];
+            if (options.useMainMenuNavigation) {
+                steps.push({
+                    guideBlockName: 'click-main-menu',
+                    options: {
+                        menu: 'sparql',
+                        mainAction: 'execute-sparql-query',
+                        showIntro: true,
+                        ...options
+                    }
+                });
+            }
+
+            steps.push({
+                guideBlockName: 'sparql-editor',
+                options: {
+                    query: options.query,
+                    queryExtraContent: options.queryExtraContent,
+                    ...options
+                }
+            }, {
+                guideBlockName: 'sparql-editor-run-button',
+                options: {...options}
+            }, {
+                guideBlockName: 'sparql-results-visual-button',
+                options: {...options}
             });
 
             return steps;
