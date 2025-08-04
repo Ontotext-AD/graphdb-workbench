2c3b3f1fc23f411f911ee52df305f5b7e0d3d794
Yordan Alexandrov <yordan.alexandrov@graphwise.ai>
2025-08-04 14:56:02 +0300

    GDB-12824 Fix latest vulnerabilities and update libraries
    ## What
    Updated libraries and fixed some vulnerabilities
    
    ## Why
    The application must be kept up-to-date and with as few vulnerabilities as possible
    
    How
    - run `npm audit fix` on the packages
    - updated Angular to latest v20

--- packages/workbench/src/app/app.component.ts
+++ packages/workbench/src/app/app.component.ts
@@ -2,14 +2,13 @@ import {Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [
-    RouterOutlet,
-    RouterLink
-  ],
-  standalone: true
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+        RouterOutlet,
+        RouterLink
+    ]
 })
 export class AppComponent {
   title = 'workbench';
