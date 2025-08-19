e5de95d4d830df067454a9276ee31df60fa6aa34
svilen.velikov <svilen.velikov@ontotext.com>
2025-08-19 13:00:07 +0300

    Adds more documentation.

--- packages/api/src/models/plugins/plugin-module.ts
+++ packages/api/src/models/plugins/plugin-module.ts
@@ -4,5 +4,9 @@ import {PluginRegistry} from './plugin-registry';
  * PluginModule interface represents a plugin module that can be registered with the PluginRegistry.
  */
 export interface PluginModule {
+  /**
+   * Registers the plugin module with the provided PluginRegistry.
+   * @param registry - The PluginRegistry instance to register the plugin module with.
+   */
   register: (registry: PluginRegistry) => void;
 }
--- packages/api/src/models/plugins/plugins-manifest.ts
+++ packages/api/src/models/plugins/plugins-manifest.ts
@@ -1,6 +1,10 @@
 import {Model} from '../common';
 import {PluginDefinitionList} from './plugin-definition-list';
 
+/**
+ * PluginsManifest is a class that represents the manifest of plugins in the application.
+ * It contains a list of plugin definitions and provides methods to access them.
+ */
 export class PluginsManifest extends Model<PluginsManifest> {
   private readonly _pluginDefinitions: PluginDefinitionList;
 
@@ -9,6 +13,10 @@ export class PluginsManifest extends Model<PluginsManifest> {
     this._pluginDefinitions = plugins;
   }
 
+  /**
+   * Retrieves the list of plugin definitions in the manifest.
+   * @return The PluginDefinitionList containing all plugin definitions.
+   */
   getPluginDefinitions(): PluginDefinitionList {
     return this._pluginDefinitions;
   }
