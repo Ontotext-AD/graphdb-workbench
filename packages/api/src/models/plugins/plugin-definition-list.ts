683160b863058d6e1b492d35462b99d96b08d5c6
svilen.velikov <svilen.velikov@ontotext.com>
2025-08-19 13:00:07 +0300

    Adds more documentation.

--- packages/api/src/models/plugins/plugin-definition-list.ts
+++ packages/api/src/models/plugins/plugin-definition-list.ts
@@ -1,6 +1,9 @@
 import {ModelList} from '../common';
 import {PluginDefinition} from './plugin-definition';
 
+/**
+ * PluginDefinitionList is a class that extends ModelList to manage a list of PluginDefinition objects.
+ */
 export class PluginDefinitionList extends ModelList<PluginDefinition> {
 
   constructor(data?: PluginDefinition[]) {
