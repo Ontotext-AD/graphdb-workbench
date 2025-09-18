8b19bc7854ab5cf7dccbe32b8bd4a9db188fec32
Yordan Alexandrov <yordan.alexandrov@graphwise.ai>
2025-09-18 09:35:29 +0300

    GDB-12700: Define authentication strategy interface
    ## What
    Created an interface for authentication strategies and an enum for supported authentication types.
    
    ## Why
    This implementation promotes flexibility and extensibility in the security module by allowing pluggable authentication strategies without entangled logic.
    
    ## How
    - Added `AuthStrategyType` enum with various authentication strategy types.
    - Defined `AuthStrategy` interface with methods for initialization, login, logout, and authentication checks.

---
+++ packages/api/src/models/security/authentication/auth-strategy-type.ts
@@ -0,0 +1,27 @@
+/**
+ * Enum representing the supported authentication strategy types.
+ *
+ * The security module provides a common interface for pluggable self-contained authentication strategies.
+ * This prevents bloating the security module with entangled logic and promotes flexibility and extensibility.
+ */
+export enum AuthStrategyType {
+  /**
+   * Authentication using a GraphDB-specific token.
+   */
+  GDB_TOKEN = 'GDB_TOKEN',
+
+  /**
+   * Authentication using OpenID Connect protocol.
+   */
+  OPENID = 'OPENID',
+
+  /**
+   * Authentication using Kerberos protocol.
+   */
+  KERBEROS = 'KERBEROS',
+
+  /**
+   * No authentication; open access.
+   */
+  NO_SECURITY = 'NO_SECURITY'
+}
