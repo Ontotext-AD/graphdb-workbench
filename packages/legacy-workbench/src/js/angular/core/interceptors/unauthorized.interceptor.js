ce4993d84979ad7e89b5ba40b4d617f8e0d3b9d2
svilen.velikov <svilen.velikov@ontotext.com>
2025-10-16 18:15:17 +0300

    Replaced $jwtAuth.canReadRepo(repository) -> AuthorizationService.canReadRepo(repo)
    Replaced $jwtAuth.isAdmin() -> AuthorizationService.isAdmin()
    
    Replaced $jwtAuth.hasRole(role) -> AuthorizationService.hasRole(role)
    
    Replaced $jwtAuth.hasGraphqlReadRights(repository) -> AuthorizationService.canReadGqlRepo(repo)
    
    Replaced $jwtAuth.isAuthenticated() -> AuthenticationService.isAuthenticated()
    
    Replaced $jwtAuth.isSecurityEnabled() -> AuthenticationService.isSecurityEnabled()
    
    Replaced $jwtAuth.hasAuthority() -> AuthorizationService.hasAuthority()
    
    Replaced $jwtAuth.updateUserData({ appSettings, username }) -> SecurityService.updateUserData(user)
    
    Replaced $jwtAuth.canWriteRepo(repo) -> AuthorizationService.canWriteRepo(repo)
    
    Replaced $jwtAuth.isRepoManager() -> AuthorizationService.isRepoManager()
    
    Removed $jwtAuth.checkForWrite(role, repo) - it was used only in the old navbar
    
    Replaces $jwtAuth.hasGraphqlRightsOverCurrentRepo() -> AuthorizationService.hasGraphqlRightsOverCurrentRepo()
    
    Adds a jsdoc to SecurityService.toggleFreeAccess
    
    Fixing conflicts after rebase and cleanup
    
    Replacing $jwtAuth.isFreeAccessEnabled() -> AuthorizationService.hasFreeAccess()
    
    Replacing $jwtAuth.hasExternalAuthUser() -> AuthenticationService.isExternalUser()
    
    Fix failing test and bug with enable/disable of security
    
    Cleanup commented out code and fixed authorizationService.canWriteGqlRepo function call parameters

--- packages/legacy-workbench/src/js/angular/core/interceptors/unauthorized.interceptor.js
+++ packages/legacy-workbench/src/js/angular/core/interceptors/unauthorized.interceptor.js
@@ -1,5 +1,9 @@
 import 'angular/core/services';
-import {ServiceProvider, SecurityContextService} from '@ontotext/workbench-api';
+import {
+    service,
+    SecurityContextService,
+    AuthenticationService,
+} from '@ontotext/workbench-api';
 import {LoggerProvider} from "../services/logger-provider";
 
 const logger = LoggerProvider.logger;
@@ -9,12 +13,14 @@ angular.module('graphdb.framework.core.interceptors.unauthorized', [
 ])
     .factory('$unauthorizedInterceptor', ['$q', '$location', '$rootScope', '$translate',
         function($q, $location, $rootScope, $translate) {
+            const authenticationService = service(AuthenticationService);
+
             return {
                 'responseError': function(response) {
                   let redirect = false;
 
                   if (response.status === 401) {
-                        if (response.config.url.indexOf('rest/security/authenticated-user') < 0 && !$rootScope.hasExternalAuthUser()) {
+                        if (response.config.url.indexOf('rest/security/authenticated-user') < 0 && !authenticationService.isExternalUser()) {
                             // Redirect to login page only if this isn't the endpoint that checks
                             // the externally logged user and when no external authentication is available.
                             // This check is essential for making free access and external auth working.
@@ -24,7 +30,7 @@ angular.module('graphdb.framework.core.interceptors.unauthorized', [
                       const pageUrl = $location.path();
                       if ($rootScope.setPermissionDenied(pageUrl)) {
                             logger.info($translate.instant('unauthorized.console.warning'));
-                            const securityContext = ServiceProvider.get(SecurityContextService);
+                            const securityContext = service(SecurityContextService);
                             const restrictedPages = securityContext.getRestrictedPages();
                             restrictedPages.setPageRestriction(pageUrl);
                             securityContext.updateRestrictedPages(restrictedPages);
