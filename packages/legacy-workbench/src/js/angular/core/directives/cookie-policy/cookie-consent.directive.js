a2cee71196d6f57a65e6db87c66861f33e0e63fd
svilen <svilen.velikov@graphwise.ai>
2025-10-22 14:14:36 +0300

    Replaced subscription for securityInit from ttyg-view.controller.js with subscription to securityContextService.onAuthenticatedUserChanged.

--- packages/legacy-workbench/src/js/angular/core/directives/cookie-policy/cookie-consent.directive.js
+++ packages/legacy-workbench/src/js/angular/core/directives/cookie-policy/cookie-consent.directive.js
@@ -83,7 +83,6 @@ function cookieConsent($jwtAuth, $uibModal, $licenseService, $translate, toastr,
             // Subscriptions
             // =========================
             const onUserChangedSubscription = securityContextService.onAuthenticatedUserChanged(onUserChanged);
-            // const securityInitListener = $scope.$on('securityInit', securityInit);
 
             $scope.$on('$destroy', () => {
                 onUserChangedSubscription();
