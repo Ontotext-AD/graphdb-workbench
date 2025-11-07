73c1a5b2c525aef0df0d783e4088a9b41f73be9e
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-11-07 14:45:20 +0200

    GDB-13339 fix similarity indexes not loading on refresh
    ## What
    Fix the loading mechanism for similarity indexes to ensure they load correctly upon page refresh.
    
    ## Why
    They weren't always loaded on refresh
    
    ## How
    There was a race condition between `repository-service.js#init()` and `similarity-list.controller.js#init()`. Depending on whether repositories were fetched on time, the similarity indexes would be shown/hidden. The reason for that is `repository-service.js#isActiveRepoOntopType` and `isActiveRepoFedXType` would return `true` if there was no active repository at the moment
    Now the check is done from the repository in the migrated context, which is available at the time of checking
    
    ## Testing
    jest

--- packages/api/src/models/repositories/test/repository.spec.ts
+++ packages/api/src/models/repositories/test/repository.spec.ts
@@ -3,7 +3,7 @@ import {RepositoryType} from '../repository-type';
 import {RepositoryState} from '../repository-state';
 
 describe('Repository', () => {
-  
+
   test('toRepositoryReference should return a valid RepositoryReference', () => {
     const repository = new Repository({
       id: 'repo123',
@@ -19,10 +19,45 @@ describe('Repository', () => {
       writable: undefined,
       unsupported: undefined,
     });
-    
+
     expect(repository.toRepositoryReference()).toEqual({
       id: 'repo123',
       location: 'http://localhost:8080'
     });
   });
+
+  test('isOntop should return true when sesameType is Ontop', () => {
+    const repository = new Repository({
+      id: 'repo-ontop',
+      sesameType: 'graphdb:OntopRepository'
+    });
+    expect(repository.isOntop()).toBe(true);
+    expect(repository.isFedx()).toBe(false);
+  });
+
+  test('isOntop should return false when sesameType is not Ontop', () => {
+    const repository = new Repository({
+      id: 'repo-not-ontop',
+      sesameType: 'some:OtherRepository'
+    });
+    expect(repository.isOntop()).toBe(false);
+  });
+
+  test('isFedx should return true when sesameType is FedX', () => {
+    const repository = new Repository({
+      id: 'repo-fedx',
+      sesameType: 'graphdb:FedXRepository'
+    });
+    expect(repository.isFedx()).toBe(true);
+    expect(repository.isOntop()).toBe(false);
+  });
+
+  test('isFedx should return false when sesameType is not FedX', () => {
+    const repository = new Repository({
+      id: 'repo-not-fedx',
+      sesameType: 'another:Type'
+    });
+    expect(repository.isFedx()).toBe(false);
+  });
+
 });
