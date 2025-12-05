67cc14f5a9b3d23d352119c343a4097cfa2953be
svilen.velikov <svilen.velikov@ontotext.com>
2025-12-05 16:32:09 +0200

    Replace icon-warning -> ri-alert-line

--- packages/legacy-workbench/src/js/angular/repositories/fedx-repo.directive.js
+++ packages/legacy-workbench/src/js/angular/repositories/fedx-repo.directive.js
@@ -178,7 +178,7 @@ function fedxRepoDirective($uibModal, RepositoriesRestService, toastr, $translat
             if (member.repoType) {
                 return 'icon-repo-' + member.repoType;
             } else if (member.store === NATIVE_STORE) {
-                return 'icon-warning';
+                return 'ri-alert-line';
             } else {
                 return 'icon-link';
             }
