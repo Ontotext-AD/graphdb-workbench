150082c6c2a369f4072eb0cd1c5aec79d0133105
desislava.borisova <desislava.borisova@graphwise.ai>
2025-12-09 19:56:03 +0200

    GDB-13535 - handle yasgui tooltip library change
    GDB-13535 - handle yasgui tooltip library change. Add updated tooltip attribute to tests.

--- e2e-tests/e2e-legacy/sparql-editor/actions/include-inferred-statements.spec.js
+++ e2e-tests/e2e-legacy/sparql-editor/actions/include-inferred-statements.spec.js
@@ -32,7 +32,7 @@ describe('Include inferred statements', () => {
         // Then I expect that include inferred statements should be enabled by default,
         YasqeSteps.getIncludeInferredStatementsButton().should('have.class', 'icon-inferred-on');
         // and the tooltip of element describes that "infer" functionality is enabled.
-        YasqeSteps.getIncludeInferredStatementsButtonTooltip().should('have.attr', 'data-tooltip', 'Include inferred data in results: ON');
+        YasqeSteps.getIncludeInferredStatementsButtonTooltip().should('have.attr', 'yasgui-data-tooltip', 'Include inferred data in results: ON');
 
         // When I execute a query.
         YasqeSteps.executeQuery();
@@ -46,7 +46,7 @@ describe('Include inferred statements', () => {
         // Then I expect the include inferred statements should be enabled by default,
         YasqeSteps.getIncludeInferredStatementsButton().should('have.class', 'icon-inferred-off');
         // and the tooltip of element describes that "infer" functionality is disabled.
-        YasqeSteps.getIncludeInferredStatementsButtonTooltip().should('have.attr', 'data-tooltip', 'Include inferred data in results: OFF');
+        YasqeSteps.getIncludeInferredStatementsButtonTooltip().should('have.attr', 'yasgui-data-tooltip', 'Include inferred data in results: OFF');
 
         // When I execute a query.
         YasqeSteps.executeQuery();
@@ -66,7 +66,7 @@ describe('Include inferred statements', () => {
         // Then I expect that "infer" element to be enabled by default,
         YasqeSteps.getActionButton(3).should('have.class', 'icon-inferred-on');
         // and the tooltip of element describes that "infer" functionality is enabled.
-        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'data-tooltip', 'Include inferred data in results: ON');
+        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'yasgui-data-tooltip', 'Include inferred data in results: ON');
 
         // When I open a new Tab.
         YasguiSteps.openANewTab();
@@ -74,7 +74,7 @@ describe('Include inferred statements', () => {
         // Then I expect that inferred element to be enabled in the new tab,
         YasqeSteps.getActionButton(3).should('have.class', 'icon-inferred-on');
         // and the tooltip of element describes that "infer" element is enabled.
-        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'data-tooltip', 'Include inferred data in results: ON');
+        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'yasgui-data-tooltip', 'Include inferred data in results: ON');
     });
 
     it('should not be enabled when user settings is set to false.', () => {
@@ -87,7 +87,7 @@ describe('Include inferred statements', () => {
         // Then I expect that "infer" element to be disabled by default,
         YasqeSteps.getActionButton(3).should('have.class', 'icon-inferred-off');
         // and the tooltip of element describes that "infer" element is disabled.
-        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'data-tooltip', 'Include inferred data in results: OFF');
+        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'yasgui-data-tooltip', 'Include inferred data in results: OFF');
 
         // When I open a new Tab.
         YasguiSteps.openANewTab();
@@ -95,6 +95,6 @@ describe('Include inferred statements', () => {
         // Then I expect that inferred element to be disabled in the new tab,
         YasqeSteps.getActionButton(3).should('have.class', 'icon-inferred-off');
         // and the tooltip of element describes that "infer" element is disabled.
-        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'data-tooltip', 'Include inferred data in results: OFF');
+        YasqeSteps.getActionButtonTooltip(3).should('have.attr', 'yasgui-data-tooltip', 'Include inferred data in results: OFF');
     });
 });
