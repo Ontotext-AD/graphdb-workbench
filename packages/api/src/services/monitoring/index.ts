9c2b1583d095d261060275deb17258140333591e
svilen.velikov <svilen.velikov@ontotext.com>
2025-12-11 16:41:44 +0200

    Improve monitoring config models, service and mappers.
    Fix sonar and copilot issues

--- packages/api/src/services/monitoring/index.ts
+++ packages/api/src/services/monitoring/index.ts
@@ -1,4 +1,3 @@
-export { MonitoringRestService } from './monitoring-rest.service';
-export { mapOperationSummaryResponseToModel } from './mapper/operation-summary-mapper';
-export { MonitoringService } from './monitoring.service';
-export { mapOperationGroupSummaryListResponseToModel } from './mapper/operation-group-summary-list.mapper';
+export {MonitoringRestService} from './monitoring-rest.service';
+export {mapOperationSummaryResponseToModel} from './mapper/operation-summary-mapper';
+export {MonitoringService} from './monitoring.service';
--- packages/api/src/services/monitoring/mapper/operation-summary-mapper.ts
+++ packages/api/src/services/monitoring/mapper/operation-summary-mapper.ts
@@ -1,14 +1,39 @@
 import {OperationStatusSummary} from '../../../models/monitoring/operation-status-summary';
-import {mapOperationListResponseToModel} from './operation-list.mapper';
-import {Operation} from '../../../models/monitoring';
+import {Operation, OperationList, OperationStatus, OperationType} from '../../../models/monitoring';
 import {MapperFn} from '../../../providers/mapper/mapper-fn';
+import {OperationStatusSummaryResponse} from '../response/operation-status-summary-response';
+import {toEnum} from '../../utils';
+
+const OPERATION_TYPE_SORT_ORDER = {
+  [OperationType.CLUSTER_HEALTH]: 0,
+  [OperationType.BACKUP_AND_RESTORE]: 1,
+  [OperationType.IMPORT]: 2,
+  [OperationType.QUERIES]: 3,
+  [OperationType.UPDATES]: 4
+};
 
 /**
- * Mapper class for converting OperationStatusSummaryResponse to OperationStatusSummary.
+ * Mapper for converting OperationStatusSummaryResponse to OperationStatusSummary.
  */
-export const mapOperationSummaryResponseToModel: MapperFn<OperationStatusSummary, OperationStatusSummary> = (data) => {
+export const mapOperationSummaryResponseToModel: MapperFn<OperationStatusSummaryResponse, OperationStatusSummary> = (data) => {
   return new OperationStatusSummary({
-    status: data.status,
-    allRunningOperations: mapOperationListResponseToModel(data.allRunningOperations as unknown as Operation[] || [])
+    status: toEnum(OperationStatus, data.status),
+    allRunningOperations: mapOperationListResponseToModel(
+      data.allRunningOperations.map((operation) => new Operation({
+        value: operation.value,
+        status: toEnum(OperationStatus, operation.status),
+        type: toEnum(OperationType, operation.type)
+      }))
+    )
   } as OperationStatusSummary);
 };
+
+/**
+ * Mapper for converting an array of Operation objects to an OperationList model.
+ */
+function mapOperationListResponseToModel(data: Operation[]): OperationList {
+  return new OperationList(
+    data
+      .sort((a, b) => OPERATION_TYPE_SORT_ORDER[a.type] - OPERATION_TYPE_SORT_ORDER[b.type])
+  );
+}
