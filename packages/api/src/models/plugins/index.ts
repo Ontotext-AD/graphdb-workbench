742f274a6b2603ed00b2f9910efa7a0abe3b9a4c
svilen.velikov <svilen.velikov@ontotext.com>
2025-12-11 18:21:00 +0200

    Improve plugins model and mapper

--- packages/api/src/models/plugins/index.ts
+++ packages/api/src/models/plugins/index.ts
@@ -3,5 +3,4 @@ export * from './plugin-definition';
 export * from './plugin-module';
 export * from './plugins-manifest';
 export * from './extension-point';
-export * from './plugins-manifest-response';
 export * from './plugin-definition-list';
--- packages/api/src/models/plugins/plugin-definition.ts
+++ packages/api/src/models/plugins/plugin-definition.ts
@@ -1,13 +1,16 @@
 /**
  * PluginDefinition interface represents the structure of a plugin definition as used in the plugin manifest.
  */
-export interface PluginDefinition {
-  /**
-   * The entry point of the plugin, typically a path to the main JavaScript file.
-   */
-  entry: string;
-  /**
-   * The name of the plugin.
-   */
-  name: string;
+export class PluginDefinition {
+  constructor(
+    /**
+     * The entry point of the plugin, typically a path to the main JavaScript file.
+     */
+    readonly entry: string,
+    /**
+     * The name of the plugin.
+     */
+    readonly name: string
+  ) {
+  }
 }
