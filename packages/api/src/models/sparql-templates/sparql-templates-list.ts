361eaa50da0559441ac864cfdd477254cc5b2742
svilen.velikov <svilen.velikov@ontotext.com>
2026-02-12 19:02:05 +0200

    GDB-13844 sparql template id with hash at the end breaks the edit page
    The sparql template id is an IRI and can have a hash (#) at the end, which breaks the templateId extraction from the url and leads to incorrect initialization of the page.
    Fixed by encoding the templateId before using it as url parameter.
    
    Reimplemented sparql templates response mapper in the api module in order to be able to add proper unit tests instead of testing everything through the UI
    
    Add TODO to reimplement the model interface as a ModelList class when services are migrated
    
    (cherry picked from commit bab732e3673560b92e04a27a020625226453b223)

---
+++ packages/api/src/models/sparql-templates/sparql-templates-list.ts
@@ -0,0 +1,6 @@
+import {SparqlTemplate} from './sparql-template';
+
+// TODO: Reimplement this as a proper ModelList class later when services are migrated too.
+export interface SparqlTemplatesList {
+  items: SparqlTemplate[];
+}
