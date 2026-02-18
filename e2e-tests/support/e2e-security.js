9d30a5dcbc8282d5a7307d0a886f8f506748d6c2
svilen <svilen.velikov@graphwise.ai>
2026-02-18 08:50:36 +0200

    Try cleaning the security status in beforeEach instead of afterEach assuming that its technically possible for test spec to fail in a way so the afterEach hook to not be triggered

--- e2e-tests/support/e2e-security.js
+++ e2e-tests/support/e2e-security.js
@@ -1,6 +1,6 @@
 import './e2e';
 
-afterEach(() => {
+beforeEach(() => {
     cy.loginAsAdmin();
     // Switch off security after each test to ensure that tests are independent and don't affect each other.
     // For example, if security is enabled in one test, it can cause other tests that don't expect security to fail.
