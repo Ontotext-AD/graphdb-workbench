8b41600335c6aa7745643070a7b56c1b1c6483ce
svilen.velikov <svilen.velikov@ontotext.com>
2026-03-10 18:49:00 +0200

    GDB-13826 Add generic page-layout component that should be used to wrap all page components providing common layout and functionality.
    GDB-13826 Implemented sparql editor page skeleton. Also added new navbar entry to access the page (this probably should be reconsidered)
    
    Add page info tooltip component
    
    Improve main menu step for SPARQL item access to properly match by exact text "SPARQL" until we have two SPARQL pages and menu items

--- packages/workbench/src/app/app.routes.ts
+++ packages/workbench/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import {Routes} from '@angular/router';
-import {SparqlComponent} from './sparql/sparql.component';
+import {SparqlEditorPageComponent} from './pages/sparql-editor/sparql-editor-page.component';
 import {NotFoundPageComponent} from './pages/not-found/not-found-page.component';
 import {LoginPageComponent} from './pages/login/login-page.component';
 import {NewViewComponent} from './new-view/new-view.component';
 import {UxTestPageComponent} from './pages/ux-test/ux-test-page.component';
 
 export const routes: Routes = [
-  {path: 'sparql-new', component: SparqlComponent},
+  {path: 'sparql-new', component: SparqlEditorPageComponent},
   {path: 'new-view', component: NewViewComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'ux-test', component: UxTestPageComponent},
---
+++ packages/workbench/src/app/components/page-info-tooltip/page-info-tooltip.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-info-tooltip',
+  imports: [],
+  templateUrl: './page-info-tooltip.component.html',
+  styleUrl: './page-info-tooltip.component.scss',
+})
+export class PageInfoTooltipComponent {
+
+}
