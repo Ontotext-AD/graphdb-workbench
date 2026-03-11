456ac2b783164a317af8b9ac46c97410f510f36f
svilen <svilen.velikov@graphwise.ai>
2026-03-11 18:12:24 +0200

    Add page info tooltip component

--- packages/workbench/src/app/pages/sparql-editor/sparql-editor-page.component.ts
+++ packages/workbench/src/app/pages/sparql-editor/sparql-editor-page.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {PageLayoutComponent} from '../../components/page-layout/page-layout.component';
 import {MessageModule} from 'primeng/message';
 import {TranslocoPipe} from '@jsverse/transloco';
+import {PageInfoTooltipComponent} from '../../components/page-info-tooltip/page-info-tooltip.component';
 
 @Component({
   selector: 'app-sparql-editor-page',
@@ -9,7 +10,8 @@ import {TranslocoPipe} from '@jsverse/transloco';
   imports: [
     TranslocoPipe,
     MessageModule,
-    PageLayoutComponent
+    PageLayoutComponent,
+    PageInfoTooltipComponent,
   ],
   templateUrl: './sparql-editor-page.component.html',
   styleUrl: './sparql-editor-page.component.scss',
