93a8b70fae813aef54f2fbc125aac136e46e62e1
svilen <svilen.velikov@graphwise.ai>
2026-05-27 14:50:40 +0300

    GDB-12880 introduce skeleton for the repository banners component
    Convert selectedRepository to a signal to make it dynamic
    
    Extend tests for the page layout

--- packages/workbench/src/app/components/page-layout/page-layout.component.ts
+++ packages/workbench/src/app/components/page-layout/page-layout.component.ts
@@ -1,30 +1,59 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 import {ApplicationQueryParams} from '../../models/application-query-params';
 import {TranslocoPipe} from '@jsverse/transloco';
 import {PageInfoTooltipComponent} from '../page-info-tooltip/page-info-tooltip.component';
+import {RepositoryRequiredBannerComponent} from '../repository-required-banner/repository-required-banner.component';
+import {
+  service,
+  RepositoryContextService,
+  Repository,
+  SubscriptionList,
+} from '@ontotext/workbench-api';
 
 @Component({
   selector: 'app-page-layout',
   standalone: true,
-  imports: [CommonModule, TranslocoPipe, PageInfoTooltipComponent],
+  imports: [
+    CommonModule,
+    TranslocoPipe,
+    PageInfoTooltipComponent,
+    RepositoryRequiredBannerComponent
+  ],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.scss'
 })
-export class PageLayoutComponent implements OnInit {
+export class PageLayoutComponent implements OnInit, OnDestroy {
+  private readonly repositoryContextService = service(RepositoryContextService);
+
   private readonly activatedRoute = inject(ActivatedRoute);
+
   embedded = signal(false);
   title = signal<string | undefined>(undefined);
   helpInfo = signal<string | undefined>(undefined);
   documentationLink = signal<string | undefined>(undefined);
+  selectedRepository = signal<Repository | undefined>(undefined);
+
+  private readonly subscriptions = new SubscriptionList();
 
   ngOnInit(): void {
+    this.selectedRepository.set(this.repositoryContextService.getSelectedRepository());
+    this.subscriptions.add(
+      this.repositoryContextService.onSelectedRepositoryChanged((repo) => {
+        this.selectedRepository.set(repo);
+      })
+    );
+
     this.getPageData();
     const queryParams = this.activatedRoute.snapshot.queryParams;
     if (queryParams.hasOwnProperty(ApplicationQueryParams.EMBEDDED)) {
       this.embedded.set(true);
     }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribeAll();
   }
 
   private getPageData(): void {
