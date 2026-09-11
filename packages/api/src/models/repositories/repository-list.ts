9746b08b756345c274af91be57f64afde9613d71
boyan.tonchev <boyan.tonchev@graphwise.ai>
2026-09-11 14:31:49 +0300

    rename filterWithCallback to filterAsList

--- packages/api/src/models/repositories/repository-list.ts
+++ packages/api/src/models/repositories/repository-list.ts
@@ -23,7 +23,7 @@ export class RepositoryList extends ModelList<Repository> {
     super(repositories);
   }
 
-  filterWithCallback(filterCallback: (repository: Repository) => boolean): RepositoryList {
+  filterAsList(filterCallback: (repository: Repository) => boolean): RepositoryList {
     return new RepositoryList(super.filter(filterCallback));
   }
 
